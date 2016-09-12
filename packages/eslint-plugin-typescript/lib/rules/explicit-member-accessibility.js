fd10c9faab2cfd4934445c25707a9eb1a508e3fb
Danny Fritz <dannyfritz@gmail.com>
2016-09-11 21:44:05 -0600

    NEW: Add rule "explicit-member-accessibility" to enforce accessibility modifiers such as public, private, and protected. (refs #4)

---
+++ packages/eslint-plugin-typescript/lib/rules/explicit-member-accessibility.js
@@ -0,0 +1,64 @@
+/**
+ * @fileoverview Enforces explicit accessibility modifier for class members
+ * @author Danny Fritz
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Rule Definition
+//------------------------------------------------------------------------------
+
+module.exports = {
+    meta: {
+        docs: {
+            description: "Enforces explicity accessibility modifiers for class members",
+            category: "TypeScript"
+        },
+        schema: []
+    },
+
+    create: function(context) {
+
+        //----------------------------------------------------------------------
+        // Helpers
+        //----------------------------------------------------------------------
+
+        /**
+         * Checks if a method declaration has an accessibility modifier.
+         * @param {ASTNode} methodDefinition The node representing a MethodDefinition.
+         * @returns {void}
+         * @private
+         */
+        function checkMethodAccessibilityModifier(methodDefinition) {
+            if (!methodDefinition.accessibility) {
+                context.report({
+                    node: methodDefinition,
+                    message: "Missing accessibility modifier on method definition " + methodDefinition.key.name + "."
+                });
+            }
+        }
+
+        /**
+         * Checks if property has an accessibility modifier.
+         * @param {ASTNode} classProperty The node representing a ClassProperty.
+         * @returns {void}
+         * @private
+         */
+        function checkPropertyAccessibilityModifier(classProperty) {
+            if (!classProperty.accessibility) {
+                context.report({
+                    node: classProperty,
+                    message: "Missing accessibility modifier on class property " + classProperty.key.name + "."
+                });
+            }
+        }
+
+        //----------------------------------------------------------------------
+        // Public
+        //----------------------------------------------------------------------
+        return {
+            ClassProperty: checkPropertyAccessibilityModifier,
+            MethodDefinition: checkMethodAccessibilityModifier
+        };
+    }
+};
