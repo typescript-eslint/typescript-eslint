926f93cb2b09503cc70063ffe70edb56a228ae1e
James Henry <james@henry.sc>
2017-06-15 12:47:37 +0100

    Breaking: Remove TypeAnnotation wrapper from constraint (#325)

--- packages/typescript-eslint-parser/tools/test-utils.js
+++ packages/typescript-eslint-parser/tools/test-utils.js
@@ -56,6 +56,13 @@ function createSnapshotTestBlock(code, config) {
             const result = parse();
             expect(result).toMatchSnapshot();
         } catch (e) {
+            /**
+             * If we are deliberately throwing because of encountering an unknown
+             * AST_NODE_TYPE, we rethrow to cause the test to fail
+             */
+            if (e.message.match("Unknown AST_NODE_TYPE")) {
+                throw new Error(e);
+            }
             expect(parse).toThrowErrorMatchingSnapshot();
         }
     };
