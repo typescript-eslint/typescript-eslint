21ec4e189537fe4a27763e2039742c5606a58658
Patricio Trevino <patricio@weirdpattern.com>
2017-08-19 19:32:03 -0500

    New: Add no-parameter-properties (#53)

---
+++ packages/eslint-plugin-typescript/lib/rules/no-parameter-properties.js
@@ -0,0 +1,86 @@
+/**
+ * @fileoverview Disallows parameter properties in class constructors.
+ * @author Patricio Trevino
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Rule Definition
+//------------------------------------------------------------------------------
+
+module.exports = {
+    meta: {
+        docs: {
+            description: "Disallows parameter properties in class constructors.",
+            category: "TypeScript"
+        },
+        schema: [
+            {
+                type: "object",
+                properties: {
+                    allows: {
+                        type: "array",
+                        items: {
+                            enum: [
+                                "readonly",
+                                "private",
+                                "protected",
+                                "public",
+                                "private readonly",
+                                "protected readonly",
+                                "public readonly"
+                            ]
+                        },
+                        minItems: 1
+                    }
+                },
+                additionalProperties: false
+            }
+        ]
+    },
+
+    create(context) {
+        const options = context.options[0] || {};
+        const allows = options.allows || [];
+
+        //----------------------------------------------------------------------
+        // Helpers
+        //----------------------------------------------------------------------
+
+        /**
+         * Gets the modifiers of `node`.
+         * @param {ASTNode} node the node to be inspected.
+         * @returns {string} the modifiers of node
+         * @private
+         */
+        function getModifiers(node) {
+            const modifiers = [];
+
+            modifiers.push(node.accessibility);
+            if (node.readonly || node.isReadonly) {
+                modifiers.push("readonly");
+            }
+
+            return modifiers.filter(Boolean).join(" ");
+        }
+
+        //----------------------------------------------------------------------
+        // Public
+        //----------------------------------------------------------------------
+        return {
+            TSParameterProperty(node) {
+                const modifiers = getModifiers(node);
+
+                if (allows.indexOf(modifiers) === -1) {
+                    context.report({
+                        node,
+                        message: "Property {{parameter}} cannot be declared in the constructor",
+                        data: {
+                            parameter: node.parameter.name
+                        }
+                    });
+                }
+            }
+        };
+    }
+};
---
+++ packages/eslint-plugin-typescript/tests/lib/rules/no-parameter-properties.js
@@ -0,0 +1,616 @@
+/**
+ * @fileoverview Disallows parameter properties in class constructors.
+ * @author Patricio Trevino
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/no-parameter-properties"),
+    RuleTester = require("eslint").RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester();
+
+ruleTester.run("no-parameter-properties", rule, {
+    valid: [
+        {
+            code: `
+class Foo {
+    constructor(name: string) {}
+}
+            `,
+            parser: "typescript-eslint-parser"
+        },
+        {
+            code: `
+class Foo {
+    constructor(name: string, age: number) {}
+}
+            `,
+            parser: "typescript-eslint-parser"
+        },
+        {
+            code: `
+class Foo {
+    constructor(name: string);
+    constructor(name: string, age?: number) {}
+}
+            `,
+            parser: "typescript-eslint-parser"
+        },
+        {
+            code: `
+class Foo {
+    constructor(readonly name: string) { }
+}
+            `,
+            options: [{ allows: ["readonly"] }],
+            parser: "typescript-eslint-parser"
+        },
+        {
+            code: `
+class Foo {
+    constructor(private name: string) { }
+}
+            `,
+            options: [{ allows: ["private"] }],
+            parser: "typescript-eslint-parser"
+        },
+        {
+            code: `
+class Foo {
+    constructor(protected name: string) { }
+}
+            `,
+            options: [{ allows: ["protected"] }],
+            parser: "typescript-eslint-parser"
+        },
+        {
+            code: `
+class Foo {
+    constructor(public name: string) { }
+}
+            `,
+            options: [{ allows: ["public"] }],
+            parser: "typescript-eslint-parser"
+        },
+        {
+            code: `
+class Foo {
+    constructor(private readonly name: string) { }
+}
+            `,
+            options: [{ allows: ["private readonly"] }],
+            parser: "typescript-eslint-parser"
+        },
+        {
+            code: `
+class Foo {
+    constructor(protected readonly name: string) { }
+}
+            `,
+            options: [{ allows: ["protected readonly"] }],
+            parser: "typescript-eslint-parser"
+        },
+        {
+            code: `
+class Foo {
+    constructor(public readonly name: string) { }
+}
+            `,
+            options: [{ allows: ["public readonly"] }],
+            parser: "typescript-eslint-parser"
+        },
+        {
+            code: `
+class Foo {
+    constructor(readonly name: string, private age: number) { }
+}
+            `,
+            options: [{ allows: ["readonly", "private"] }],
+            parser: "typescript-eslint-parser"
+        },
+        {
+            code: `
+class Foo {
+    constructor(public readonly name: string, private age: number) { }
+}
+            `,
+            options: [{ allows: ["public readonly", "private"] }],
+            parser: "typescript-eslint-parser"
+        }
+    ],
+    invalid: [
+        {
+            code: `
+class Foo {
+    constructor(readonly name: string) {}
+}
+            `,
+            parser: "typescript-eslint-parser",
+            errors: [
+                {
+                    message: "Property name cannot be declared in the constructor",
+                    line: 3,
+                    column: 17
+                }
+            ]
+        },
+        {
+            code: `
+class Foo {
+    constructor(private name: string) {}
+}
+            `,
+            parser: "typescript-eslint-parser",
+            errors: [
+                {
+                    message: "Property name cannot be declared in the constructor",
+                    line: 3,
+                    column: 17
+                }
+            ]
+        },
+        {
+            code: `
+class Foo {
+    constructor(protected name: string) {}
+}
+            `,
+            parser: "typescript-eslint-parser",
+            errors: [
+                {
+                    message: "Property name cannot be declared in the constructor",
+                    line: 3,
+                    column: 17
+                }
+            ]
+        },
+        {
+            code: `
+class Foo {
+    constructor(public name: string) {}
+}
+            `,
+            parser: "typescript-eslint-parser",
+            errors: [
+                {
+                    message: "Property name cannot be declared in the constructor",
+                    line: 3,
+                    column: 17
+                }
+            ]
+        },
+        {
+            code: `
+class Foo {
+    constructor(private readonly name: string) {}
+}
+            `,
+            parser: "typescript-eslint-parser",
+            errors: [
+                {
+                    message: "Property name cannot be declared in the constructor",
+                    line: 3,
+                    column: 17
+                }
+            ]
+        },
+        {
+            code: `
+class Foo {
+    constructor(protected readonly name: string) {}
+}
+            `,
+            parser: "typescript-eslint-parser",
+            errors: [
+                {
+                    message: "Property name cannot be declared in the constructor",
+                    line: 3,
+                    column: 17
+                }
+            ]
+        },
+        {
+            code: `
+class Foo {
+    constructor(public readonly name: string) {}
+}
+            `,
+            parser: "typescript-eslint-parser",
+            errors: [
+                {
+                    message: "Property name cannot be declared in the constructor",
+                    line: 3,
+                    column: 17
+                }
+            ]
+        },
+        {
+            code: `
+class Foo {
+    constructor(public name: string, age: number) {}
+}
+            `,
+            parser: "typescript-eslint-parser",
+            errors: [
+                {
+                    message: "Property name cannot be declared in the constructor",
+                    line: 3,
+                    column: 17
+                }
+            ]
+        },
+        {
+            code: `
+class Foo {
+    constructor(private name: string, private age: number) {}
+}
+            `,
+            parser: "typescript-eslint-parser",
+            errors: [
+                {
+                    message: "Property name cannot be declared in the constructor",
+                    line: 3,
+                    column: 17
+                },
+                {
+                    message: "Property age cannot be declared in the constructor",
+                    line: 3,
+                    column: 39
+                }
+            ]
+        },
+        {
+            code: `
+class Foo {
+    constructor(protected name: string, protected age: number) {}
+}
+            `,
+            parser: "typescript-eslint-parser",
+            errors: [
+                {
+                    message: "Property name cannot be declared in the constructor",
+                    line: 3,
+                    column: 17
+                },
+                {
+                    message: "Property age cannot be declared in the constructor",
+                    line: 3,
+                    column: 41
+                }
+            ]
+        },
+        {
+            code: `
+class Foo {
+    constructor(public name: string, public age: number) {}
+}
+            `,
+            parser: "typescript-eslint-parser",
+            errors: [
+                {
+                    message: "Property name cannot be declared in the constructor",
+                    line: 3,
+                    column: 17
+                },
+                {
+                    message: "Property age cannot be declared in the constructor",
+                    line: 3,
+                    column: 38
+                }
+            ]
+        },
+        {
+            code: `
+class Foo {
+    constructor(name: string);
+    constructor(private name: string, age?: number) {}
+}
+            `,
+            parser: "typescript-eslint-parser",
+            errors: [
+                {
+                    message: "Property name cannot be declared in the constructor",
+                    line: 4,
+                    column: 17
+                }
+            ]
+        },
+        {
+            code: `
+class Foo {
+    constructor(private name: string);
+    constructor(private name: string, age?: number) {}
+}
+            `,
+            parser: "typescript-eslint-parser",
+            errors: [
+                {
+                    message: "Property name cannot be declared in the constructor",
+                    line: 3,
+                    column: 17
+                },
+                {
+                    message: "Property name cannot be declared in the constructor",
+                    line: 4,
+                    column: 17
+                }
+            ]
+        },
+        {
+            code: `
+class Foo {
+    constructor(private name: string);
+    constructor(private name: string, private age?: number) {}
+}
+            `,
+            parser: "typescript-eslint-parser",
+            errors: [
+                {
+                    message: "Property name cannot be declared in the constructor",
+                    line: 3,
+                    column: 17
+                },
+                {
+                    message: "Property name cannot be declared in the constructor",
+                    line: 4,
+                    column: 17
+                },
+                {
+                    message: "Property age cannot be declared in the constructor",
+                    line: 4,
+                    column: 39
+                }
+            ]
+        },
+        {
+            code: `
+class Foo {
+    constructor(name: string);
+    constructor(protected name: string, age?: number) {}
+}
+            `,
+            parser: "typescript-eslint-parser",
+            errors: [
+                {
+                    message: "Property name cannot be declared in the constructor",
+                    line: 4,
+                    column: 17
+                }
+            ]
+        },
+        {
+            code: `
+class Foo {
+    constructor(protected name: string);
+    constructor(protected name: string, age?: number) {}
+}
+            `,
+            parser: "typescript-eslint-parser",
+            errors: [
+                {
+                    message: "Property name cannot be declared in the constructor",
+                    line: 3,
+                    column: 17
+                },
+                {
+                    message: "Property name cannot be declared in the constructor",
+                    line: 4,
+                    column: 17
+                }
+            ]
+        },
+        {
+            code: `
+class Foo {
+    constructor(protected name: string);
+    constructor(protected name: string, protected age?: number) {}
+}
+            `,
+            parser: "typescript-eslint-parser",
+            errors: [
+                {
+                    message: "Property name cannot be declared in the constructor",
+                    line: 3,
+                    column: 17
+                },
+                {
+                    message: "Property name cannot be declared in the constructor",
+                    line: 4,
+                    column: 17
+                },
+                {
+                    message: "Property age cannot be declared in the constructor",
+                    line: 4,
+                    column: 41
+                }
+            ]
+        },
+        {
+            code: `
+class Foo {
+    constructor(name: string);
+    constructor(public name: string, age?: number) {}
+}
+            `,
+            parser: "typescript-eslint-parser",
+            errors: [
+                {
+                    message: "Property name cannot be declared in the constructor",
+                    line: 4,
+                    column: 17
+                }
+            ]
+        },
+        {
+            code: `
+class Foo {
+    constructor(public name: string);
+    constructor(public name: string, age?: number) {}
+}
+            `,
+            parser: "typescript-eslint-parser",
+            errors: [
+                {
+                    message: "Property name cannot be declared in the constructor",
+                    line: 3,
+                    column: 17
+                },
+                {
+                    message: "Property name cannot be declared in the constructor",
+                    line: 4,
+                    column: 17
+                }
+            ]
+        },
+        {
+            code: `
+class Foo {
+    constructor(public name: string);
+    constructor(public name: string, public age?: number) {}
+}
+            `,
+            parser: "typescript-eslint-parser",
+            errors: [
+                {
+                    message: "Property name cannot be declared in the constructor",
+                    line: 3,
+                    column: 17
+                },
+                {
+                    message: "Property name cannot be declared in the constructor",
+                    line: 4,
+                    column: 17
+                },
+                {
+                    message: "Property age cannot be declared in the constructor",
+                    line: 4,
+                    column: 38
+                }
+            ]
+        },
+
+
+
+        {
+            code: `
+class Foo {
+    constructor(readonly name: string) {}
+}
+            `,
+            options: [{ allows: ["private"] }],
+            parser: "typescript-eslint-parser",
+            errors: [
+                {
+                    message: "Property name cannot be declared in the constructor",
+                    line: 3,
+                    column: 17
+                }
+            ]
+        },
+        {
+            code: `
+class Foo {
+    constructor(private name: string) {}
+}
+            `,
+            options: [{ allows: ["readonly"] }],
+            parser: "typescript-eslint-parser",
+            errors: [
+                {
+                    message: "Property name cannot be declared in the constructor",
+                    line: 3,
+                    column: 17
+                }
+            ]
+        },
+        {
+            code: `
+class Foo {
+    constructor(protected name: string) {}
+}
+            `,
+            options: [{ allows: ["readonly", "private", "public", "protected readonly"] }],
+            parser: "typescript-eslint-parser",
+            errors: [
+                {
+                    message: "Property name cannot be declared in the constructor",
+                    line: 3,
+                    column: 17
+                }
+            ]
+        },
+        {
+            code: `
+class Foo {
+    constructor(public name: string) {}
+}
+            `,
+            options: [{ allows: ["readonly", "private", "protected", "protected readonly", "public readonly"] }],
+            parser: "typescript-eslint-parser",
+            errors: [
+                {
+                    message: "Property name cannot be declared in the constructor",
+                    line: 3,
+                    column: 17
+                }
+            ]
+        },
+        {
+            code: `
+class Foo {
+    constructor(private readonly name: string) {}
+}
+            `,
+            options: [{ allows: ["readonly", "private"] }],
+            parser: "typescript-eslint-parser",
+            errors: [
+                {
+                    message: "Property name cannot be declared in the constructor",
+                    line: 3,
+                    column: 17
+                }
+            ]
+        },
+        {
+            code: `
+class Foo {
+    constructor(protected readonly name: string) {}
+}
+            `,
+            options: [{ allows: ["readonly", "protected", "private readonly", "public readonly"] }],
+            parser: "typescript-eslint-parser",
+            errors: [
+                {
+                    message: "Property name cannot be declared in the constructor",
+                    line: 3,
+                    column: 17
+                }
+            ]
+        },
+        {
+            code: `
+class Foo {
+    constructor(private name: string);
+    constructor(private name: string, protected age?: number) {}
+}
+            `,
+            options: [{ allows: ["private"] }],
+            parser: "typescript-eslint-parser",
+            errors: [
+                {
+                    message: "Property age cannot be declared in the constructor",
+                    line: 4,
+                    column: 39
+                }
+            ]
+        }
+    ]
+});
