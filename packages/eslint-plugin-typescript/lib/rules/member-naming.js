886fec633a4247873e1d81d6ec29e871d6f51cc1
Ian MacLeod <ian@nevir.net>
2017-09-25 14:51:10 -0700

    New: Add member-naming (#67)

---
+++ packages/eslint-plugin-typescript/lib/rules/member-naming.js
@@ -0,0 +1,74 @@
+/**
+ * @fileoverview Enforces naming conventions for class members by visibility.
+ * @author Ian MacLeod
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Rule Definition
+//------------------------------------------------------------------------------
+
+module.exports = {
+    meta: {
+        docs: {
+            description:
+                "Enforces naming conventions for class members by visibility.",
+            category: "TypeScript"
+        },
+        schema: [
+            {
+                type: "object",
+                properties: {
+                    public: { type: "string" },
+                    protected: { type: "string" },
+                    private: { type: "string" }
+                },
+                additionalProperties: false
+            }
+        ]
+    },
+
+    create(context) {
+        const config = context.options[0] || {};
+        const conventions = {};
+
+        for (const accessibility of Object.getOwnPropertyNames(config)) {
+            conventions[accessibility] = new RegExp(config[accessibility]);
+        }
+
+        //----------------------------------------------------------------------
+        // Helpers
+        //----------------------------------------------------------------------
+
+        /**
+         * Check that the property name matches the convention for its
+         * accessibility.
+         * @param {ASTNode} node the named node to evaluate.
+         * @returns {void}
+         * @private
+         */
+        function validateName(node) {
+            const name = node.key.name;
+            const accessibility = node.accessibility || "public";
+            const convention = conventions[accessibility];
+
+            if (!convention || convention.test(name)) return;
+
+            context.report({
+                node: node.key,
+                message:
+                    "{{accessibility}} property {{name}} should match {{convention}}",
+                data: { accessibility, name, convention }
+            });
+        }
+
+        //----------------------------------------------------------------------
+        // Public
+        //----------------------------------------------------------------------
+
+        return {
+            MethodDefinition: validateName,
+            ClassProperty: validateName
+        };
+    }
+};
