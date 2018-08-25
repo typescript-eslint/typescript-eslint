dbfdb1e7fa835113c1fc5188f6ce89750f99a062
roman.vasilev <>
2018-08-25 15:42:00 +0400

    style: Changed indent to 4 spaces

--- packages/eslint-plugin-tslint/src/index.ts
+++ packages/eslint-plugin-tslint/src/index.ts
@@ -17,146 +17,145 @@ import { createService } from 'typescript-service';
 //------------------------------------------------------------------------------
 
 type RawRuleConfig =
-  | null
-  | undefined
-  | boolean
-  | any[]
-  | {
-      severity?: RuleSeverity | 'warn' | 'none' | 'default';
-      options?: any;
+    | null
+    | undefined
+    | boolean
+    | any[]
+    | {
+        severity?: RuleSeverity | 'warn' | 'none' | 'default';
+        options?: any;
     };
 
 interface RawRulesConfig {
-  [key: string]: RawRuleConfig;
+    [key: string]: RawRuleConfig;
 }
 
 interface TSLintPluginOptions {
-  rulesDirectory?: string[];
-  rules?: RawRulesConfig;
+    rulesDirectory?: string[];
+    rules?: RawRulesConfig;
 }
 
 let languageService: ReturnType<typeof createService>;
 
 export const rules = {
-  /**
-   * Expose a single rule called "config", which will be accessed in the user's eslint config files
-   * via "tslint/config"
-   */
-  config: {
-    meta: {
-      docs: {
-        description:
-          'Wraps a TSLint configuration and lints the whole source using TSLint',
-        category: 'TSLint',
-      },
-      fixable: false,
-      schema: [
-        {
-          type: 'object',
-          properties: {
-            rules: {
-              type: 'object',
-              /**
-               * No fixed schema properties for rules, as this would be a permanently moving target
-               */
-              additionalProperties: true,
+    /**
+     * Expose a single rule called "config", which will be accessed in the user's eslint config files
+     * via "tslint/config"
+     */
+    config: {
+        meta: {
+            docs: {
+                description:
+                    'Wraps a TSLint configuration and lints the whole source using TSLint',
+                category: 'TSLint',
             },
-            rulesDirectory: {
-              type: 'array',
-              items: {
-                type: 'string',
-              },
-            },
-            configFile: {
-              type: 'string',
-            },
-            compilerOptions: {
-              type: 'object',
-              additionalProperties: true,
-            },
-          },
-          additionalProperties: false,
+            schema: [
+                {
+                    type: 'object',
+                    properties: {
+                        rules: {
+                            type: 'object',
+                            /**
+                             * No fixed schema properties for rules, as this would be a permanently moving target
+                             */
+                            additionalProperties: true,
+                        },
+                        rulesDirectory: {
+                            type: 'array',
+                            items: {
+                                type: 'string',
+                            },
+                        },
+                        configFile: {
+                            type: 'string',
+                        },
+                        compilerOptions: {
+                            type: 'object',
+                            additionalProperties: true,
+                        },
+                    },
+                    additionalProperties: false,
+                },
+            ],
         },
-      ],
+        create: function(context: Rule.RuleContext) {
+            const fileName = context.getFilename();
+            const sourceCode = context.getSourceCode().text;
+
+            /**
+             * The TSLint rules configuration passed in by the user
+             */
+            const {
+                rules: tslintRules,
+                rulesDirectory: tslintRulesDirectory,
+                configFile,
+                compilerOptions,
+            } = context.options[0];
+
+            const tslintOptions = {
+                formatter: 'json',
+                fix: false,
+                rulesDirectory: tslintRulesDirectory,
+            };
+
+            /**
+             * Manually construct a configFile for TSLint
+             */
+            const rawConfig: TSLintPluginOptions = {};
+            rawConfig.rules = tslintRules || {};
+            rawConfig.rulesDirectory = tslintRulesDirectory || [];
+
+            const tslintConfig = Configuration.parseConfigFile(rawConfig);
+
+            let program: ts.Program | undefined = undefined;
+
+            if (fileName !== '<input>' && configFile) {
+                if (!languageService) {
+                    languageService = createService({ configFile, compilerOptions });
+                }
+                program = languageService.getProgram();
+            }
+
+            /**
+             * Create an instance of TSLint
+             */
+            const tslint = new TSLintLinter(tslintOptions, program);
+
+            /**
+             * Lint the source code using the configured TSLint instance, and the rules which have been
+             * passed via the ESLint rule options for this rule (using "tslint/config")
+             */
+            tslint.lint(fileName, sourceCode, tslintConfig);
+
+            const result = tslint.getResult();
+
+            /**
+             * Format the TSLint results for ESLint
+             */
+            if (result.failures && result.failures.length) {
+                result.failures.forEach(failure => {
+                    const start = failure.getStartPosition().getLineAndCharacter();
+                    const end = failure.getEndPosition().getLineAndCharacter();
+                    context.report({
+                        message: `${failure.getFailure()} (tslint:${failure.getRuleName()})`,
+                        loc: {
+                            start: {
+                                line: start.line + 1,
+                                column: start.character,
+                            },
+                            end: {
+                                line: end.line + 1,
+                                column: end.character,
+                            },
+                        },
+                    });
+                });
+            }
+
+            /**
+             * Return an empty object for the ESLint rule
+             */
+            return {};
+        },
     },
-    create: function(context: Rule.RuleContext) {
-      const fileName = context.getFilename();
-      const sourceCode = context.getSourceCode().text;
-
-      /**
-       * The TSLint rules configuration passed in by the user
-       */
-      const {
-        rules: tslintRules,
-        rulesDirectory: tslintRulesDirectory,
-        configFile,
-        compilerOptions,
-      } = context.options[0];
-
-      const tslintOptions = {
-        formatter: 'json',
-        fix: false,
-        rulesDirectory: tslintRulesDirectory,
-      };
-
-      /**
-       * Manually construct a configFile for TSLint
-       */
-      const rawConfig: TSLintPluginOptions = {};
-      rawConfig.rules = tslintRules || {};
-      rawConfig.rulesDirectory = tslintRulesDirectory || [];
-
-      const tslintConfig = Configuration.parseConfigFile(rawConfig);
-
-      let program: ts.Program | undefined = undefined;
-
-      if (fileName !== '<input>' && configFile) {
-        if (!languageService) {
-          languageService = createService({ configFile, compilerOptions });
-        }
-        program = languageService.getProgram();
-      }
-
-      /**
-       * Create an instance of TSLint
-       */
-      const tslint = new TSLintLinter(tslintOptions, program);
-
-      /**
-       * Lint the source code using the configured TSLint instance, and the rules which have been
-       * passed via the ESLint rule options for this rule (using "tslint/config")
-       */
-      tslint.lint(fileName, sourceCode, tslintConfig);
-
-      const result = tslint.getResult();
-
-      /**
-       * Format the TSLint results for ESLint
-       */
-      if (result.failures && result.failures.length) {
-        result.failures.forEach(failure => {
-          const start = failure.getStartPosition().getLineAndCharacter();
-          const end = failure.getEndPosition().getLineAndCharacter();
-          context.report({
-            message: `${failure.getFailure()} (tslint:${failure.getRuleName()})`,
-            loc: {
-              start: {
-                line: start.line + 1,
-                column: start.character,
-              },
-              end: {
-                line: end.line + 1,
-                column: end.character,
-              },
-            },
-          });
-        });
-      }
-
-      /**
-       * Return an empty object for the ESLint rule
-       */
-      return {};
-    },
-  },
 };
--- packages/eslint-plugin-tslint/test/index.spec.ts
+++ packages/eslint-plugin-tslint/test/index.spec.ts
@@ -2,12 +2,9 @@
  * @fileoverview TSLint wrapper plugin for ESLint
  * @author James Henry
  */
-//------------------------------------------------------------------------------
-// Requirements
-//------------------------------------------------------------------------------
 
-const rule = require('../lib/index').rules.config;
-const RuleTester = require('eslint').RuleTester;
+import { rules } from '../src/index';
+import { RuleTester, Linter } from 'eslint';
 
 //------------------------------------------------------------------------------
 // Tests
@@ -15,90 +12,90 @@ const RuleTester = require('eslint').RuleTester;
 
 const ruleTester = new RuleTester();
 
-const parserOptions = {
-  ecmaVersion: 6,
-  sourceType: 'module',
-  ecmaFeatures: {},
+const parserOptions: Linter.ParserOptions = {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {},
 };
 
 /**
  * Inline rules should be supported
  */
 const tslintRulesConfig = {
-  rules: {
-    semicolon: [true, 'always'],
-  },
+    rules: {
+        semicolon: [true, 'always'],
+    },
 };
 
 /**
  * Custom rules directories should be supported
  */
 const tslintRulesDirectoryConfig = {
-  rulesDirectory: ['./test/test-tslint-rules-directory'],
-  rules: {
-    'always-fail': {
-      severity: 'error',
+    rulesDirectory: ['./test/test-tslint-rules-directory'],
+    rules: {
+        'always-fail': {
+            severity: 'error',
+        },
     },
-  },
 };
 
-ruleTester.run('tslint/config', rule, {
-  valid: [
-    {
-      code: 'var foo = true;',
-      parser: 'typescript-eslint-parser',
-      parserOptions,
-      options: [tslintRulesConfig],
-    },
-  ],
+ruleTester.run('tslint/config', rules.config, {
+    valid: [
+        {
+            code: 'var foo = true;',
+            parser: 'typescript-eslint-parser',
+            parserOptions,
+            options: [tslintRulesConfig],
+        },
+    ],
 
-  invalid: [
-    {
-      code: 'var foo = true',
-      parser: 'typescript-eslint-parser',
-      parserOptions,
-      options: [tslintRulesConfig],
-      output: 'var foo = true',
-      errors: [
+    invalid: [
         {
-          message: 'Missing semicolon (tslint:semicolon)',
-          line: 1,
-          column: 15,
+            code: 'var foo = true',
+            parser: 'typescript-eslint-parser',
+            parserOptions,
+            options: [tslintRulesConfig],
+            output: 'var foo = true',
+            errors: [
+                {
+                    message: 'Missing semicolon (tslint:semicolon)',
+                    line: 1,
+                    column: 15,
+                },
+            ],
         },
-      ],
-    },
-    {
-      code: 'var foo = true',
-      parser: 'typescript-eslint-parser',
-      parserOptions,
-      options: [tslintRulesDirectoryConfig],
-      output: 'var foo = true',
-      errors: [
         {
-          message: 'failure (tslint:always-fail)',
-          line: 1,
-          column: 1,
+            code: 'var foo = true',
+            parser: 'typescript-eslint-parser',
+            parserOptions,
+            options: [tslintRulesDirectoryConfig],
+            output: 'var foo = true',
+            errors: [
+                {
+                    message: 'failure (tslint:always-fail)',
+                    line: 1,
+                    column: 1,
+                },
+            ],
         },
-      ],
-    },
-    {
-      filename: './test/test-project/source.ts',
-      code: '/* tslint rules requires type info */',
-      parser: 'typescript-eslint-parser',
-      parserOptions,
-      options: [
         {
-          rulesDirectory: ['node_modules/tslint/lib/rules'],
-          rules: { 'restrict-plus-operands': true, 'no-for-in-array': true },
-          configFile: `${__dirname}/test-project/tsconfig.json`,
+            filename: './test/test-project/source.ts',
+            code: '/* tslint rules requires type info */',
+            parser: 'typescript-eslint-parser',
+            parserOptions,
+            options: [
+                {
+                    rulesDirectory: ['node_modules/tslint/lib/rules'],
+                    rules: { 'restrict-plus-operands': true, 'no-for-in-array': true },
+                    configFile: `${__dirname}/test-project/tsconfig.json`,
+                },
+            ],
+            errors: [
+                {
+                    message:
+                        'Operands of \'+\' operation must either be both strings or both numbers, consider using template literals (tslint:restrict-plus-operands)',
+                },
+            ],
         },
-      ],
-      errors: [
-        {
-          message:
-            'Operands of \'+\' operation must either be both strings or both numbers, consider using template literals (tslint:restrict-plus-operands)',
-        },
-      ],
-    },
-  ],
+    ],
 });
