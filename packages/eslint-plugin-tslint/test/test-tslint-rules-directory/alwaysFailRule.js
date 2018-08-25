14d6cb488762d13632a4a7b9815994f232464380
roman.vasilev <>
2018-08-25 15:51:06 +0400

    refactor: Rewritten always-fail-rule in es2015 style

--- packages/eslint-plugin-tslint/test/test-tslint-rules-directory/alwaysFailRule.js
+++ packages/eslint-plugin-tslint/test/test-tslint-rules-directory/alwaysFailRule.js
@@ -1,36 +1,22 @@
-var __extends =
-  (this && this.__extends) ||
-  function(d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
-    function __() {
-      this.constructor = d;
+const Lint = require('tslint');
+
+class Rule extends Lint.Rules.AbstractRule {
+
+    apply(sourceFile) {
+        return this.applyWithWalker(
+            new AlwaysFailWalker(sourceFile, this.getOptions()),
+        );
     }
-    d.prototype =
-      b === null ? Object.create(b) : ((__.prototype = b.prototype), new __());
-  };
-var Lint = require('tslint');
-var Rule = (function(_super) {
-  __extends(Rule, _super);
-  function Rule() {
-    _super.apply(this, arguments);
-  }
-  Rule.prototype.apply = function(sourceFile) {
-    return this.applyWithWalker(
-      new AlwaysFailWalker(sourceFile, this.getOptions()),
-    );
-  };
-  return Rule;
-})(Lint.Rules.AbstractRule);
+}
+
+
+class AlwaysFailWalker extends Lint.RuleWalker {
+
+    visitSourceFile(node) {
+        this.addFailure(
+            this.createFailure(node.getStart(), node.getWidth(), 'failure'),
+        );
+    }
+}
+
 exports.Rule = Rule;
-var AlwaysFailWalker = (function(_super) {
-  __extends(AlwaysFailWalker, _super);
-  function AlwaysFailWalker() {
-    _super.apply(this, arguments);
-  }
-  AlwaysFailWalker.prototype.visitSourceFile = function(node) {
-    this.addFailure(
-      this.createFailure(node.getStart(), node.getWidth(), 'failure'),
-    );
-  };
-  return AlwaysFailWalker;
-})(Lint.RuleWalker);
