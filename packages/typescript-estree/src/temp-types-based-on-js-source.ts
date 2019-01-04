dbddd652028409fff44bf64e17a336bade9f58be
Armano <armano2@users.noreply.github.com>
2019-01-04 16:48:12 +0100

    refactor: improve typing (#83)

--- packages/typescript-estree/src/temp-types-based-on-js-source.ts
+++ packages/typescript-estree/src/temp-types-based-on-js-source.ts
@@ -21,7 +21,7 @@ export interface ESTreeToken {
 export interface ESTreeNode {
   type: string;
   loc: ESTreeNodeLoc;
-  range: number[];
+  range: [number, number];
   declaration?: ESTreeNode;
   specifiers?: any[];
   source?: any;
@@ -36,7 +36,7 @@ export interface ESTreeNode {
   modifiers?: any;
   body?: any;
   params?: any;
-  accessibility?: any;
+  accessibility?: 'public' | 'protected' | 'private';
   readonly?: boolean;
   static?: boolean;
   export?: boolean;
