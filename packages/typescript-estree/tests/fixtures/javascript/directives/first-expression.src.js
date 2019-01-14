4a385d76eb421c42588c651175550e49f504dc27
Armano <armano2@users.noreply.github.com>
2019-01-14 01:16:40 +0100

    fix: mark only leading string expressions as directives (#4)

---
+++ packages/typescript-estree/tests/fixtures/javascript/directives/first-expression.src.js
@@ -0,0 +1,4 @@
+// Prevent strings from being parsed as directives
+// See https://github.com/prettier/prettier/pull/1560#issue-227225960
+0;
+"abc"
---
+++ packages/typescript-estree/tests/fixtures/javascript/directives/non-unique-directive.src.js
@@ -0,0 +1,3 @@
+"use strict";
+"use strict";
+"test";
