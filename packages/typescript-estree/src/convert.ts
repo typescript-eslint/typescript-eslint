14d63d905498ec94a64d4416c1c66b50cc034d2c
Armano <armano2@users.noreply.github.com>
2019-01-14 05:21:06 +0100

    fix(estree): remove unneeded regexp

--- packages/typescript-estree/src/convert.ts
+++ packages/typescript-estree/src/convert.ts
@@ -1785,10 +1785,7 @@ export default function convert(config: ConvertConfig): ESTreeNode | null {
       // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Errors/Deprecated_octal
       // http://www.ecma-international.org/ecma-262/6.0/#sec-additional-syntax-numeric-literals
       if (/^(-?)0([0-7]+)$/i.test(rawValue)) {
-        value = parseInt(
-          node.text.replace(/^(-?)0([0-7]+)$/i, (match, p0, p1) => p0 + p1),
-          8
-        );
+        value = parseInt(node.text, 8);
       }
       Object.assign(result, {
         type: AST_NODE_TYPES.Literal,
