dd28d2bb04649c79354b5ee187b4ed0fea64b16a
Armano <armano2@users.noreply.github.com>
2019-01-18 21:36:08 +0100

    test(eslint): migrate eslint rule tests from parser to plugin

---
+++ packages/eslint-plugin-typescript/tests/lib/eslint-rules/no-restricted-globals.js
@@ -0,0 +1,35 @@
+'use strict';
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require('eslint/lib/rules/no-restricted-globals'),
+  RuleTester = require('eslint').RuleTester;
+
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {}
+  },
+  parser: '@typescript-eslint/parser'
+});
+
+ruleTester.run('no-restricted-globals', rule, {
+  valid: [
+    // https://github.com/eslint/typescript-eslint-parser/issues/487
+    {
+      code: `
+export default class Test {
+    private status: string;
+    getStatus() {
+        return this.status;
+    }
+}
+      `,
+      options: ['status']
+    }
+  ],
+  invalid: []
+});
---
+++ packages/eslint-plugin-typescript/tests/lib/eslint-rules/no-use-before-define.js
@@ -0,0 +1,37 @@
+'use strict';
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require('eslint/lib/rules/no-use-before-define'),
+  RuleTester = require('eslint').RuleTester;
+
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module'
+  },
+  parser: '@typescript-eslint/parser'
+});
+
+ruleTester.run('no-undef', rule, {
+  valid: [
+    // https://github.com/eslint/typescript-eslint-parser/issues/550
+    `
+function test(file: Blob) {
+  const slice: typeof file.slice =
+    file.slice || (file as any).webkitSlice || (file as any).mozSlice
+  return slice
+}
+    `,
+    // https://github.com/eslint/typescript-eslint-parser/issues/435
+    `
+interface Foo {
+    bar: string
+}
+const bar = 'blah'
+    `
+  ],
+  invalid: []
+});
