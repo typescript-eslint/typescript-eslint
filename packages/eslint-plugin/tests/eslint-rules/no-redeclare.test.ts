ca49c338a9859c0ed67fa455aeb36e3d977f1b3b
Armano <armano2@users.noreply.github.com>
2019-02-11 03:43:25 +0100

    fix(eslint-plugin): fix merge conflicts

--- packages/eslint-plugin/tests/eslint-rules/no-redeclare.test.ts
+++ packages/eslint-plugin/tests/eslint-rules/no-redeclare.test.ts
@@ -1,5 +1,6 @@
-const rule = require('eslint/lib/rules/no-redeclare'),
-  RuleTester = require('eslint').RuleTester;
+import rule from 'eslint/lib/rules/no-redeclare';
+import { AST_NODE_TYPES } from '@typescript-eslint/typescript-estree';
+import { RuleTester } from '../RuleTester';
 
 const ruleTester = new RuleTester({
   parserOptions: {
@@ -81,62 +82,128 @@ class D<T> {}
     {
       code: 'var a = 3; var a = 10;',
       parserOptions: { ecmaVersion: 6 },
-      errors: [{ message: "'a' is already defined.", type: 'Identifier' }]
+      errors: [
+        {
+          message: "'a' is already defined.",
+          type: AST_NODE_TYPES.Identifier
+        } as any
+      ]
     },
     {
       code: 'switch(foo) { case a: var b = 3;\ncase b: var b = 4}',
-      errors: [{ message: "'b' is already defined.", type: 'Identifier' }]
+      errors: [
+        {
+          message: "'b' is already defined.",
+          type: AST_NODE_TYPES.Identifier
+        } as any
+      ]
     },
     {
       code: 'var a = 3; var a = 10;',
-      errors: [{ message: "'a' is already defined.", type: 'Identifier' }]
+      errors: [
+        {
+          message: "'a' is already defined.",
+          type: AST_NODE_TYPES.Identifier
+        } as any
+      ]
     },
     {
       code: 'var a = {}; var a = [];',
-      errors: [{ message: "'a' is already defined.", type: 'Identifier' }]
+      errors: [
+        {
+          message: "'a' is already defined.",
+          type: AST_NODE_TYPES.Identifier
+        } as any
+      ]
     },
     {
       code: 'var a; function a() {}',
-      errors: [{ message: "'a' is already defined.", type: 'Identifier' }]
+      errors: [
+        {
+          message: "'a' is already defined.",
+          type: AST_NODE_TYPES.Identifier
+        } as any
+      ]
     },
     {
       code: 'function a() {} function a() {}',
-      errors: [{ message: "'a' is already defined.", type: 'Identifier' }]
+      errors: [
+        {
+          message: "'a' is already defined.",
+          type: AST_NODE_TYPES.Identifier
+        } as any
+      ]
     },
     {
       code: 'var a = function() { }; var a = function() { }',
-      errors: [{ message: "'a' is already defined.", type: 'Identifier' }]
+      errors: [
+        {
+          message: "'a' is already defined.",
+          type: AST_NODE_TYPES.Identifier
+        } as any
+      ]
     },
     {
       code: 'var a = function() { }; var a = new Date();',
-      errors: [{ message: "'a' is already defined.", type: 'Identifier' }]
+      errors: [
+        {
+          message: "'a' is already defined.",
+          type: AST_NODE_TYPES.Identifier
+        } as any
+      ]
     },
     {
       code: 'var a = 3; var a = 10; var a = 15;',
       errors: [
-        { message: "'a' is already defined.", type: 'Identifier' },
-        { message: "'a' is already defined.", type: 'Identifier' }
+        {
+          message: "'a' is already defined.",
+          type: AST_NODE_TYPES.Identifier
+        } as any,
+        {
+          message: "'a' is already defined.",
+          type: AST_NODE_TYPES.Identifier
+        } as any
       ]
     },
     {
       code: 'var a; var a;',
       parserOptions: { sourceType: 'module' },
-      errors: [{ message: "'a' is already defined.", type: 'Identifier' }]
+      errors: [
+        {
+          message: "'a' is already defined.",
+          type: AST_NODE_TYPES.Identifier
+        } as any
+      ]
     },
     {
       code: 'export var a; var a;',
       parserOptions: { sourceType: 'module' },
-      errors: [{ message: "'a' is already defined.", type: 'Identifier' }]
+      errors: [
+        {
+          message: "'a' is already defined.",
+          type: AST_NODE_TYPES.Identifier
+        } as any
+      ]
     },
     {
       code: 'var Object = 0;',
       options: [{ builtinGlobals: true }],
-      errors: [{ message: "'Object' is already defined.", type: 'Identifier' }]
+      errors: [
+        {
+          message: "'Object' is already defined.",
+          type: AST_NODE_TYPES.Identifier
+        } as any
+      ]
     },
     {
       code: 'var top = 0;',
       options: [{ builtinGlobals: true }],
-      errors: [{ message: "'top' is already defined.", type: 'Identifier' }],
+      errors: [
+        {
+          message: "'top' is already defined.",
+          type: AST_NODE_TYPES.Identifier
+        } as any
+      ],
       env: { browser: true }
     },
     {
@@ -144,34 +211,60 @@ class D<T> {}
       options: [{ builtinGlobals: true }],
       parserOptions: { ecmaVersion: 6 },
       errors: [
-        { message: "'a' is already defined.", type: 'Identifier' },
-        { message: "'Object' is already defined.", type: 'Identifier' }
+        {
+          message: "'a' is already defined.",
+          type: AST_NODE_TYPES.Identifier
+        } as any,
+        {
+          message: "'Object' is already defined.",
+          type: AST_NODE_TYPES.Identifier
+        } as any
       ]
     },
     {
       code: 'var a; var {a = 0, b: Object = 0} = {};',
       options: [{ builtinGlobals: true }],
       parserOptions: { ecmaVersion: 6, sourceType: 'module' },
-      errors: [{ message: "'a' is already defined.", type: 'Identifier' }]
+      errors: [
+        {
+          message: "'a' is already defined.",
+          type: AST_NODE_TYPES.Identifier
+        } as any
+      ]
     },
     {
       code: 'var a; var {a = 0, b: Object = 0} = {};',
       options: [{ builtinGlobals: true }],
       parserOptions: { ecmaVersion: 6, ecmaFeatures: { globalReturn: true } },
-      errors: [{ message: "'a' is already defined.", type: 'Identifier' }]
+      errors: [
+        {
+          message: "'a' is already defined.",
+          type: AST_NODE_TYPES.Identifier
+        } as any
+      ]
     },
     {
       code: 'var a; var {a = 0, b: Object = 0} = {};',
       options: [{ builtinGlobals: false }],
       parserOptions: { ecmaVersion: 6 },
-      errors: [{ message: "'a' is already defined.", type: 'Identifier' }]
+      errors: [
+        {
+          message: "'a' is already defined.",
+          type: AST_NODE_TYPES.Identifier
+        } as any
+      ]
     },
 
     // Notifications of readonly are moved from no-undef: https://github.com/eslint/eslint/issues/4504
     {
       code: '/*global b:false*/ var b = 1;',
       options: [{ builtinGlobals: true }],
-      errors: [{ message: "'b' is already defined.", type: 'Identifier' }]
+      errors: [
+        {
+          message: "'b' is already defined.",
+          type: AST_NODE_TYPES.Identifier
+        } as any
+      ]
     }
   ]
 });
