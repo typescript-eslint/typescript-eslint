430965375c4a90466cce256dfb74861609342cf6
Armano <armano2@users.noreply.github.com>
2019-02-12 22:40:59 +0100

    feat(parser): remove redundant tests fixtures

--- packages/parser/tests/fixtures/scope-analysis/parameter-property.ts
+++
@@ -1,8 +0,0 @@
-import { InjectRepository, Repository, User } from 'test';
-
-export default class Foo {
-  constructor(
-    @InjectRepository(User)
-    private readonly userRepository: Repository<User>,
-  ) {}
-}
