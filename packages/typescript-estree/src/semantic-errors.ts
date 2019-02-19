adb61e9c498046563db54f71773fe6891f5fe3e7
Armano <armano2@users.noreply.github.com>
2019-02-19 22:54:09 +0100

    docs(ts-estree): restore comment

--- packages/typescript-estree/src/semantic-errors.ts
+++ packages/typescript-estree/src/semantic-errors.ts
@@ -37,6 +37,12 @@ export function getFirstSemanticOrSyntacticError(
     /**
      * TypeScript compiler has certain Debug.fail() statements in, which will cause the diagnostics
      * retrieval above to throw.
+     *
+     * E.g. from ast-alignment-tests
+     * "Debug Failure. Shouldn't ever directly check a JsxOpeningElement"
+     *
+     * For our current use-cases this is undesired behavior, so we just suppress it
+     * and log a a warning.
      */
     /* istanbul ignore next */
     console.warn(`Warning From TSC: "${e.message}`);
