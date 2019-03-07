863654ee98b91ee06c322ae10cb3e0a33be56d2b
Ricky Lippmann <ricky.lippmann@gmail.com>
2019-03-07 15:07:19 +0100

    feat(eslint-plugin): add base config to generated configs

--- packages/eslint-plugin/tools/generate-configs.ts
+++ packages/eslint-plugin/tools/generate-configs.ts
@@ -3,21 +3,32 @@
 import path from 'path';
 import fs from 'fs';
 import rules from '../src/rules';
+import RuleModule from 'ts-eslint';
+import { Linter } from 'eslint';
 
 const RULE_NAME_PREFIX = '@typescript-eslint/';
-const MAX_RULE_NAME_LENGTH = 33 + RULE_NAME_PREFIX.length;
+const MAX_RULE_NAME_LENGTH = 32 + RULE_NAME_PREFIX.length;
 
 const ruleEntries = Object.entries(rules);
+
+interface LinterConfigRules {
+  [name: string]: Linter.RuleLevel;
+}
+
+interface LinterConfig extends Linter.Config {
+  extends?: string | string[];
+  plugins?: string[];
+}
 
 /**
  * Helper function reduces records to key - value pairs.
  * @param config
  * @param entry
  */
-const reducer = (
-  config: Record<string, string>,
-  entry: Record<string, any>,
-): Record<string, string> => {
+const reducer = <TMessageIds extends string>(
+  config: LinterConfigRules,
+  entry: [string, RuleModule<TMessageIds, any, any>],
+) => {
   const key = entry[0];
   const value = entry[1];
   const ruleName = `${RULE_NAME_PREFIX}${key}`;
@@ -31,24 +42,43 @@ const reducer = (
 };
 
 /**
- * Helper function generates configuration.
+ * Helper function writes configuration.
  */
-function generate(rules: Record<string, string>, filePath: string): void {
-  const config = Object.assign({ extends: './configs/base.json' }, { rules });
-
+function writeConfig(config: LinterConfig, filePath: string): void {
   fs.writeFileSync(filePath, `${JSON.stringify(config, null, 2)}\n`);
 }
 
-console.log('------------------------ all.json ------------------------');
-generate(
-  ruleEntries.reduce(reducer, {}),
-  path.resolve(__dirname, '../src/configs/all.json'),
-);
+const baseConfig: LinterConfig = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    camelcase: 'off',
+    indent: 'off',
+    'no-array-constructor': 'off',
+    'no-unused-vars': 'off',
+    'no-useless-constructor': 'off',
+  },
+};
+writeConfig(baseConfig, path.resolve(__dirname, '../src/configs/base.json'));
 
-console.log('-------------------- recommended.json --------------------');
-generate(
-  ruleEntries
+console.log('------------------------- all.json -------------------------');
+const allConfig: LinterConfig = {
+  extends: './configs/base.json',
+  rules: ruleEntries.reduce(reducer, {}) as LinterConfigRules,
+};
+writeConfig(allConfig, path.resolve(__dirname, '../src/configs/all.json'));
+
+console.log('--------------------- recommended.json ---------------------');
+const recommendedConfig: LinterConfig = {
+  extends: './configs/base.json',
+  rules: ruleEntries
     .filter(entry => !!entry[1].meta.docs.recommended)
-    .reduce(reducer, {}),
+    .reduce(reducer, {}) as LinterConfigRules,
+};
+writeConfig(
+  recommendedConfig,
   path.resolve(__dirname, '../src/configs/recommended.json'),
 );
