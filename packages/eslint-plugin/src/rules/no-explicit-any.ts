78e96b6fe1d034b2a8a68c620b686dff23a94cca
Brad Zacher <brad.zacher@gmail.com>
2019-06-20 16:33:36 -0700

    fix missing type

--- packages/eslint-plugin/src/rules/no-explicit-any.ts
+++ packages/eslint-plugin/src/rules/no-explicit-any.ts
@@ -8,6 +8,7 @@ import { TSESLint } from '@typescript-eslint/experimental-utils';
 type Options = [
   {
     fixToUnknown: boolean;
+    ignoreRestArgs: boolean;
   }
 ];
 type MessageIds = 'unexpectedAny';
