28a131dfe1e785e791deda04ff99cd46ec0594e4
Jeff Principe <princjef@gmail.com>
2019-07-16 07:48:31 -0700

    feat(eslint-plugin): add new rule no-misused-promises (#612)
    * feat(eslint-plugin): add new rule no-misused-promises
    
    * chore: review feedback
    
    * chore: regenerate plugin configs

---
+++ packages/eslint-plugin/src/rules/no-misused-promises.ts
@@ -0,0 +1,253 @@
+import { TSESLint, TSESTree } from '@typescript-eslint/experimental-utils';
+import * as tsutils from 'tsutils';
+import ts from 'typescript';
+
+import * as util from '../util';
+
+type Options = [
+  {
+    checksConditionals?: boolean;
+    checksVoidReturn?: boolean;
+  }
+];
+
+export default util.createRule<Options, 'conditional' | 'voidReturn'>({
+  name: 'no-misused-promises',
+  meta: {
+    docs: {
+      description: 'Avoid using promises in places not designed to handle them',
+      category: 'Best Practices',
+      recommended: false,
+    },
+    messages: {
+      voidReturn:
+        'Promise returned in function argument where a void return was expected.',
+      conditional: 'Expected non-Promise value in a boolean conditional.',
+    },
+    schema: [
+      {
+        type: 'object',
+        properties: {
+          checksConditionals: {
+            type: 'boolean',
+          },
+          checksVoidReturn: {
+            type: 'boolean',
+          },
+        },
+      },
+    ],
+    type: 'problem',
+  },
+  defaultOptions: [
+    {
+      checksConditionals: true,
+      checksVoidReturn: true,
+    },
+  ],
+
+  create(context, [{ checksConditionals, checksVoidReturn }]) {
+    const parserServices = util.getParserServices(context);
+    const checker = parserServices.program.getTypeChecker();
+
+    const conditionalChecks: TSESLint.RuleListener = {
+      ConditionalExpression: checkTestConditional,
+      DoWhileStatement: checkTestConditional,
+      ForStatement: checkTestConditional,
+      IfStatement: checkTestConditional,
+      LogicalExpression(node) {
+        // We only check the lhs of a logical expression because the rhs might
+        // be the return value of a short circuit expression.
+        checkConditional(node.left);
+      },
+      UnaryExpression(node) {
+        if (node.operator === '!') {
+          checkConditional(node.argument);
+        }
+      },
+      WhileStatement: checkTestConditional,
+    };
+
+    const voidReturnChecks: TSESLint.RuleListener = {
+      CallExpression: checkArguments,
+      NewExpression: checkArguments,
+    };
+
+    function checkTestConditional(node: { test: TSESTree.Expression | null }) {
+      if (node.test) {
+        checkConditional(node.test);
+      }
+    }
+
+    function checkConditional(node: TSESTree.Expression) {
+      const tsNode = parserServices.esTreeNodeToTSNodeMap.get(node);
+      if (isAlwaysThenable(checker, tsNode)) {
+        context.report({
+          messageId: 'conditional',
+          node,
+        });
+      }
+    }
+
+    function checkArguments(
+      node: TSESTree.CallExpression | TSESTree.NewExpression,
+    ) {
+      const tsNode = parserServices.esTreeNodeToTSNodeMap.get<
+        ts.CallExpression | ts.NewExpression
+      >(node);
+      const voidParams = voidFunctionParams(checker, tsNode);
+      if (voidParams.size === 0) {
+        return;
+      }
+
+      for (const [index, argument] of node.arguments.entries()) {
+        if (!voidParams.has(index)) {
+          continue;
+        }
+
+        const tsNode = parserServices.esTreeNodeToTSNodeMap.get(argument);
+        if (returnsThenable(checker, tsNode as ts.Expression)) {
+          context.report({
+            messageId: 'voidReturn',
+            node: argument,
+          });
+        }
+      }
+    }
+
+    return {
+      ...(checksConditionals ? conditionalChecks : {}),
+      ...(checksVoidReturn ? voidReturnChecks : {}),
+    };
+  },
+});
+
+// Variation on the thenable check which requires all forms of the type (read:
+// alternates in a union) to be thenable. Otherwise, you might be trying to
+// check if something is defined or undefined and get caught because one of the
+// branches is thenable.
+function isAlwaysThenable(checker: ts.TypeChecker, node: ts.Node) {
+  const type = checker.getTypeAtLocation(node);
+
+  for (const subType of tsutils.unionTypeParts(checker.getApparentType(type))) {
+    const thenProp = subType.getProperty('then');
+
+    // If one of the alternates has no then property, it is not thenable in all
+    // cases.
+    if (thenProp === undefined) {
+      return false;
+    }
+
+    // We walk through each variation of the then property. Since we know it
+    // exists at this point, we just need at least one of the alternates to
+    // be of the right form to consider it thenable.
+    const thenType = checker.getTypeOfSymbolAtLocation(thenProp, node);
+    let hasThenableSignature = false;
+    for (const subType of tsutils.unionTypeParts(thenType)) {
+      for (const signature of subType.getCallSignatures()) {
+        if (
+          signature.parameters.length !== 0 &&
+          isFunctionParam(checker, signature.parameters[0], node)
+        ) {
+          hasThenableSignature = true;
+          break;
+        }
+      }
+
+      // We only need to find one variant of the then property that has a
+      // function signature for it to be thenable.
+      if (hasThenableSignature) {
+        break;
+      }
+    }
+
+    // If no flavors of the then property are thenable, we don't consider the
+    // overall type to be thenable
+    if (!hasThenableSignature) {
+      return false;
+    }
+  }
+
+  // If all variants are considered thenable (i.e. haven't returned false), we
+  // consider the overall type thenable
+  return true;
+}
+
+function isFunctionParam(
+  checker: ts.TypeChecker,
+  param: ts.Symbol,
+  node: ts.Node,
+): boolean {
+  const type: ts.Type | undefined = checker.getApparentType(
+    checker.getTypeOfSymbolAtLocation(param, node),
+  );
+  for (const subType of tsutils.unionTypeParts(type)) {
+    if (subType.getCallSignatures().length !== 0) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// Get the positions of parameters which are void functions (and not also
+// thenable functions). These are the candidates for the void-return check at
+// the current call site.
+function voidFunctionParams(
+  checker: ts.TypeChecker,
+  node: ts.CallExpression | ts.NewExpression,
+) {
+  const voidReturnIndices = new Set<number>();
+  const thenableReturnIndices = new Set<number>();
+  const type = checker.getTypeAtLocation(node.expression);
+
+  for (const subType of tsutils.unionTypeParts(type)) {
+    // Standard function calls and `new` have two different types of signatures
+    const signatures = ts.isCallExpression(node)
+      ? subType.getCallSignatures()
+      : subType.getConstructSignatures();
+    for (const signature of signatures) {
+      for (const [index, parameter] of signature.parameters.entries()) {
+        const type = checker.getTypeOfSymbolAtLocation(
+          parameter,
+          node.expression,
+        );
+        for (const subType of tsutils.unionTypeParts(type)) {
+          for (const signature of subType.getCallSignatures()) {
+            const returnType = signature.getReturnType();
+            if (tsutils.isTypeFlagSet(returnType, ts.TypeFlags.Void)) {
+              voidReturnIndices.add(index);
+            } else if (
+              tsutils.isThenableType(checker, node.expression, returnType)
+            ) {
+              thenableReturnIndices.add(index);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  // If a certain positional argument accepts both thenable and void returns,
+  // a promise-returning function is valid
+  for (const thenable of thenableReturnIndices) {
+    voidReturnIndices.delete(thenable);
+  }
+
+  return voidReturnIndices;
+}
+
+// Returns true if the expression is a function that returns a thenable
+function returnsThenable(checker: ts.TypeChecker, node: ts.Expression) {
+  const type = checker.getApparentType(checker.getTypeAtLocation(node));
+
+  for (const subType of tsutils.unionTypeParts(type)) {
+    for (const signature of subType.getCallSignatures()) {
+      const returnType = signature.getReturnType();
+      if (tsutils.isThenableType(checker, node, returnType)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
---
+++ packages/eslint-plugin/tests/rules/no-misused-promises.test.ts
@@ -0,0 +1,282 @@
+import rule from '../../src/rules/no-misused-promises';
+import { RuleTester, getFixturesRootDir } from '../RuleTester';
+
+const rootDir = getFixturesRootDir();
+
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 2018,
+    tsconfigRootDir: rootDir,
+    project: './tsconfig.json',
+  },
+  parser: '@typescript-eslint/parser',
+});
+
+ruleTester.run('no-misused-promises', rule, {
+  valid: [
+    `if (true) {}`,
+    {
+      code: `if (Promise.resolve()) {}`,
+      options: [{ checksConditionals: false }],
+    },
+    `
+if (true) {}
+else if (false) {}
+else {}
+`,
+    {
+      code: `
+if (Promise.resolve()) {}
+else if (Promise.resolve()) {}
+else {}
+`,
+      options: [{ checksConditionals: false }],
+    },
+    `for (;;) {}`,
+    `for (let i; i < 10; i++) {}`,
+    {
+      code: `for (let i; Promise.resolve(); i++) {}`,
+      options: [{ checksConditionals: false }],
+    },
+    `do {} while (true);`,
+    {
+      code: `do {} while (Promise.resolve())`,
+      options: [{ checksConditionals: false }],
+    },
+    `while (true) {}`,
+    {
+      code: `while (Promise.resolve()) {}`,
+      options: [{ checksConditionals: false }],
+    },
+    `true ? 123 : 456`,
+    {
+      code: `Promise.resolve() ? 123 : 456`,
+      options: [{ checksConditionals: false }],
+    },
+    `if (!true) {}`,
+    {
+      code: `if (!Promise.resolve()) {}`,
+      options: [{ checksConditionals: false }],
+    },
+    `(await Promise.resolve()) || false`,
+    {
+      code: `Promise.resolve() || false`,
+      options: [{ checksConditionals: false }],
+    },
+    `(true && await Promise.resolve()) || false`,
+    {
+      code: `(true && Promise.resolve()) || false`,
+      options: [{ checksConditionals: false }],
+    },
+    `false || (true && Promise.resolve())`,
+    `
+async function test() {
+  if (await Promise.resolve()) {}
+}
+`,
+    `
+async function test() {
+  const mixed: Promise | undefined = Promise.resolve();
+  if (mixed) {
+    await mixed;
+  }
+}
+`,
+    `if (~Promise.resolve()) {}`,
+    `
+interface NotQuiteThenable {
+  then(param: string): void;
+  then(): void;
+}
+const value: NotQuiteThenable = { then() {} };
+if (value) {}
+`,
+    `[1, 2, 3].forEach(val => {});`,
+    {
+      code: `[1, 2, 3].forEach(async val => {});`,
+      options: [{ checksVoidReturn: false }],
+    },
+    `new Promise((resolve, reject) => resolve());`,
+    {
+      code: `new Promise(async (resolve, reject) => resolve());`,
+      options: [{ checksVoidReturn: false }],
+    },
+    `Promise.all(['abc', 'def'].map(async val => { await val; }))`,
+    `
+const fn: (arg: () => Promise<void> | void) => void = () => {};
+fn(() => Promise.resolve());
+`,
+  ],
+
+  invalid: [
+    {
+      code: `if (Promise.resolve()) {}`,
+      errors: [
+        {
+          line: 1,
+          messageId: 'conditional',
+        },
+      ],
+    },
+    {
+      code: `
+if (Promise.resolve()) {}
+else if (Promise.resolve()) {}
+else {}
+`,
+      errors: [
+        {
+          line: 2,
+          messageId: 'conditional',
+        },
+        {
+          line: 3,
+          messageId: 'conditional',
+        },
+      ],
+    },
+    {
+      code: `for (let i; Promise.resolve(); i++) {}`,
+      errors: [
+        {
+          line: 1,
+          messageId: 'conditional',
+        },
+      ],
+    },
+    {
+      code: `do {} while (Promise.resolve())`,
+      errors: [
+        {
+          line: 1,
+          messageId: 'conditional',
+        },
+      ],
+    },
+    {
+      code: `while (Promise.resolve()) {}`,
+      errors: [
+        {
+          line: 1,
+          messageId: 'conditional',
+        },
+      ],
+    },
+    {
+      code: `Promise.resolve() ? 123 : 456`,
+      errors: [
+        {
+          line: 1,
+          messageId: 'conditional',
+        },
+      ],
+    },
+    {
+      code: `if (!Promise.resolve()) {}`,
+      errors: [
+        {
+          line: 1,
+          messageId: 'conditional',
+        },
+      ],
+    },
+    {
+      code: `Promise.resolve() || false`,
+      errors: [
+        {
+          line: 1,
+          messageId: 'conditional',
+        },
+      ],
+    },
+    {
+      code: `(true && Promise.resolve()) || false`,
+      errors: [
+        {
+          line: 1,
+          messageId: 'conditional',
+        },
+      ],
+    },
+    {
+      code: `
+[Promise.resolve(), Promise.reject()].forEach(
+  async val => { await val; }
+);
+`,
+      errors: [
+        {
+          line: 3,
+          messageId: 'voidReturn',
+        },
+      ],
+    },
+    {
+      code: `
+new Promise(async (resolve, reject) => {
+  await Promise.resolve();
+  resolve();
+});
+`,
+      errors: [
+        {
+          line: 2,
+          messageId: 'voidReturn',
+        },
+      ],
+    },
+    {
+      code: `
+const fnWithCallback = (arg: string, cb: (err: any, res: string) => void) => {
+  cb(null, arg);
+};
+
+fnWithCallback('val', async (err, res) => {
+  await res;
+});
+`,
+      errors: [
+        {
+          line: 6,
+          messageId: 'voidReturn',
+        },
+      ],
+    },
+    {
+      code: `
+const fnWithCallback = (arg: string, cb: (err: any, res: string) => void) => {
+  cb(null, arg);
+};
+
+fnWithCallback('val', (err, res) => Promise.resolve(res));
+`,
+      errors: [
+        {
+          line: 6,
+          messageId: 'voidReturn',
+        },
+      ],
+    },
+    {
+      code: `
+const fnWithCallback = (arg: string, cb: (err: any, res: string) => void) => {
+  cb(null, arg);
+};
+
+fnWithCallback('val', (err, res) => {
+  if (err) {
+    return 'abc';
+  } else {
+    return Promise.resolve(res);
+  }
+});
+`,
+      errors: [
+        {
+          line: 6,
+          messageId: 'voidReturn',
+        },
+      ],
+    },
+  ],
+});
