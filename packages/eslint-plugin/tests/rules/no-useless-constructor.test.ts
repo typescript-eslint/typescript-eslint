55e788c818d022f6b4cd9e00ef69882d78daaa21
Brad Zacher <brad.zacher@gmail.com>
2019-07-24 17:34:25 -0700

    fix(eslint-plugin): [no-useless-constructor] handle bodyless constructor (#685)

--- packages/eslint-plugin/tests/rules/no-useless-constructor.test.ts
+++ packages/eslint-plugin/tests/rules/no-useless-constructor.test.ts
@@ -50,6 +50,8 @@ ruleTester.run('no-useless-constructor', rule, {
     'class A extends B { private constructor(foo, bar) { super(bar); } }',
     'class A extends B { public constructor(foo){ super(foo); } }',
     'class A extends B { public constructor(foo){} }',
+    // type definition / overload
+    'class A { constructor(foo); }',
   ],
   invalid: [
     {
