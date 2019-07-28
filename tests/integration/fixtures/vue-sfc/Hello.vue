5f03ab1fc91d055ae3976d0a8a3a9a98b08e9fe2
James Henry <james@henry.sc>
2019-07-28 15:20:02 -0400

    test: ensure integration tests can fail, add vue-sfc

---
+++ tests/integration/fixtures/vue-sfc/Hello.vue
@@ -0,0 +1,36 @@
+<!-- Hello.vue -->
+<template>
+    <div>
+        <div>
+		    <!-- !!!!! expected error !!!!! -->
+			Hello {{ (name as any) }}{{exclamationMarks}}
+		</div>
+        <button @click="decrement">-</button>
+        <button @click="increment">+</button>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+    props: ['name', 'initialEnthusiasm'],
+    data() {
+        return {
+            enthusiasm: this.initialEnthusiasm,
+        }
+    },
+    methods: {
+        increment() { this.enthusiasm++; },
+        decrement() {
+            if (this.enthusiasm > 1) {
+                this.enthusiasm--;
+            }
+        },
+    },
+    computed: {
+        exclamationMarks(): any { // !!!!! expected error !!!!!
+            return Array(this.enthusiasm + 1).join('!');
+        }
+    }
+});
+</script>
