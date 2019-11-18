64412ee8a4c3d5de6cfa79d6a5fecebf16b4d217
Brad Zacher <brad.zacher@gmail.com>
2019-11-18 09:17:15 -0800

    fix: lint

--- packages/eslint-plugin/src/rules/restrict-template-expressions.ts
+++ packages/eslint-plugin/src/rules/restrict-template-expressions.ts
@@ -70,7 +70,7 @@ export default util.createRule<Options, MessageId>({
     }
 
     return {
-      TemplateLiteral(node: TSESTree.TemplateLiteral) {
+      TemplateLiteral(node: TSESTree.TemplateLiteral): void {
         // don't check tagged template literals
         if (node.parent!.type === AST_NODE_TYPES.TaggedTemplateExpression) {
           return;
