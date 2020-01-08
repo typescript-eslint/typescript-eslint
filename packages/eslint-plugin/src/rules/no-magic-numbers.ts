71925c7768a496b39d0d7c174ebab336c316e555
Armano <armano2@users.noreply.github.com>
2020-01-08 23:40:51 +0100

    chore(eslint-plugin): unnecessary parents

--- packages/eslint-plugin/src/rules/no-magic-numbers.ts
+++ packages/eslint-plugin/src/rules/no-magic-numbers.ts
@@ -86,7 +86,7 @@ export default util.createRule<Options, MessageIds>({
           let raw = node.raw;
 
           if (
-            node.parent?.type === AST_NODE_TYPES.UnaryExpression &&
+            node.parent.type === AST_NODE_TYPES.UnaryExpression &&
             // the base rule only shows the operator for negative numbers
             // https://github.com/eslint/eslint/blob/9dfc8501fb1956c90dc11e6377b4cb38a6bea65d/lib/rules/no-magic-numbers.js#L126
             node.parent.operator === '-'
@@ -114,9 +114,9 @@ export default util.createRule<Options, MessageIds>({
 /**
  * Gets the true parent of the literal, handling prefixed numbers (-1 / +1)
  */
-function getLiteralParent(node: TSESTree.Literal): TSESTree.Node | undefined {
+function getLiteralParent(node: TSESTree.Literal): TSESTree.Node {
   if (
-    node.parent?.type === AST_NODE_TYPES.UnaryExpression &&
+    node.parent.type === AST_NODE_TYPES.UnaryExpression &&
     ['-', '+'].includes(node.parent.operator)
   ) {
     return node.parent.parent;
