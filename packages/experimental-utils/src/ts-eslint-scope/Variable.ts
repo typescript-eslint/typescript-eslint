59d2dfae635b9a752bf5adef876b087c202ceac5
Armano <armano2@users.noreply.github.com>
2020-01-21 01:05:48 +0100

    test(experimental-utils): correct eslint scope types

--- packages/experimental-utils/src/ts-eslint-scope/Variable.ts
+++ packages/experimental-utils/src/ts-eslint-scope/Variable.ts
@@ -2,6 +2,7 @@ import { TSESTree } from '@typescript-eslint/typescript-estree';
 import ESLintVariable from 'eslint-scope/lib/variable';
 import { Reference } from './Reference';
 import { Definition } from './Definition';
+import { Scope } from './Scope';
 
 interface Variable {
   name: string;
@@ -9,6 +10,9 @@ interface Variable {
   references: Reference[];
   defs: Definition[];
   eslintUsed?: boolean;
+  stack?: unknown;
+  tainted?: boolean;
+  scope?: Scope;
 }
 
 const Variable = ESLintVariable as {
