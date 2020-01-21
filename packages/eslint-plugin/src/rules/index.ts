420db96921435e8bf7fb484ae74552a912a6adde
Armano <armano2@users.noreply.github.com>
2020-01-21 04:03:31 +0100

    feat(eslint-plugin): add new rule prefer-as-const (#1431)

--- packages/eslint-plugin/src/rules/index.ts
+++ packages/eslint-plugin/src/rules/index.ts
@@ -56,6 +56,7 @@ import noUnusedVarsExperimental from './no-unused-vars-experimental';
 import noUseBeforeDefine from './no-use-before-define';
 import noUselessConstructor from './no-useless-constructor';
 import noVarRequires from './no-var-requires';
+import preferAsConst from './prefer-as-const';
 import preferForOf from './prefer-for-of';
 import preferFunctionType from './prefer-function-type';
 import preferIncludes from './prefer-includes';
@@ -140,6 +141,7 @@ export default {
   'no-use-before-define': noUseBeforeDefine,
   'no-useless-constructor': noUselessConstructor,
   'no-var-requires': noVarRequires,
+  'prefer-as-const': preferAsConst,
   'prefer-for-of': preferForOf,
   'prefer-function-type': preferFunctionType,
   'prefer-includes': preferIncludes,
