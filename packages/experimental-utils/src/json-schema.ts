f9bc9aa4c672007fcd61e95be72b09a4b2d43b44
Brad Zacher <brad.zacher@gmail.com>
2020-01-30 17:31:36 -0800

    fix: lint

--- packages/experimental-utils/src/json-schema.ts
+++ packages/experimental-utils/src/json-schema.ts
@@ -1,1 +1,8 @@
+/*
+This is a types-only package, so there's no actual json-schema package
+
+This is the reason that we export it from here - so consumers don't need to do
+this weird pseudo-import to have type-checked JSON schemas.
+*/
+// eslint-disable-next-line import/no-extraneous-dependencies
 export * from 'json-schema';
