fec9656ce69909f51a9a495038d4e33cc7cd4970
Brad Zacher <brad.zacher@gmail.com>
2020-03-03 23:03:49 -0800

    docs: update experimental-utils docs

--- packages/experimental-utils/src/index.ts
+++ packages/experimental-utils/src/index.ts
@@ -1,8 +1,8 @@
 import * as ASTUtils from './ast-utils';
 import * as ESLintUtils from './eslint-utils';
+import * as JSONSchema from './json-schema';
 import * as TSESLint from './ts-eslint';
 import * as TSESLintScope from './ts-eslint-scope';
-import * as JSONSchema from './json-schema';
 
 export { ASTUtils, ESLintUtils, JSONSchema, TSESLint, TSESLintScope };
 export * from './ts-estree';
