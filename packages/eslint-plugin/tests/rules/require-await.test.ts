2a33b47bc21e7db6f5b72c37ee0d21991f20d88a
Brad Zacher <brad.zacher@gmail.com>
2020-03-29 20:02:19 -0700

    chore: require-await

--- packages/eslint-plugin/tests/rules/require-await.test.ts
+++ packages/eslint-plugin/tests/rules/require-await.test.ts
@@ -15,131 +15,168 @@ const ruleTester = new RuleTester({
 ruleTester.run('require-await', rule, {
   valid: [
     // Non-async function declaration
-    `function numberOne(): number {
-          return 1;
-        }`,
+    `
+function numberOne(): number {
+  return 1;
+}
+    `,
     // Non-async function expression
-    `const numberOne = function(): number {
-          return 1;
-        }`,
+    `
+const numberOne = function(): number {
+  return 1;
+};
+    `,
     // Non-async arrow function expression (concise-body)
-    `const numberOne = (): number => 1;`,
+    `
+      const numberOne = (): number => 1;
+    `,
     // Non-async arrow function expression (block-body)
-    `const numberOne = (): number => {
-          return 1;
-        };`,
+    `
+const numberOne = (): number => {
+  return 1;
+};
+    `,
     // Non-async function that returns a promise
     // https://github.com/typescript-eslint/typescript-eslint/issues/1226
     `
-    function delay() {
-      return Promise.resolve();
-    }
-        `,
+function delay() {
+  return Promise.resolve();
+}
+    `,
     `
-    const delay = () => {
-      return Promise.resolve();
-    }
-        `,
-    `const delay = () => Promise.resolve();`,
+const delay = () => {
+  return Promise.resolve();
+};
+    `,
+    'const delay = () => Promise.resolve();',
     // Async function declaration with await
-    `async function numberOne(): Promise<number> {
-          return await 1;
-        }`,
+    `
+async function numberOne(): Promise<number> {
+  return await 1;
+}
+    `,
     // Async function expression with await
-    `const numberOne = async function(): Promise<number> {
-          return await 1;
-        }`,
+    `
+const numberOne = async function(): Promise<number> {
+  return await 1;
+};
+    `,
     // Async arrow function expression with await (concise-body)
-    `const numberOne = async (): Promise<number> => await 1;`,
+    'const numberOne = async (): Promise<number> => await 1;',
     // Async arrow function expression with await (block-body)
-    `const numberOne = async (): Promise<number> => {
-          return await 1;
-        };`,
+    `
+const numberOne = async (): Promise<number> => {
+  return await 1;
+};
+    `,
     // Async function declaration with promise return
-    `async function numberOne(): Promise<number> {
-          return Promise.resolve(1);
-        }`,
+    `
+async function numberOne(): Promise<number> {
+  return Promise.resolve(1);
+}
+    `,
     // Async function expression with promise return
-    `const numberOne = async function(): Promise<number> {
-          return Promise.resolve(1);
-        }`,
+    `
+const numberOne = async function(): Promise<number> {
+  return Promise.resolve(1);
+};
+    `,
     // Async arrow function with promise return (concise-body)
-    `const numberOne = async (): Promise<number> => Promise.resolve(1);`,
+    'const numberOne = async (): Promise<number> => Promise.resolve(1);',
     // Async arrow function with promise return (block-body)
-    `const numberOne = async (): Promise<number> => {
-          return Promise.resolve(1);
-        };`,
+    `
+const numberOne = async (): Promise<number> => {
+  return Promise.resolve(1);
+};
+    `,
     // Async function declaration with async function return
-    `async function numberOne(): Promise<number> {
-          return getAsyncNumber(1);
-        }
-        async function getAsyncNumber(x: number): Promise<number> {
-          return Promise.resolve(x);
-        }`,
+    `
+async function numberOne(): Promise<number> {
+  return getAsyncNumber(1);
+}
+async function getAsyncNumber(x: number): Promise<number> {
+  return Promise.resolve(x);
+}
+    `,
     // Async function expression with async function return
-    `const numberOne = async function(): Promise<number> {
-          return getAsyncNumber(1);
-        }
-        const getAsyncNumber = async function(x: number): Promise<number> {
-          return Promise.resolve(x);
-        }`,
+    `
+const numberOne = async function(): Promise<number> {
+  return getAsyncNumber(1);
+};
+const getAsyncNumber = async function(x: number): Promise<number> {
+  return Promise.resolve(x);
+};
+    `,
     // Async arrow function with async function return (concise-body)
-    `const numberOne = async (): Promise<number> => getAsyncNumber(1);
-        const getAsyncNumber = async function(x: number): Promise<number> {
-          return Promise.resolve(x);
-        }`,
+    `
+const numberOne = async (): Promise<number> => getAsyncNumber(1);
+const getAsyncNumber = async function(x: number): Promise<number> {
+  return Promise.resolve(x);
+};
+    `,
     // Async arrow function with async function return (block-body)
-    `const numberOne = async (): Promise<number> => {
-          return getAsyncNumber(1);
-        };
-        const getAsyncNumber = async function(x: number): Promise<number> {
-          return Promise.resolve(x);
-        }`,
+    `
+const numberOne = async (): Promise<number> => {
+  return getAsyncNumber(1);
+};
+const getAsyncNumber = async function(x: number): Promise<number> {
+  return Promise.resolve(x);
+};
+    `,
     // https://github.com/typescript-eslint/typescript-eslint/issues/1188
     `
-    async function testFunction(): Promise<void> {
-      await Promise.all([1, 2, 3].map(
-        // this should not trigger an error on the parent function
-        async value => Promise.resolve(value)
-      ))
-    }
-        `,
-    `
-        async function* run() {
-          await new Promise(resolve => setTimeout(resolve, 100));
-          yield 'Hello';
-          console.log('World');
-        }
-        `,
-    'async function* run() { }',
-    'async function* asyncGenerator() { await Promise.resolve(); yield 1 }',
-    'function* test6() { yield* syncGenerator() }',
-    'function* test8() { yield syncGenerator() }',
-    'function* syncGenerator() { yield 1 }',
-    `
-    async function* asyncGenerator() {
-      await Promise.resolve()
-      yield 1
-    }
-    async function* test1() {yield* asyncGenerator() }
+async function testFunction(): Promise<void> {
+  await Promise.all(
+    [1, 2, 3].map(
+      // this should not trigger an error on the parent function
+      async value => Promise.resolve(value),
+    ),
+  );
+}
     `,
-    `async function* foo() {
-            await Promise.resolve()
-            yield 1
-          }
-          async function* bar() {
-            yield* foo()
-          }`,
-    'const foo : () => void = async function *(){}',
-    'async function* foo() : Promise<string> { return new Promise((res) => res(`hello`)) }',
+    'async function* run() {}',
+    `
+async function* run() {
+  await new Promise(resolve => setTimeout(resolve, 100));
+  yield 'Hello';
+  console.log('World');
+}
+    `,
+    `
+function* test6() {
+  yield* syncGenerator();
+}
+    `,
+    `
+function* syncGenerator() {
+  yield 1;
+}
+    `,
+    `
+async function* asyncGenerator() {
+  await Promise.resolve();
+  yield 1;
+}
+async function* test1() {
+  yield* asyncGenerator();
+}
+    `,
+    'const foo: () => void = async function*() {};',
+    `
+async function* foo(): Promise<string> {
+  return new Promise(res => res(\`hello\`));
+}
+    `,
   ],
 
   invalid: [
     {
       // Async function declaration with no await
-      code: `async function numberOne(): Promise<number> {
-            return 1;
-          }`,
+      code: `
+async function numberOne(): Promise<number> {
+  return 1;
+}
+      `,
       errors: [
         {
           messageId: 'missingAwait',
@@ -151,9 +188,11 @@ ruleTester.run('require-await', rule, {
     },
     {
       // Async function expression with no await
-      code: `const numberOne = async function(): Promise<number> {
-            return 1;
-          }`,
+      code: `
+const numberOne = async function(): Promise<number> {
+  return 1;
+};
+      `,
       errors: [
         {
           messageId: 'missingAwait',
@@ -165,7 +204,7 @@ ruleTester.run('require-await', rule, {
     },
     {
       // Async arrow function expression with no await
-      code: `const numberOne = async (): Promise<number> => 1;`,
+      code: 'const numberOne = async (): Promise<number> => 1;',
       errors: [
         {
           messageId: 'missingAwait',
@@ -177,8 +216,13 @@ ruleTester.run('require-await', rule, {
     },
     {
       // non-async function with await inside async function without await
-      code:
-        'async function foo() { function nested() { await doSomething() } }',
+      code: `
+        async function foo() {
+          function nested() {
+            await doSomething();
+          }
+        }
+      `,
       errors: [
         {
           messageId: 'missingAwait',
@@ -189,7 +233,11 @@ ruleTester.run('require-await', rule, {
       ],
     },
     {
-      code: 'async function* foo() : void { doSomething() }',
+      code: `
+async function* foo(): void {
+  doSomething();
+}
+      `,
       errors: [
         {
           messageId: 'missingAwait',
@@ -200,7 +248,11 @@ ruleTester.run('require-await', rule, {
       ],
     },
     {
-      code: 'async function* foo() { yield 1 }',
+      code: `
+async function* foo() {
+  yield 1;
+}
+      `,
       errors: [
         {
           messageId: 'missingAwait',
@@ -211,18 +263,11 @@ ruleTester.run('require-await', rule, {
       ],
     },
     {
-      code: 'async function* run() { console.log("bar") }',
-      errors: [
-        {
-          messageId: 'missingAwait',
-          data: {
-            name: "Async generator function 'run'",
-          },
-        },
-      ],
-    },
-    {
-      code: 'const foo = async function *(){ console.log("bar") }',
+      code: `
+const foo = async function*() {
+  console.log('bar');
+};
+      `,
       errors: [
         {
           messageId: 'missingAwait',
@@ -233,34 +278,34 @@ ruleTester.run('require-await', rule, {
       ],
     },
     {
-      code: 'async function* syncGenerator() { yield 1 }',
+      code: `
+async function* asyncGenerator() {
+  yield 1;
+}
+      `,
       errors: [
         {
           messageId: 'missingAwait',
           data: {
-            name: "Async generator function 'syncGenerator'",
+            name: "Async generator function 'asyncGenerator'",
           },
         },
       ],
     },
     {
-      code: 'async function* test3() { yield asyncGenerator() }',
+      code: `
+function* syncGenerator() {
+  yield 1;
+}
+async function* asyncGenerator() {
+  yield* syncGenerator();
+}
+      `,
       errors: [
         {
           messageId: 'missingAwait',
           data: {
-            name: "Async generator function 'test3'",
-          },
-        },
-      ],
-    },
-    {
-      code: 'async function* test7() { yield syncGenerator() }',
-      errors: [
-        {
-          messageId: 'missingAwait',
-          data: {
-            name: "Async generator function 'test7'",
+            name: "Async generator function 'asyncGenerator'",
           },
         },
       ],
@@ -271,36 +316,84 @@ ruleTester.run('require-await', rule, {
 // https://github.com/eslint/eslint/blob/03a69dbe86d5b5768a310105416ae726822e3c1c/tests/lib/rules/require-await.js#L25-L132
 ruleTester.run('require-await', rule, {
   valid: [
-    'async function foo() { await doSomething() }',
-    '(async function() { await doSomething() })',
-    'async () => { await doSomething() }',
-    'async () => await doSomething()',
-    '({ async foo() { await doSomething() } })',
-    'class A { async foo() { await doSomething() } }',
-    '(class { async foo() { await doSomething() } })',
-    'async function foo() { await (async () => { await doSomething() }) }',
+    `
+async function foo() {
+  await doSomething();
+}
+    `,
+    `
+(async function() {
+  await doSomething();
+});
+    `,
+    `
+async () => {
+  await doSomething();
+};
+    `,
+    'async () => await doSomething();',
+    `
+({
+  async foo() {
+    await doSomething();
+  },
+});
+    `,
+    `
+class A {
+  async foo() {
+    await doSomething();
+  }
+}
+    `,
+    `
+(class {
+  async foo() {
+    await doSomething();
+  }
+});
+    `,
+    `
+async function foo() {
+  await (async () => {
+    await doSomething();
+  });
+}
+    `,
     // empty functions are ok.
     'async function foo() {}',
-    'async () => {}',
+    'async () => {};',
     // normal functions are ok.
-    'function foo() { doSomething() }',
+    `
+function foo() {
+  doSomething();
+}
+    `,
     // for-await-of
-    'async function foo() { for await (x of xs); }',
+    `
+async function foo() {
+  for await (x of xs);
+}
+    `,
     // global await
     {
-      code: 'await foo()',
+      code: 'await foo();',
     },
     {
       code: `
-            for await (let num of asyncIterable) {
-                console.log(num);
-            }
-          `,
+for await (let num of asyncIterable) {
+  console.log(num);
+}
+      `,
     },
   ],
   invalid: [
     {
-      code: 'async function foo() { doSomething() }',
+      code: `
+        async function foo() {
+          doSomething();
+        }
+      `,
       errors: [
         {
           messageId: 'missingAwait',
@@ -309,7 +402,11 @@ ruleTester.run('require-await', rule, {
       ],
     },
     {
-      code: '(async function() { doSomething() })',
+      code: `
+        (async function() {
+          doSomething();
+        });
+      `,
       errors: [
         {
           messageId: 'missingAwait',
@@ -318,7 +415,11 @@ ruleTester.run('require-await', rule, {
       ],
     },
     {
-      code: 'async () => { doSomething() }',
+      code: `
+        async () => {
+          doSomething();
+        };
+      `,
       errors: [
         {
           messageId: 'missingAwait',
@@ -327,7 +428,7 @@ ruleTester.run('require-await', rule, {
       ],
     },
     {
-      code: 'async () => doSomething()',
+      code: 'async () => doSomething();',
       errors: [
         {
           messageId: 'missingAwait',
@@ -336,7 +437,13 @@ ruleTester.run('require-await', rule, {
       ],
     },
     {
-      code: '({ async foo() { doSomething() } })',
+      code: `
+        ({
+          async foo() {
+            doSomething();
+          },
+        });
+      `,
       errors: [
         {
           messageId: 'missingAwait',
@@ -345,7 +452,13 @@ ruleTester.run('require-await', rule, {
       ],
     },
     {
-      code: 'class A { async foo() { doSomething() } }',
+      code: `
+        class A {
+          async foo() {
+            doSomething();
+          }
+        }
+      `,
       errors: [
         {
           messageId: 'missingAwait',
@@ -354,7 +467,13 @@ ruleTester.run('require-await', rule, {
       ],
     },
     {
-      code: '(class { async foo() { doSomething() } })',
+      code: `
+        (class {
+          async foo() {
+            doSomething();
+          }
+        });
+      `,
       errors: [
         {
           messageId: 'missingAwait',
@@ -363,7 +482,13 @@ ruleTester.run('require-await', rule, {
       ],
     },
     {
-      code: "(class { async ''() { doSomething() } })",
+      code: `
+        (class {
+          async ''() {
+            doSomething();
+          }
+        });
+      `,
       errors: [
         {
           messageId: 'missingAwait',
@@ -372,7 +497,13 @@ ruleTester.run('require-await', rule, {
       ],
     },
     {
-      code: 'async function foo() { async () => { await doSomething() } }',
+      code: `
+        async function foo() {
+          async () => {
+            await doSomething();
+          };
+        }
+      `,
       errors: [
         {
           messageId: 'missingAwait',
@@ -381,29 +512,17 @@ ruleTester.run('require-await', rule, {
       ],
     },
     {
-      code: 'async function foo() { await (async () => { doSomething() }) }',
+      code: `
+        async function foo() {
+          await (async () => {
+            doSomething();
+          });
+        }
+      `,
       errors: [
         {
           messageId: 'missingAwait',
           data: { name: 'Async arrow function' },
-        },
-      ],
-    },
-    {
-      code: 'async function* run() { yield * anotherAsyncGenerator() }',
-      errors: [
-        {
-          messageId: 'missingAwait',
-          data: { name: "Async generator function 'run'" },
-        },
-      ],
-    },
-    {
-      code: 'async function* run() { yield* 1 }',
-      errors: [
-        {
-          messageId: 'missingAwait',
-          data: { name: "Async generator function 'run'" },
         },
       ],
     },
