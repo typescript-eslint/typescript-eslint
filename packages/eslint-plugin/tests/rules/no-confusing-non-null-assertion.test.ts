e32208f98f7d651b3e258aae6e196f83c7218fce
zhangciwu <conanla@qq.com>
2020-06-07 04:36:47 +0800

    Update packages/eslint-plugin/tests/rules/no-confusing-non-null-assertion.test.ts
    Co-authored-by: Brad Zacher <brad.zacher@gmail.com>

--- packages/eslint-plugin/tests/rules/no-confusing-non-null-assertion.test.ts
+++ packages/eslint-plugin/tests/rules/no-confusing-non-null-assertion.test.ts
@@ -1,3 +1,8 @@
+/* eslint-disable eslint-comments/no-use */
+// this rule enforces adding parens, which prettier will want to fix and break the tests
+/* eslint "@typescript-eslint/internal/plugin-test-formatting": ["error", { formatWithPrettier: false }] */
+/* eslint-enable eslint-comments/no-use */
+
 import rule from '../../src/rules/no-confusing-non-null-assertion';
 import { RuleTester } from '../RuleTester';
 
