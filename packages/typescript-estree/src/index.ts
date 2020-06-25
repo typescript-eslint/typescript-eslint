689dae37392d527c64ae83db2a4c3e6b7fecece7
Brad Zacher <brad.zacher@gmail.com>
2020-06-24 17:30:06 -0700

    feat: split visitor keys into their own package (#2230)

--- packages/typescript-estree/src/index.ts
+++ packages/typescript-estree/src/index.ts
@@ -6,9 +6,11 @@ export {
 } from './parser';
 export { ParserServices, TSESTreeOptions } from './parser-options';
 export { simpleTraverse } from './simple-traverse';
-export { visitorKeys } from './visitor-keys';
 export * from './ts-estree';
 export { clearCaches } from './create-program/createWatchProgram';
+
+// re-export for backwards-compat
+export { visitorKeys } from '@typescript-eslint/visitor-keys';
 
 // note - cannot migrate this to an import statement because it will make TSC copy the package.json to the dist folder
 export const version: string = require('../package.json').version;
