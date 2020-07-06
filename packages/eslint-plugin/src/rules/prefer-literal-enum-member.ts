112a47c559fc17fbe846451971928057c9117f6b
Brad Zacher <brad.zacher@gmail.com>
2020-07-06 10:57:54 -0700

    fix(eslint-plugin): [prefer-literal-enum-member] allow negative numbers

--- packages/eslint-plugin/src/rules/prefer-literal-enum-member.ts
+++ packages/eslint-plugin/src/rules/prefer-literal-enum-member.ts
@@ -1,7 +1,7 @@
 import { AST_NODE_TYPES } from '@typescript-eslint/experimental-utils';
 import { createRule } from '../util';
 
-export default createRule<[], 'notLiteral'>({
+export default createRule({
   name: 'prefer-literal-enum-member',
   meta: {
     type: 'suggestion',
@@ -22,15 +22,26 @@ export default createRule<[], 'notLiteral'>({
     return {
       TSEnumMember(node): void {
         // If there is no initializer, then this node is just the name of the member, so ignore.
-        if (
-          node.initializer != null &&
-          node.initializer.type !== AST_NODE_TYPES.Literal
-        ) {
-          context.report({
-            node: node.id,
-            messageId: 'notLiteral',
-          });
+        if (node.initializer == null) {
+          return;
         }
+        // any old literal
+        if (node.initializer.type === AST_NODE_TYPES.Literal) {
+          return;
+        }
+        // -1 and +1
+        if (
+          node.initializer.type === AST_NODE_TYPES.UnaryExpression &&
+          ['+', '-'].includes(node.initializer.operator) &&
+          node.initializer.argument.type === AST_NODE_TYPES.Literal
+        ) {
+          return;
+        }
+
+        context.report({
+          node: node.id,
+          messageId: 'notLiteral',
+        });
       },
     };
   },
