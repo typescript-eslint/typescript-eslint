a964e523879ff37a801ae54abd30886eadf6ac46
bob1983 <bob.1983.g@gmail.com>
2020-07-29 00:21:22 +0900

    feat(eslint-plugin): provide suggestion

--- packages/eslint-plugin/src/rules/prefer-enum-initializers.ts
+++ packages/eslint-plugin/src/rules/prefer-enum-initializers.ts
@@ -1,7 +1,11 @@
 import { TSESTree } from '@typescript-eslint/experimental-utils';
 import * as util from '../util';
+import { RuleFix } from '@typescript-eslint/experimental-utils/dist/ts-eslint';
 
-export default util.createRule({
+type Option = '0-based' | '1-based' | 'key-name';
+type MessageIds = 'defineInitializer' | 'defineInitializerSuggestion';
+
+export default util.createRule<Option[], MessageIds>({
   name: 'prefer-enum-initializers',
   meta: {
     type: 'suggestion',
@@ -9,33 +13,60 @@ export default util.createRule({
       description: 'Prefer initializing each enums member value',
       category: 'Best Practices',
       recommended: false,
+      suggestion: true,
     },
     messages: {
       defineInitializer:
         "The value of the member '{{ name }}' should be explicitly defined",
+      defineInitializerSuggestion:
+        'can be suggestion fixed to {{ name }} = {{ suggestedValue }}',
     },
-    schema: [],
+    schema: [
+      {
+        enum: ['0-based', '1-based', 'key-name'],
+      },
+    ],
   },
   defaultOptions: [],
   create(context) {
     const sourceCode = context.getSourceCode();
+    const config = context.options[0];
 
     return {
       TSEnumDeclaration(node: TSESTree.TSEnumDeclaration): void {
         const { members } = node;
-        const violatingMembers = members.filter(member => {
-          return member.initializer == null;
+        members.forEach((member, index) => {
+          if (member.initializer == null) {
+            const name = sourceCode.getText(member);
+            context.report({
+              node: member,
+              messageId: 'defineInitializer',
+              data: {
+                name,
+              },
+              suggest: [
+                {
+                  messageId: 'defineInitializerSuggestion',
+                  data: { name },
+                  fix: (fixer): RuleFix | null => {
+                    if (config === '0-based') {
+                      return fixer.replaceText(member, `${name} = ${index}`);
+                    } else if (config === '1-based') {
+                      return fixer.replaceText(
+                        member,
+                        `${name} = ${index + 1}`,
+                      );
+                    } else if (config === 'key-name') {
+                      return fixer.replaceText(member, `${name} = '${name}'`);
+                    } else {
+                      return null;
+                    }
+                  },
+                },
+              ],
+            });
+          }
         });
-
-        for (const member of violatingMembers) {
-          context.report({
-            node: member,
-            messageId: 'defineInitializer',
-            data: {
-              name: sourceCode.getText(member),
-            },
-          });
-        }
       },
     };
   },
