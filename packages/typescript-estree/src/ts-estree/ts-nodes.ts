9df80bd239328865d5db830dc99a91b894180bd4
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2020-08-19 13:36:15 -0400

    fix: add eslint ignore comment

--- packages/typescript-estree/src/ts-estree/ts-nodes.ts
+++ packages/typescript-estree/src/ts-estree/ts-nodes.ts
@@ -4,6 +4,7 @@ import * as ts from 'typescript';
 // https://github.com/typescript-eslint/typescript-eslint/issues/2388
 // to support both TypeScript 3.9 & 4:
 declare module 'typescript' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   export interface NamedTupleMember extends ts.Node {}
 }
 
