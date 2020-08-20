efd5f5bfe1913facc40d3126c7fed2efe0875116
Alexander T <alexander.tarasyuk@outlook.com>
2020-08-20 21:44:56 +0300

    fix(eslint-plugin): [explicit-module-boundary-types] ignore abstract setters

--- packages/eslint-plugin/tests/rules/explicit-module-boundary-types.test.ts
+++ packages/eslint-plugin/tests/rules/explicit-module-boundary-types.test.ts
@@ -646,6 +646,11 @@ type Buz = () => (n: number) => string;
 
 export const buz: Buz = () => n => String(n);
     `,
+    `
+export abstract class Foo<T> {
+  abstract set value(element: T);
+}
+    `,
   ],
   invalid: [
     {
