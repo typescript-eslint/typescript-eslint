c3a6c2aa7c8afaf07bb4d3eb5565943ea613fb61
Brad Zacher <brad.zacher@gmail.com>
2020-08-29 12:09:38 -0700

    chore: add downlevel-dts to all packages with type declarations

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,5 +1,9 @@
+// TODO(bradzacher) - convert this to export * as TSESTree from './ts-estree'
+// https://github.com/sandersn/downlevel-dts/pull/42
+import * as TSESTree from './ts-estree';
+
 export { AST_NODE_TYPES } from './ast-node-types';
 export { AST_TOKEN_TYPES } from './ast-token-types';
 export * from './lib';
 export * from './parser-options';
-export * as TSESTree from './ts-estree';
+export { TSESTree };
