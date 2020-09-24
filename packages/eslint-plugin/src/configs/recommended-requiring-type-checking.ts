9e6540c2052398143272a770785d0e5b65f859f6
lex(a) mourek <lexaurin@users.noreply.github.com>
2020-09-24 12:27:55 +0200

    Remove prefer-regexp-exec from recommended-requiring-type-checking
    As there is no proof that exec is faster - it then makes no sense to insist on this rule in recommended list.
    See discussion https://github.com/typescript-eslint/typescript-eslint/issues/2280

--- packages/eslint-plugin/src/configs/recommended-requiring-type-checking.ts
+++ packages/eslint-plugin/src/configs/recommended-requiring-type-checking.ts
@@ -15,7 +15,6 @@ export = {
     '@typescript-eslint/no-unsafe-call': 'error',
     '@typescript-eslint/no-unsafe-member-access': 'error',
     '@typescript-eslint/no-unsafe-return': 'error',
-    '@typescript-eslint/prefer-regexp-exec': 'error',
     'require-await': 'off',
     '@typescript-eslint/require-await': 'error',
     '@typescript-eslint/restrict-plus-operands': 'error',
