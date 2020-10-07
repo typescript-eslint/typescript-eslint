4b962348e1484fee4329d352f169799f7c8f747d
Govind <gvind4@gmail.com>
2020-10-07 23:46:17 +0530

    chore: support block comments for ban-ts-comment rule in eslint-plugin

--- packages/eslint-plugin/src/rules/ban-ts-comment.ts
+++ packages/eslint-plugin/src/rules/ban-ts-comment.ts
@@ -1,4 +1,7 @@
-import { AST_TOKEN_TYPES } from '@typescript-eslint/experimental-utils';
+import {
+  AST_TOKEN_TYPES,
+  TSESTree,
+} from '@typescript-eslint/experimental-utils';
 import * as util from '../util';
 
 interface Options {
@@ -21,15 +24,15 @@ export default util.createRule<[Options], MessageIds>({
     type: 'problem',
     docs: {
       description:
-        'Bans `// @ts-<directive>` comments from being used or requires descriptions after directive',
+        'Bans `@ts-<directive>` comments from being used or requires descriptions after directive',
       category: 'Best Practices',
       recommended: 'error',
     },
     messages: {
       tsDirectiveComment:
-        'Do not use "// @ts-{{directive}}" because it alters compilation errors.',
+        'Do not use "@ts-{{directive}}" because it alters compilation errors.',
       tsDirectiveCommentRequiresDescription:
-        'Include a description after the "// @ts-{{directive}}" directive to explain why the @ts-{{directive}} is necessary. The description must be {{minimumDescriptionLength}} characters or longer.',
+        'Include a description after the "@ts-{{directive}}" directive to explain why the @ts-{{directive}} is necessary. The description must be {{minimumDescriptionLength}} characters or longer.',
     },
     schema: [
       {
@@ -98,20 +101,29 @@ export default util.createRule<[Options], MessageIds>({
     },
   ],
   create(context, [options]) {
-    const tsCommentRegExp = /^\/*\s*@ts-(expect-error|ignore|check|nocheck)(.*)/;
+    /**
+     * Test for whether a single line comment's text contains a directive.
+     */
+    const commentDirectiveRegExSingleLine = /^\/*\s*@ts-(expect-error|ignore|check|nocheck)(.*)/;
+
+    /**
+     * Test for whether a multi-line comment's last line contains a directive.
+     */
+    const commentDirectiveRegExMultiLine = /^\s*(?:\/|\*)*\s*@ts-(expect-error|ignore|check|nocheck)(.*)/;
     const sourceCode = context.getSourceCode();
+
+    function isLineComment(comment: TSESTree.Comment): boolean {
+      return comment.type === AST_TOKEN_TYPES.Line;
+    }
 
     return {
       Program(): void {
         const comments = sourceCode.getAllComments();
 
         comments.forEach(comment => {
-          if (comment.type !== AST_TOKEN_TYPES.Line) {
-            return;
-          }
-
-          const [, directive, description] =
-            tsCommentRegExp.exec(comment.value) ?? [];
+          const [, directive, description] = isLineComment(comment)
+            ? commentDirectiveRegExSingleLine.exec(comment.value) ?? []
+            : commentDirectiveRegExMultiLine.exec(comment.value) ?? [];
 
           const fullDirective = `ts-${directive}` as keyof Options;
 
