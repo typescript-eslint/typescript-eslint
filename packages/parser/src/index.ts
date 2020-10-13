4709fb635fd3c49c72b32e80c38a23778e5f93cc
koooge <koooooge@gmail.com>
2020-10-14 01:14:36 +0200

    chore(parser): clean unused code
    Signed-off-by: koooge <koooooge@gmail.com>

--- packages/parser/src/index.ts
+++ packages/parser/src/index.ts
@@ -1,6 +1,8 @@
 export { parse, parseForESLint, ParserOptions } from './parser';
-export { ParserServices } from '@typescript-eslint/typescript-estree';
-export { clearCaches } from '@typescript-eslint/typescript-estree';
+export {
+  ParserServices,
+  clearCaches,
+} from '@typescript-eslint/typescript-estree';
 
 // note - cannot migrate this to an import statement because it will make TSC copy the package.json to the dist folder
 export const version: string = require('../package.json').version;
