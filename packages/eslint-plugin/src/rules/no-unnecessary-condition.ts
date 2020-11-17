891693c3b005d20c5be8374822134a8b27189496
Arnaud Barré <arnaud@nabla.com>
2020-11-17 01:48:17 +0100

    fix(eslint-plugin): false positive alwaysTruthyFunc for no-unnecessary-condition when predicate returns unknown

--- packages/eslint-plugin/src/rules/no-unnecessary-condition.ts
+++ packages/eslint-plugin/src/rules/no-unnecessary-condition.ts
@@ -33,6 +33,8 @@ const isTruthyLiteral = (type: ts.Type): boolean =>
   isBooleanLiteralType(type, true) || (isLiteralType(type) && !!type.value);
 
 const isPossiblyFalsy = (type: ts.Type): boolean =>
+  isTypeUnknownType(type) ||
+  isTypeAnyType(type) ||
   unionTypeParts(type)
     // PossiblyFalsy flag includes literal values, so exclude ones that
     // are definitely truthy
