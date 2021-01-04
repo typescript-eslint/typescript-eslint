9092c0494ebd3486e38852198c1930f1432ef21f
Brad Zacher <brad.zacher@gmail.com>
2021-01-03 17:12:56 -0800

    feat(eslint-plugin): add rule `sort-type-union-intersection-members` (#2913)

--- packages/eslint-plugin/src/rules/index.ts
+++ packages/eslint-plugin/src/rules/index.ts
@@ -102,6 +102,7 @@ import restrictPlusOperands from './restrict-plus-operands';
 import restrictTemplateExpressions from './restrict-template-expressions';
 import returnAwait from './return-await';
 import semi from './semi';
+import sortTypeUnionIntersectionMembers from './sort-type-union-intersection-members';
 import spaceBeforeFunctionParen from './space-before-function-paren';
 import spaceInfixOps from './space-infix-ops';
 import strictBooleanExpressions from './strict-boolean-expressions';
@@ -217,6 +218,7 @@ export default {
   'restrict-template-expressions': restrictTemplateExpressions,
   'return-await': returnAwait,
   semi: semi,
+  'sort-type-union-intersection-members': sortTypeUnionIntersectionMembers,
   'space-before-function-paren': spaceBeforeFunctionParen,
   'space-infix-ops': spaceInfixOps,
   'strict-boolean-expressions': strictBooleanExpressions,
