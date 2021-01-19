cabfcf7bfd468187372615c72338c6b5e77fca17
Brad Zacher <brad.zacher@gmail.com>
2021-01-18 22:32:43 -0800

    fix(scope-manager): fix incorrect handling of class decorators and class method default params
    Fixes #2941
    Fixes #2942
    
    This was a regression introduced by #2751

--- packages/eslint-plugin/tests/eslint-rules/no-undef.test.ts
+++ packages/eslint-plugin/tests/eslint-rules/no-undef.test.ts
@@ -224,6 +224,17 @@ function Foo() {}
     `
 const x = 1 as const;
     `,
+    // https://github.com/typescript-eslint/typescript-eslint/issues/2942
+    `
+declare function deco(...param: any): (...param: any) => any;
+
+@deco({
+  components: {
+    val: true,
+  },
+})
+class Foo {}
+    `,
   ],
   invalid: [
     {

typescript-eslint/typescript-eslint

commit 445e416878b27a54bf07c2d3b84dabd7b06e51bc

feat: add support for decorator metadata in scope analysis and in consistent-type-imports (#2751)
Fixes #2559

packages/scope-manager/src/referencer/ClassVisitor.ts
@@ -1,0 +1,362 @@
+import { AST_NODE_TYPES, TSESTree } from '@typescript-eslint/types';
+import { ClassNameDefinition, ParameterDefinition } from '../definition';
+import { Referencer } from './Referencer';
+import { TypeVisitor } from './TypeVisitor';
+import { Visitor } from './Visitor';
+
+class ClassVisitor extends Visitor {
+  readonly #classNode: TSESTree.ClassDeclaration | TSESTree.ClassExpression;
+  readonly #referencer: Referencer;
+  readonly #emitDecoratorMetadata: boolean;
+
+  constructor(
+    referencer: Referencer,
+    node: TSESTree.ClassDeclaration | TSESTree.ClassExpression,
+    emitDecoratorMetadata: boolean,
+  ) {
+    super(referencer);
+    this.#referencer = referencer;
+    this.#classNode = node;
+    this.#emitDecoratorMetadata = emitDecoratorMetadata;
+  }
+
+  static visit(
+    referencer: Referencer,
+    node: TSESTree.ClassDeclaration | TSESTree.ClassExpression,
+    emitDecoratorMetadata: boolean,
+  ): void {
+    const classVisitor = new ClassVisitor(
+      referencer,
+      node,
+      emitDecoratorMetadata,
+    );
+    classVisitor.visitClass(node);
+  }
+
+  ///////////////////
+  // Visit helpers //
+  ///////////////////
+
+  protected visitClass(
+    node: TSESTree.ClassDeclaration | TSESTree.ClassExpression,
+  ): void {
+    if (node.type === AST_NODE_TYPES.ClassDeclaration && node.id) {
+      this.#referencer
+        .currentScope()
+        .defineIdentifier(node.id, new ClassNameDefinition(node.id, node));
+    }
+
+    node.decorators?.forEach(d => this.visit(d));
+
+    this.#referencer.scopeManager.nestClassScope(node);
+
+    if (node.id) {
+      // define the class name again inside the new scope
+      // references to the class should not resolve directly to the parent class
+      this.#referencer
+        .currentScope()
+        .defineIdentifier(node.id, new ClassNameDefinition(node.id, node));
+    }
+
+    this.#referencer.visit(node.superClass);
+
+    // visit the type param declarations
+    this.visitType(node.typeParameters);
+    // then the usages
+    this.visitType(node.superTypeParameters);
+    node.implements?.forEach(imp => this.visitType(imp));
+
+    this.visit(node.body);
+
+    this.#referencer.close(node);
+  }
+
+  protected visitClassProperty(
+    node: TSESTree.TSAbstractClassProperty | TSESTree.ClassProperty,
+  ): void {
+    this.visitProperty(node);
+    /**
+     * class A {
+     *   @meta     // <--- check this
+     *   foo: Type;
+     * }
+     */
+    this.visitMetadataType(node.typeAnnotation, !!node.decorators);
+  }
+
+  protected visitFunctionParameterTypeAnnotation(
+    node: TSESTree.Parameter,
+    withDecorators: boolean,
+  ): void {
+    if ('typeAnnotation' in node) {
+      this.visitMetadataType(node.typeAnnotation, withDecorators);
+    } else if (node.type === AST_NODE_TYPES.AssignmentPattern) {
+      this.visitMetadataType(node.left.typeAnnotation, withDecorators);
+    } else if (node.type === AST_NODE_TYPES.TSParameterProperty) {
+      this.visitFunctionParameterTypeAnnotation(node.parameter, withDecorators);
+    }
+  }
+
+  protected visitMethodFunction(
+    node: TSESTree.FunctionExpression,
+    methodNode: TSESTree.MethodDefinition,
+  ): void {
+    if (node.id) {
+      // FunctionExpression with name creates its special scope;
+      // FunctionExpressionNameScope.
+      this.#referencer.scopeManager.nestFunctionExpressionNameScope(node);
+    }
+
+    // Consider this function is in the MethodDefinition.
+    this.#referencer.scopeManager.nestFunctionScope(node, true);
+
+    /**
+     * class A {
+     *   @meta     // <--- check this
+     *   foo(a: Type) {}
+     *
+     *   @meta     // <--- check this
+     *   foo(): Type {}
+     * }
+     */
+    let withMethodDecorators = !!methodNode.decorators;
+    /**
+     * class A {
+     *   foo(
+     *     @meta    // <--- check this
+     *     a: Type
+     *   ) {}
+     *
+     *   set foo(
+     *     @meta    // <--- EXCEPT this. TS do nothing for this
+     *     a: Type
+     *   ) {}
+     * }
+     */
+    withMethodDecorators =
+      withMethodDecorators ||
+      (methodNode.kind !== 'set' &&
+        node.params.some(param => param.decorators));
+    if (!withMethodDecorators && methodNode.kind === 'set') {
+      const keyName = getLiteralMethodKeyName(methodNode);
+
+      /**
+       * class A {
+       *   @meta      // <--- check this
+       *   get a() {}
+       *   set ['a'](v: Type) {}
+       * }
+       */
+      if (
+        keyName !== null &&
+        this.#classNode.body.body.find(
+          (node): node is TSESTree.MethodDefinition =>
+            node !== methodNode &&
+            node.type === AST_NODE_TYPES.MethodDefinition &&
+            // Node must both be static or not
+            node.static === methodNode.static &&
+            getLiteralMethodKeyName(node) === keyName,
+        )?.decorators
+      ) {
+        withMethodDecorators = true;
+      }
+    }
+
+    /**
+     * @meta      // <--- check this
+     * class A {
+     *   constructor(a: Type) {}
+     * }
+     */
+    if (
+      !withMethodDecorators &&
+      methodNode.kind === 'constructor' &&
+      this.#classNode.decorators
+    ) {
+      withMethodDecorators = true;
+    }
+
+    // Process parameter declarations.
+    for (const param of node.params) {
+      this.visitPattern(
+        param,
+        (pattern, info) => {
+          this.#referencer
+            .currentScope()
+            .defineIdentifier(
+              pattern,
+              new ParameterDefinition(pattern, node, info.rest),
+            );
+
+          this.#referencer.referencingDefaultValue(
+            pattern,
+            info.assignments,
+            null,
+            true,
+          );
+        },
+        { processRightHandNodes: true },
+      );
+      this.visitFunctionParameterTypeAnnotation(param, withMethodDecorators);
+      param.decorators?.forEach(d => this.visit(d));
+    }
+
+    this.visitMetadataType(node.returnType, withMethodDecorators);
+    this.visitType(node.typeParameters);
+
+    // In TypeScript there are a number of function-like constructs which have no body,
+    // so check it exists before traversing
+    if (node.body) {
+      // Skip BlockStatement to prevent creating BlockStatement scope.
+      if (node.body.type === AST_NODE_TYPES.BlockStatement) {
+        this.#referencer.visitChildren(node.body);
+      } else {
+        this.#referencer.visit(node.body);
+      }
+    }
+
+    this.#referencer.close(node);
+  }
+
+  protected visitProperty(
+    node:
+      | TSESTree.ClassProperty
+      | TSESTree.TSAbstractClassProperty
+      | TSESTree.TSAbstractMethodDefinition,
+  ): void {
+    if (node.computed) {
+      this.#referencer.visit(node.key);
+    }
+
+    this.#referencer.visit(node.value);
+
+    if ('decorators' in node) {
+      node.decorators?.forEach(d => this.#referencer.visit(d));
+    }
+  }
+
+  protected visitMethod(node: TSESTree.MethodDefinition): void {
+    if (node.computed) {
+      this.#referencer.visit(node.key);
+    }
+
+    if (node.value.type === AST_NODE_TYPES.FunctionExpression) {
+      this.visitMethodFunction(node.value, node);
+    } else {
+      this.#referencer.visit(node.value);
+    }
+
+    if ('decorators' in node) {
+      node.decorators?.forEach(d => this.#referencer.visit(d));
+    }
+  }
+
+  protected visitType(node: TSESTree.Node | null | undefined): void {
+    if (!node) {
+      return;
+    }
+    TypeVisitor.visit(this.#referencer, node);
+  }
+
+  protected visitMetadataType(
+    node: TSESTree.TSTypeAnnotation | null | undefined,
+    withDecorators: boolean,
+  ): void {
+    if (!node) {
+      return;
+    }
+    // emit decorators metadata only work for TSTypeReference in ClassDeclaration
+    if (
+      this.#classNode.type === AST_NODE_TYPES.ClassDeclaration &&
+      !this.#classNode.declare &&
+      node.typeAnnotation.type === AST_NODE_TYPES.TSTypeReference &&
+      this.#emitDecoratorMetadata
+    ) {
+      let identifier: TSESTree.Identifier;
+      if (
+        node.typeAnnotation.typeName.type === AST_NODE_TYPES.TSQualifiedName
+      ) {
+        let iter = node.typeAnnotation.typeName;
+        while (iter.left.type === AST_NODE_TYPES.TSQualifiedName) {
+          iter = iter.left;
+        }
+        identifier = iter.left;
+      } else {
+        identifier = node.typeAnnotation.typeName;
+      }
+
+      if (withDecorators) {
+        return this.#referencer
+          .currentScope()
+          .referenceDualValueType(identifier);
+      }
+    }
+    this.visitType(node);
+  }
+
+  /////////////////////
+  // Visit selectors //
+  /////////////////////
+
+  protected ClassProperty(node: TSESTree.ClassProperty): void {
+    this.visitClassProperty(node);
+  }
+
+  protected MethodDefinition(node: TSESTree.MethodDefinition): void {
+    this.visitMethod(node);
+  }
+
+  protected TSAbstractClassProperty(
+    node: TSESTree.TSAbstractClassProperty,
+  ): void {
+    this.visitClassProperty(node);
+  }
+
+  protected TSAbstractMethodDefinition(
+    node: TSESTree.TSAbstractMethodDefinition,
+  ): void {
+    this.visitProperty(node);
+  }
+
+  protected Identifier(node: TSESTree.Identifier): void {
+    this.#referencer.visit(node);
+  }
+}
+
+/**
+ * Only if key is one of [identifier, string, number], ts will combine metadata of accessors .
+ * class A {
+ *   get a() {}
+ *   set ['a'](v: Type) {}
+ *
+ *   get [1]() {}
+ *   set [1](v: Type) {}
+ *
+ *   // Following won't be combined
+ *   get [key]() {}
+ *   set [key](v: Type) {}
+ *
+ *   get [true]() {}
+ *   set [true](v: Type) {}
+ *
+ *   get ['a'+'b']() {}
+ *   set ['a'+'b']() {}
+ * }
+ */
+function getLiteralMethodKeyName(
+  node: TSESTree.MethodDefinition,
+): string | number | null {
+  if (node.computed && node.key.type === AST_NODE_TYPES.Literal) {
+    if (
+      typeof node.key.value === 'string' ||
+      typeof node.key.value === 'number'
+    ) {
+      return node.key.value;
+    }
+  } else if (!node.computed && node.key.type === AST_NODE_TYPES.Identifier) {
+    return node.key.name;
+  }
+  return null;
+}
+
+export { ClassVisitor };

packages/eslint-plugin/tests/rules/consistent-type-imports.test.ts
@@ -1,5 +1,5 @@
 import rule from '../../src/rules/consistent-type-imports';
-import { RuleTester, noFormat } from '../RuleTester';
+import { RuleTester, noFormat, getFixturesRootDir } from '../RuleTester';
 
 const ruleTester = new RuleTester({
   parser: '@typescript-eslint/parser',
@@ -8,6 +8,11 @@ const ruleTester = new RuleTester({
     sourceType: 'module',
   },
 });
+
+const withMetaParserOptions = {
+  tsconfigRootDir: getFixturesRootDir(),
+  project: './tsconfig-withmeta.json',
+};
 
 ruleTester.run('consistent-type-imports', rule, {
   valid: [
@@ -230,6 +235,112 @@ ruleTester.run('consistent-type-imports', rule, {
       const a: typeof Default = Default;
       const b: typeof Rest = Rest;
     `,
+    {
+      code: `
+        import Foo from 'foo';
+        @deco
+        class A {
+          constructor(foo: Foo) {}
+        }
+      `,
+      parserOptions: withMetaParserOptions,
+    },
+    {
+      code: `
+        import Foo from 'foo';
+        class A {
+          @deco
+          foo: Foo;
+        }
+      `,
+      parserOptions: withMetaParserOptions,
+    },
+    {
+      code: `
+        import Foo from 'foo';
+        class A {
+          @deco
+          foo(foo: Foo) {}
+        }
+      `,
+      parserOptions: withMetaParserOptions,
+    },
+    {
+      code: `
+        import Foo from 'foo';
+        class A {
+          @deco
+          foo(): Foo {}
+        }
+      `,
+      parserOptions: withMetaParserOptions,
+    },
+    {
+      code: `
+        import Foo from 'foo';
+        class A {
+          foo(@deco foo: Foo) {}
+        }
+      `,
+      parserOptions: withMetaParserOptions,
+    },
+    {
+      code: `
+        import Foo from 'foo';
+        class A {
+          @deco
+          set foo(value: Foo) {}
+        }
+      `,
+      parserOptions: withMetaParserOptions,
+    },
+    {
+      code: `
+        import Foo from 'foo';
+        class A {
+          @deco
+          get foo() {}
+
+          set foo(value: Foo) {}
+        }
+      `,
+      parserOptions: withMetaParserOptions,
+    },
+    {
+      code: `
+        import Foo from 'foo';
+        class A {
+          @deco
+          get foo() {}
+
+          set ['foo'](value: Foo) {}
+        }
+      `,
+      parserOptions: withMetaParserOptions,
+    },
+    {
+      code: `
+        import type { Foo } from 'foo';
+        const key = 'k';
+        class A {
+          @deco
+          get [key]() {}
+
+          set [key](value: Foo) {}
+        }
+      `,
+      parserOptions: withMetaParserOptions,
+    },
+    {
+      code: `
+        import * as foo from 'foo';
+        @deco
+        class A {
+          constructor(foo: foo.Foo) {}
+        }
+      `,
+      parserOptions: withMetaParserOptions,
+    },
   ],
   invalid: [
     {
@@ -1214,6 +1325,189 @@ const a: Default = '';
           column: 1,
         },
       ],
+    },
+    {
+      code: `
+        import Foo from 'foo';
+        @deco
+        class A {
+          constructor(foo: Foo) {}
+        }
+      `,
+      output: `
+        import type Foo from 'foo';
+        @deco
+        class A {
+          constructor(foo: Foo) {}
+        }
+      `,
+      errors: [
+        {
+          messageId: 'typeOverValue',
+          line: 2,
+          column: 9,
+        },
+      ],
+    },
+    {
+      code: `
+        import type Foo from 'foo';
+        @deco
+        class A {
+          constructor(foo: Foo) {}
+        }
+      `,
+      output: noFormat`
+        import Foo from 'foo';
+        @deco
+        class A {
+          constructor(foo: Foo) {}
+        }
+      `,
+      errors: [
+        {
+          messageId: 'aImportInDecoMeta',
+          data: { typeImports: '"Foo"' },
+          line: 2,
+          column: 9,
+        },
+      ],
+      parserOptions: withMetaParserOptions,
+    },
+    {
+      code: `
+        import type { Foo } from 'foo';
+        @deco
+        class A {
+          constructor(foo: Foo) {}
+        }
+      `,
+      output: noFormat`
+        import { Foo } from 'foo';
+        @deco
+        class A {
+          constructor(foo: Foo) {}
+        }
+      `,
+      errors: [
+        {
+          messageId: 'aImportInDecoMeta',
+          data: { typeImports: '"Foo"' },
+          line: 2,
+          column: 9,
+        },
+      ],
+      parserOptions: withMetaParserOptions,
+    },
+    {
+      code: noFormat`
+        import type { Type } from 'foo';
+        import { Foo, Bar } from 'foo';
+        @deco
+        class A {
+          constructor(foo: Foo) {}
+        }
+        type T = Bar;
+      `,
+      output: noFormat`
+        import type { Type , Bar } from 'foo';
+        import { Foo } from 'foo';
+        @deco
+        class A {
+          constructor(foo: Foo) {}
+        }
+        type T = Bar;
+      `,
+      errors: [
+        {
+          messageId: 'aImportIsOnlyTypes',
+          data: { typeImports: '"Bar"' },
+          line: 3,
+          column: 9,
+        },
+      ],
+      parserOptions: withMetaParserOptions,
+    },
+    {
+      code: `
+        import { V } from 'foo';
+        import type { Foo, Bar, T } from 'foo';
+        @deco
+        class A {
+          constructor(foo: Foo) {}
+          foo(@deco bar: Bar) {}
+        }
+      `,
+      output: noFormat`
+        import { V , Foo, Bar} from 'foo';
+        import type { T } from 'foo';
+        @deco
+        class A {
+          constructor(foo: Foo) {}
+          foo(@deco bar: Bar) {}
+        }
+      `,
+      errors: [
+        {
+          messageId: 'someImportsInDecoMeta',
+          data: { typeImports: '"Foo" and "Bar"' },
+          line: 3,
+          column: 9,
+        },
+      ],
+      parserOptions: withMetaParserOptions,
+    },
+    {
+      code: `
+        import type { Foo, T } from 'foo';
+        import { V } from 'foo';
+        @deco
+        class A {
+          constructor(foo: Foo) {}
+        }
+      `,
+      output: noFormat`
+        import type { T } from 'foo';
+        import { V , Foo} from 'foo';
+        @deco
+        class A {
+          constructor(foo: Foo) {}
+        }
+      `,
+      errors: [
+        {
+          messageId: 'aImportInDecoMeta',
+          data: { typeImports: '"Foo"' },
+          line: 2,
+          column: 9,
+        },
+      ],
+      parserOptions: withMetaParserOptions,
+    },
+    {
+      code: `
+        import type * as Type from 'foo';
+        @deco
+        class A {
+          constructor(foo: Type.Foo) {}
+        }
+      `,
+      output: noFormat`
+        import * as Type from 'foo';
+        @deco
+        class A {
+          constructor(foo: Type.Foo) {}
+        }
+      `,
+      errors: [
+        {
+          messageId: 'aImportInDecoMeta',
+          data: { typeImports: '"Type"' },
+          line: 2,
+          column: 9,
+        },
+      ],
+      parserOptions: withMetaParserOptions,
     },
   ],
 });

packages/parser/src/parser.ts
@@ -164,6 +164,10 @@ function parseForESLint(
         );
       }
     }
+    if (compilerOptions.emitDecoratorMetadata === true) {
+      analyzeOptions.emitDecoratorMetadata =
+        compilerOptions.emitDecoratorMetadata;
+    }
   }
 
   const scopeManager = analyze(ast, analyzeOptions);

packages/scope-manager/src/analyze.ts
@@ -61,6 +61,12 @@ interface AnalyzeOptions {
    * The source type of the script.
    */
   sourceType?: 'script' | 'module';
+
+  /**
+   * Emit design-type metadata for decorated declarations in source.
+   * Defaults to `false`.
+   */
+  emitDecoratorMetadata?: boolean;
 }
 
 const DEFAULT_OPTIONS: Required<AnalyzeOptions> = {
@@ -72,6 +78,7 @@ const DEFAULT_OPTIONS: Required<AnalyzeOptions> = {
   jsxFragmentName: null,
   lib: ['es2018'],
   sourceType: 'script',
+  emitDecoratorMetadata: false,
 };
 
 function mapEcmaVersion(version: EcmaVersion | undefined): Lib {
@@ -106,6 +113,9 @@ function analyze(
       providedOptions?.jsxFragmentName ?? DEFAULT_OPTIONS.jsxFragmentName,
     sourceType: providedOptions?.sourceType ?? DEFAULT_OPTIONS.sourceType,
     lib: providedOptions?.lib ?? [mapEcmaVersion(ecmaVersion)],
+    emitDecoratorMetadata:
+      providedOptions?.emitDecoratorMetadata ??
+      DEFAULT_OPTIONS.emitDecoratorMetadata,
   };
 
   // ensure the option is lower cased

commit cabfcf7bfd468187372615c72338c6b5e77fca17

fix(scope-manager): fix incorrect handling of class decorators and class method default params
Fixes #2941
Fixes #2942

This was a regression introduced by #2751

packages/scope-manager/src/referencer/ClassVisitor.ts
@@ -33,6 +33,15 @@ class ClassVisitor extends Visitor {
     classVisitor.visitClass(node);
   }
 
+  visit(node: TSESTree.Node | null | undefined): void {
+    // make sure we only handle the nodes we are designed to handle
+    if (node && node.type in this) {
+      super.visit(node);
+    } else {
+      this.#referencer.visit(node);
+    }
+  }
+
   ///////////////////
   // Visit helpers //
   ///////////////////
@@ -46,7 +55,7 @@ class ClassVisitor extends Visitor {
         .defineIdentifier(node.id, new ClassNameDefinition(node.id, node));
     }
 
-    node.decorators?.forEach(d => this.visit(d));
+    node.decorators?.forEach(d => this.#referencer.visit(d));
 
     this.#referencer.scopeManager.nestClassScope(node);
 
@@ -297,6 +306,12 @@ class ClassVisitor extends Visitor {
   /////////////////////
   // Visit selectors //
   /////////////////////
+
+  protected ClassBody(node: TSESTree.ClassBody): void {
+    // this is here on purpose so that this visitor explicitly declares visitors
+    // for all nodes it cares about (see the instance visit method above)
+    this.visitChildren(node);
+  }
 
   protected ClassProperty(node: TSESTree.ClassProperty): void {
     this.visitClassProperty(node);

packages/eslint-plugin/tests/eslint-rules/no-undef.test.ts
@@ -224,6 +224,17 @@ function Foo() {}
     `
 const x = 1 as const;
     `,
+    // https://github.com/typescript-eslint/typescript-eslint/issues/2942
+    `
+declare function deco(...param: any): (...param: any) => any;
+
+@deco({
+  components: {
+    val: true,
+  },
+})
+class Foo {}
+    `,
   ],
   invalid: [
     {

packages/eslint-plugin/tests/rules/no-use-before-define.test.ts
@@ -428,6 +428,20 @@ export class CidrIpPatternDirective implements Validator {}
         },
       ],
     },
+    // https://github.com/typescript-eslint/typescript-eslint/issues/2941
+    `
+class A {
+  constructor(printName) {
+    this.printName = printName;
+  }
+
+  openPort(printerName = this.printerName) {
+    this.tscOcx.ActiveXopenport(printerName);
+
+    return this;
+  }
+}
+    `,
   ],
   invalid: [
     {

packages/scope-manager/tests/fixtures/class/declaration/method-param-default.ts
@@ -1,0 +1,13 @@
+// https://github.com/typescript-eslint/typescript-eslint/issues/2941
+
+class A {
+  constructor(printName) {
+    this.printName = printName;
+  }
+
+  openPort(printerName = this.printerName) {
+    this.tscOcx.ActiveXopenport(printerName);
+
+    return this;
+  }
+}

packages/scope-manager/tests/fixtures/class/declaration/method-param-default.ts.shot
@@ -1,0 +1,164 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`class declaration method-param-default 1`] = `
+ScopeManager {
+  variables: Array [
+    ImplicitGlobalConstTypeVariable,
+    Variable$2 {
+      defs: Array [
+        ClassNameDefinition$1 {
+          name: Identifier<"A">,
+          node: ClassDeclaration$1,
+        },
+      ],
+      name: "A",
+      references: Array [],
+      isValueVariable: true,
+      isTypeVariable: true,
+    },
+    Variable$3 {
+      defs: Array [
+        ClassNameDefinition$2 {
+          name: Identifier<"A">,
+          node: ClassDeclaration$1,
+        },
+      ],
+      name: "A",
+      references: Array [],
+      isValueVariable: true,
+      isTypeVariable: true,
+    },
+    Variable$4 {
+      defs: Array [],
+      name: "arguments",
+      references: Array [],
+      isValueVariable: true,
+      isTypeVariable: true,
+    },
+    Variable$5 {
+      defs: Array [
+        ParameterDefinition$3 {
+          name: Identifier<"printName">,
+          node: FunctionExpression$2,
+        },
+      ],
+      name: "printName",
+      references: Array [
+        Reference$1 {
+          identifier: Identifier<"printName">,
+          isRead: true,
+          isTypeReference: false,
+          isValueReference: true,
+          isWrite: false,
+          resolved: Variable$5,
+        },
+      ],
+      isValueVariable: true,
+      isTypeVariable: false,
+    },
+    Variable$6 {
+      defs: Array [],
+      name: "arguments",
+      references: Array [],
+      isValueVariable: true,
+      isTypeVariable: true,
+    },
+    Variable$7 {
+      defs: Array [
+        ParameterDefinition$4 {
+          name: Identifier<"printerName">,
+          node: FunctionExpression$3,
+        },
+      ],
+      name: "printerName",
+      references: Array [
+        Reference$2 {
+          identifier: Identifier<"printerName">,
+          init: true,
+          isRead: false,
+          isTypeReference: false,
+          isValueReference: true,
+          isWrite: true,
+          resolved: Variable$7,
+          writeExpr: MemberExpression$4,
+        },
+        Reference$3 {
+          identifier: Identifier<"printerName">,
+          isRead: true,
+          isTypeReference: false,
+          isValueReference: true,
+          isWrite: false,
+          resolved: Variable$7,
+        },
+      ],
+      isValueVariable: true,
+      isTypeVariable: false,
+    },
+  ],
+  scopes: Array [
+    GlobalScope$1 {
+      block: Program$5,
+      isStrict: false,
+      references: Array [],
+      set: Map {
+        "const" => ImplicitGlobalConstTypeVariable,
+        "A" => Variable$2,
+      },
+      type: "global",
+      upper: null,
+      variables: Array [
+        ImplicitGlobalConstTypeVariable,
+        Variable$2,
+      ],
+    },
+    ClassScope$2 {
+      block: ClassDeclaration$1,
+      isStrict: true,
+      references: Array [],
+      set: Map {
+        "A" => Variable$3,
+      },
+      type: "class",
+      upper: GlobalScope$1,
+      variables: Array [
+        Variable$3,
+      ],
+    },
+    FunctionScope$3 {
+      block: FunctionExpression$2,
+      isStrict: true,
+      references: Array [
+        Reference$1,
+      ],
+      set: Map {
+        "arguments" => Variable$4,
+        "printName" => Variable$5,
+      },
+      type: "function",
+      upper: ClassScope$2,
+      variables: Array [
+        Variable$4,
+        Variable$5,
+      ],
+    },
+    FunctionScope$4 {
+      block: FunctionExpression$3,
+      isStrict: true,
+      references: Array [
+        Reference$2,
+        Reference$3,
+      ],
+      set: Map {
+        "arguments" => Variable$6,
+        "printerName" => Variable$7,
+      },
+      type: "function",
+      upper: ClassScope$2,
+      variables: Array [
+        Variable$6,
+        Variable$7,
+      ],
+    },
+  ],
+}
+`;

packages/scope-manager/tests/fixtures/decorators/class-deco-with-object-param.ts
@@ -1,0 +1,10 @@
+// https://github.com/typescript-eslint/typescript-eslint/issues/2942
+
+declare function deco(...param: any): (...param: any) => any;
+
+@deco({
+  components: {
+    val: true,
+  },
+})
+class Foo {}

packages/scope-manager/tests/fixtures/decorators/class-deco-with-object-param.ts.shot
@@ -1,0 +1,147 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`decorators class-deco-with-object-param 1`] = `
+ScopeManager {
+  variables: Array [
+    ImplicitGlobalConstTypeVariable,
+    Variable$2 {
+      defs: Array [
+        FunctionNameDefinition$1 {
+          name: Identifier<"deco">,
+          node: TSDeclareFunction$1,
+        },
+      ],
+      name: "deco",
+      references: Array [
+        Reference$1 {
+          identifier: Identifier<"deco">,
+          isRead: true,
+          isTypeReference: false,
+          isValueReference: true,
+          isWrite: false,
+          resolved: Variable$2,
+        },
+      ],
+      isValueVariable: true,
+      isTypeVariable: false,
+    },
+    Variable$3 {
+      defs: Array [],
+      name: "arguments",
+      references: Array [],
+      isValueVariable: true,
+      isTypeVariable: true,
+    },
+    Variable$4 {
+      defs: Array [
+        ParameterDefinition$2 {
+          name: Identifier<"param">,
+          node: TSDeclareFunction$1,
+        },
+      ],
+      name: "param",
+      references: Array [],
+      isValueVariable: true,
+      isTypeVariable: false,
+    },
+    Variable$5 {
+      defs: Array [
+        ParameterDefinition$3 {
+          name: Identifier<"param">,
+          node: TSFunctionType$2,
+        },
+      ],
+      name: "param",
+      references: Array [],
+      isValueVariable: true,
+      isTypeVariable: false,
+    },
+    Variable$6 {
+      defs: Array [
+        ClassNameDefinition$4 {
+          name: Identifier<"Foo">,
+          node: ClassDeclaration$3,
+        },
+      ],
+      name: "Foo",
+      references: Array [],
+      isValueVariable: true,
+      isTypeVariable: true,
+    },
+    Variable$7 {
+      defs: Array [
+        ClassNameDefinition$5 {
+          name: Identifier<"Foo">,
+          node: ClassDeclaration$3,
+        },
+      ],
+      name: "Foo",
+      references: Array [],
+      isValueVariable: true,
+      isTypeVariable: true,
+    },
+  ],
+  scopes: Array [
+    GlobalScope$1 {
+      block: Program$4,
+      isStrict: false,
+      references: Array [
+        Reference$1,
+      ],
+      set: Map {
+        "const" => ImplicitGlobalConstTypeVariable,
+        "deco" => Variable$2,
+        "Foo" => Variable$6,
+      },
+      type: "global",
+      upper: null,
+      variables: Array [
+        ImplicitGlobalConstTypeVariable,
+        Variable$2,
+        Variable$6,
+      ],
+    },
+    FunctionScope$2 {
+      block: TSDeclareFunction$1,
+      isStrict: false,
+      references: Array [],
+      set: Map {
+        "arguments" => Variable$3,
+        "param" => Variable$4,
+      },
+      type: "function",
+      upper: GlobalScope$1,
+      variables: Array [
+        Variable$3,
+        Variable$4,
+      ],
+    },
+    FunctionTypeScope$3 {
+      block: TSFunctionType$2,
+      isStrict: true,
+      references: Array [],
+      set: Map {
+        "param" => Variable$5,
+      },
+      type: "functionType",
+      upper: FunctionScope$2,
+      variables: Array [
+        Variable$5,
+      ],
+    },
+    ClassScope$4 {
+      block: ClassDeclaration$3,
+      isStrict: true,
+      references: Array [],
+      set: Map {
+        "Foo" => Variable$7,
+      },
+      type: "class",
+      upper: GlobalScope$1,
+      variables: Array [
+        Variable$7,
+      ],
+    },
+  ],
+}
+`;