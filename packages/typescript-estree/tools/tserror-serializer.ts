8604da4d8e2b9c091d43dd3c69a15494f9fe6ad7
Armano <armano2@users.noreply.github.com>
2021-02-08 08:50:20 +0100

    test(typescript-estree): improve printing TSError in snapshots

--- packages/typescript-estree/tools/tserror-serializer.ts
+++ packages/typescript-estree/tools/tserror-serializer.ts
@@ -2,7 +2,13 @@ import { TSError } from '../src/node-utils';
 
 export const serializer: import('pretty-format').Plugin = {
   test: (val: unknown): val is TSError => val instanceof TSError,
-  serialize(val: TSError) {
-    return `[${val.name}: ${val.message} (${val.lineNumber}:${val.column} | ${val.index})`;
+  serialize(val: TSError, config) {
+    return (
+      `[${val.name}: ${val.message}\n` +
+      `${config.indent}"lineNumber": ${val.lineNumber},\n` +
+      `${config.indent}"column": ${val.column},\n` +
+      `${config.indent}"index": ${val.index},\n` +
+      `]`
+    );
   },
 };
