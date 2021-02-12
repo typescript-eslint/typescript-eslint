4d9d0caec09b42aa2278daee09c431bb896a3145
Armano <armano2@users.noreply.github.com>
2021-02-12 08:41:03 +0100

    test: update ast alignment tests and simplify shared fixtures
    update babel to 7.12.16

--- packages/typescript-estree/tests/lib/semantic-diagnostics-enabled.test.ts
+++ packages/typescript-estree/tests/lib/semantic-diagnostics-enabled.test.ts
@@ -1,21 +1,21 @@
 import { readFileSync } from 'fs';
 import glob from 'glob';
+import path from 'path';
 import * as parser from '../../src';
-import { extname } from 'path';
 import { formatSnapshotName, isJSXFileType } from '../../tools/test-utils';
 
 /**
  * Process all fixtures, we will only snapshot the ones that have semantic errors
  * which are ignored by default parsing logic.
  */
-const FIXTURES_DIR =
-  '../../node_modules/@typescript-eslint/shared-fixtures/fixtures';
+const FIXTURES_DIR = path.join(__dirname, '../../../shared-fixtures/fixtures');
+
 const testFiles = glob.sync(`${FIXTURES_DIR}/**/*.src.*`);
 
 describe('Parse all fixtures with "errorOnTypeScriptSyntacticAndSemanticIssues" enabled', () => {
   testFiles.forEach(filename => {
     const code = readFileSync(filename, 'utf8');
-    const fileExtension = extname(filename);
+    const fileExtension = path.extname(filename);
     const config: parser.TSESTreeOptions = {
       loc: true,
       range: true,
