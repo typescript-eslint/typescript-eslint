38c001416ba91df8c36094dcd62f12a6d7765e8c
Armano <armano2@users.noreply.github.com>
2021-03-18 14:48:14 +0100

    docs(website): fix ssr build

--- website/src/pages/repl.tsx
+++ website/src/pages/repl.tsx
@@ -6,7 +6,7 @@ let Repl;
  * This is a hack for stuff that are bad in docosaurus
  * https://reactjs.org/docs/error-decoder.html?invariant=294
  */
-if (process.env.IS_SERVER !== 'true') {
+if (!process.env.IS_SERVER) {
   const Playground = lazy(() => import('../components/playground'));
 
   Repl = function () {
