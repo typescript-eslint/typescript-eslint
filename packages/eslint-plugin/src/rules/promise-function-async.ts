e3a3ea04757464aa2dded1ef46af8ad4e05246f2
Nikita Stefaniak <phaux4@gmail.com>
2021-03-18 20:56:00 +0100

    fix(eslint-plugin): [promise-function-async] bad fixer with computed and literal methods (#3163)

--- packages/eslint-plugin/src/rules/promise-function-async.ts
+++ packages/eslint-plugin/src/rules/promise-function-async.ts
@@ -1,5 +1,6 @@
 import {
   AST_NODE_TYPES,
+  AST_TOKEN_TYPES,
   TSESTree,
 } from '@typescript-eslint/experimental-utils';
 import * as ts from 'typescript';
@@ -156,8 +157,40 @@ export default util.createRule<Options, MessageIds>({
               (node.parent.type === AST_NODE_TYPES.Property &&
                 node.parent.method))
           ) {
-            return fixer.insertTextBefore(node.parent.key, 'async ');
+            // this function is a class method or object function property shorthand
+            const method = node.parent;
+
+            // the token to put `async` before
+            let keyToken = sourceCode.getFirstToken(method)!;
+
+            // if there are decorators then skip past them
+            if (
+              method.type === AST_NODE_TYPES.MethodDefinition &&
+              method.decorators
+            ) {
+              const lastDecorator =
+                method.decorators[method.decorators.length - 1];
+              keyToken = sourceCode.getTokenAfter(lastDecorator)!;
+            }
+
+            // if current token is a keyword like `static` or `public` then skip it
+            while (keyToken.type === AST_TOKEN_TYPES.Keyword) {
+              keyToken = sourceCode.getTokenAfter(keyToken)!;
+            }
+
+            // check if there is a space between key and previous token
+            const insertSpace = !sourceCode.isSpaceBetween!(
+              sourceCode.getTokenBefore(keyToken)!,
+              keyToken,
+            );
+
+            let code = 'async ';
+            if (insertSpace) {
+              code = ` ${code}`;
+            }
+            return fixer.insertTextBefore(keyToken, code);
           }
+
           return fixer.insertTextBefore(node, 'async ');
         },
       });
