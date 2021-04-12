20f05f659d5cb219cfc7f7212679c0a17811b3e7
Josh Goldberg <joshuakgoldberg@outlook.com>
2021-04-12 02:58:31 -0400

    chore: enable no-floating-promises internally (#3279)

--- .eslintrc.js
+++ .eslintrc.js
@@ -63,7 +63,6 @@ module.exports = {
     ],
 
     // TODO - enable these new recommended rules
-    '@typescript-eslint/no-floating-promises': 'off',
     '@typescript-eslint/no-unsafe-assignment': 'off',
     '@typescript-eslint/no-unsafe-call': 'off',
     '@typescript-eslint/no-unsafe-member-access': 'off',
