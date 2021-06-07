d5a98204402ffbf669136c578e669e418313c235
Michaël De Boey <info@michaeldeboey.be>
2021-06-07 18:53:15 +0200

    fix(ast-spec): specify `PunctuatorToken`'s `value` type

--- packages/ast-spec/src/token/PunctuatorToken/spec.ts
+++ packages/ast-spec/src/token/PunctuatorToken/spec.ts
@@ -3,4 +3,18 @@ import type { BaseToken } from '../../base/BaseToken';
 
 export interface PunctuatorToken extends BaseToken {
   type: AST_TOKEN_TYPES.Punctuator;
+  value:
+    | ','
+    | ';'
+    | ':'
+    | '!'
+    | '?.'
+    | '?'
+    | '('
+    | ')'
+    | '['
+    | ']'
+    | '{'
+    | '}'
+    | '=';
 }
