28ce573aea8acf3a14e3bedccfac85b7f0bf2b39
Edmundo Santos <rdsedmundo@gmail.com>
2021-06-12 14:00:41 -0300

    feat(typescript-estree): add support for custom module resolution

--- packages/typescript-estree/tests/lib/parse.test.ts
+++ packages/typescript-estree/tests/lib/parse.test.ts
@@ -670,4 +670,47 @@ describe('parseAndGenerateServices', () => {
       expect(testParse('includeme', ignore)).not.toThrow();
     });
   });
+
+  describe('moduleResolver', () => {
+    beforeEach(() => {
+      parser.clearCaches();
+    });
+
+    const PROJECT_DIR = resolve(FIXTURES_DIR, '../moduleResolver');
+    const code = `
+      import { something } from '__PLACEHOLDER__';
+      something;
+    `;
+    const config: TSESTreeOptions = {
+      comment: true,
+      tokens: true,
+      range: true,
+      loc: true,
+      tsconfigRootDir: PROJECT_DIR,
+      project: './tsconfig.json',
+      filePath: resolve(PROJECT_DIR, 'file.ts'),
+    };
+
+    it('returns error because __PLACEHOLDER__ can not be resolved', () => {
+      expect(
+        parser
+          .parseAndGenerateServices(code, config)
+          .services.program.getSemanticDiagnostics(),
+      ).toHaveProperty(
+        [0, 'messageText'],
+        "Cannot find module '__PLACEHOLDER__' or its corresponding type declarations.",
+      );
+    });
+
+    it('resolves __PLACEHOLDER__ correctly', () => {
+      expect(
+        parser
+          .parseAndGenerateServices(code, {
+            ...config,
+            moduleResolver: resolve(PROJECT_DIR, './moduleResolver.js'),
+          })
+          .services.program.getSemanticDiagnostics(),
+      ).toHaveLength(0);
+    });
+  });
 });
