6dd5abcbdd11b076792db175e9c006ab67d6ea27
JounQin <admin@1stg.me>
2021-07-14 10:35:11 +0000

    chore: prettier format

--- packages/typescript-estree/src/parser.ts
+++ packages/typescript-estree/src/parser.ts
@@ -346,7 +346,8 @@ function applyParserOptionsToExtra(options: TSESTreeOptions): void {
 
 function warnAboutTSVersion(): void {
   if (!isRunningSupportedTypeScriptVersion && !warnedAboutTSVersion) {
-    const isTTY = typeof process === 'undefined' ? false : process.stdout?.isTTY;
+    const isTTY =
+      typeof process === 'undefined' ? false : process.stdout?.isTTY;
     if (isTTY) {
       const border = '=============';
       const versionWarning = [
