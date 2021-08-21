804deb66951598275a22269e0ff6cdb571de7cd1
Michaël De Boey <info@michaeldeboey.be>
2021-08-21 19:45:46 +0200

    fix(ast-spec): simplify `Literal` types

--- packages/ast-spec/src/expression/literal/RegExpLiteral/spec.ts
+++ packages/ast-spec/src/expression/literal/RegExpLiteral/spec.ts
@@ -1,8 +1,6 @@
-import type { AST_NODE_TYPES } from '../../../ast-node-types';
 import type { LiteralBase } from '../../../base/LiteralBase';
 
 export interface RegExpLiteral extends LiteralBase {
-  type: AST_NODE_TYPES.Literal;
   value: RegExp | null;
   regex: {
     pattern: string;
