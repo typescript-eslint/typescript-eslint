207a23142d6f7a9bf6a0b285a5933d6f47359d7c
thomas michael wallace <thomasmichaelwallace@gmail.com>
2021-08-22 02:26:22 +0100

    chore(eslint-plugin): remove support for eslint 5 (#2683)

--- packages/eslint-plugin/src/rules/init-declarations.ts
+++ packages/eslint-plugin/src/rules/init-declarations.ts
@@ -27,12 +27,7 @@ export default createRule<Options, MessageIds>({
     },
     hasSuggestions: baseRule.meta.hasSuggestions,
     schema: baseRule.meta.schema,
-    messages: baseRule.meta.messages ?? {
-      initialized:
-        "Variable '{{idName}}' should be initialized on declaration.",
-      notInitialized:
-        "Variable '{{idName}}' should not be initialized on declaration.",
-    },
+    messages: baseRule.meta.messages,
   },
   defaultOptions: ['always'],
   create(context) {
