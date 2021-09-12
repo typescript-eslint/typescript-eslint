3baeb31e77d7f56f068ec8ffb5416ef4c7d532f2
Karl Horky <karl.horky@gmail.com>
2021-09-12 19:23:17 +0200

    Add start and end properties

--- packages/ast-spec/src/special/TemplateElement/spec.ts
+++ packages/ast-spec/src/special/TemplateElement/spec.ts
@@ -3,6 +3,8 @@ import type { BaseNode } from '../../base/BaseNode';
 
 export interface TemplateElement extends BaseNode {
   type: AST_NODE_TYPES.TemplateElement;
+  start: number;
+  end: number;
   value: {
     raw: string;
     cooked: string;
