9f7dc263ca6c7b6aef6f70b0737090b5e1700241
Rafael Santana <rafaelsantana3095@gmail.com>
2021-09-12 00:51:31 -0300

    fix(eslint-plugin): [no-require-imports] report only global `require`

--- packages/eslint-plugin/src/rules/no-require-imports.ts
+++ packages/eslint-plugin/src/rules/no-require-imports.ts
@@ -1,4 +1,4 @@
-import { TSESTree } from '@typescript-eslint/experimental-utils';
+import { TSESLint, TSESTree } from '@typescript-eslint/experimental-utils';
 import * as util from '../util';
 
 export default util.createRule({
@@ -18,13 +18,17 @@ export default util.createRule({
   defaultOptions: [],
   create(context) {
     return {
-      'CallExpression > Identifier[name="require"]'(
-        node: TSESTree.Identifier,
+      'CallExpression[callee.name="require"]'(
+        node: TSESTree.CallExpression,
       ): void {
-        context.report({
-          node: node.parent!,
-          messageId: 'noRequireImports',
-        });
+        const variable = getVariableByName(context.getScope(), 'require');
+
+        if (!variable?.identifiers.length) {
+          context.report({
+            node,
+            messageId: 'noRequireImports',
+          });
+        }
       },
       TSExternalModuleReference(node): void {
         context.report({
@@ -35,3 +39,28 @@ export default util.createRule({
     };
   },
 });
+
+/**
+ * Finds the variable by a given name in a given scope and its upper scopes.
+ * @param initScope A scope to start find.
+ * @param name A variable name to find.
+ * @returns A found variable or `null`.
+ */
+function getVariableByName(
+  initScope: TSESLint.Scope.Scope | null,
+  name: string,
+): TSESLint.Scope.Variable | null {
+  let scope = initScope;
+
+  while (scope) {
+    const variable = scope.set.get(name);
+
+    if (variable) {
+      return variable;
+    }
+
+    scope = scope.upper;
+  }
+
+  return null;
+}
