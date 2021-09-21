c3d54523c2772d0cdd693cf9f896b65249da06fe
Rafael Santana <rafaelsantana3095@gmail.com>
2021-09-21 01:20:31 -0300

    refactor: update from CR

--- packages/eslint-plugin/src/rules/no-require-imports.ts
+++ packages/eslint-plugin/src/rules/no-require-imports.ts
@@ -1,4 +1,4 @@
-import { TSESLint, TSESTree } from '@typescript-eslint/experimental-utils';
+import { ASTUtils, TSESTree } from '@typescript-eslint/experimental-utils';
 import * as util from '../util';
 
 export default util.createRule({
@@ -21,8 +21,10 @@ export default util.createRule({
       'CallExpression[callee.name="require"]'(
         node: TSESTree.CallExpression,
       ): void {
-        const variable = getVariableByName(context.getScope(), 'require');
+        const variable = ASTUtils.findVariable(context.getScope(), 'require');
 
+        // ignore non-global require usage as it's something userland custom instead
+        // of the commonjs standard
         if (!variable?.identifiers.length) {
           context.report({
             node,
@@ -39,28 +41,3 @@ export default util.createRule({
     };
   },
 });
-
-/**
- * Finds the variable by a given name in a given scope and its upper scopes.
- * @param initScope A scope to start find.
- * @param name A variable name to find.
- * @returns A found variable or `null`.
- */
-function getVariableByName(
-  initScope: TSESLint.Scope.Scope | null,
-  name: string,
-): TSESLint.Scope.Variable | null {
-  let scope = initScope;
-
-  while (scope) {
-    const variable = scope.set.get(name);
-
-    if (variable) {
-      return variable;
-    }
-
-    scope = scope.upper;
-  }
-
-  return null;
-}
