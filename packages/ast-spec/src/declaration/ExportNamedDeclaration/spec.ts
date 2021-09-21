cbbf22a18de07c405bf4364950c9a906f8e4b8ae
Brad Zacher <brad.zacher@gmail.com>
2021-09-20 17:56:34 -0700

    chore: cleanup after rebase

--- packages/ast-spec/src/declaration/ExportNamedDeclaration/spec.ts
+++ packages/ast-spec/src/declaration/ExportNamedDeclaration/spec.ts
@@ -1,14 +1,14 @@
 import type { AST_NODE_TYPES } from '../../ast-node-types';
 import type { BaseNode } from '../../base/BaseNode';
+import type { StringLiteral } from '../../expression/literal/StringLiteral/spec';
 import type { ExportSpecifier } from '../../special/ExportSpecifier/spec';
 import type { ExportDeclaration } from '../../unions/ExportDeclaration';
-import type { Literal } from '../../unions/Literal';
 import type { ExportKind } from '../ExportAndImportKind';
 
 export interface ExportNamedDeclaration extends BaseNode {
   type: AST_NODE_TYPES.ExportNamedDeclaration;
   declaration: ExportDeclaration | null;
   specifiers: ExportSpecifier[];
-  source: Literal | null;
+  source: StringLiteral | null;
   exportKind: ExportKind;
 }
