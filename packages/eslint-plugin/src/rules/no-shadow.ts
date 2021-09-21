431afbdb7f170dca8f5764ebe77cd116311a3d05
Rafael Santana <rafaelsantana3095@gmail.com>
2021-09-20 21:56:23 -0300

    feat(eslint-plugin-internal): [prefer-ast-types-enum] add `DefinitionType` enum

--- packages/eslint-plugin/src/rules/no-shadow.ts
+++ packages/eslint-plugin/src/rules/no-shadow.ts
@@ -1,9 +1,9 @@
 import {
-  TSESTree,
-  TSESLint,
   AST_NODE_TYPES,
+  TSESLint,
+  TSESTree,
 } from '@typescript-eslint/experimental-utils';
-import { ScopeType } from '@typescript-eslint/scope-manager';
+import { DefinitionType, ScopeType } from '@typescript-eslint/scope-manager';
 import * as util from '../util';
 
 type MessageIds = 'noShadow';
@@ -82,7 +82,10 @@ export default util.createRule<Options, MessageIds>({
      * Check if variable is a `this` parameter.
      */
     function isThisParam(variable: TSESLint.Scope.Variable): boolean {
-      return variable.defs[0].type === 'Parameter' && variable.name === 'this';
+      return (
+        variable.defs[0].type === DefinitionType.Parameter &&
+        variable.name === 'this'
+      );
     }
 
     function isTypeValueShadow(
@@ -276,7 +279,7 @@ export default util.createRule<Options, MessageIds>({
         inner &&
         outer[0] < inner[0] &&
         inner[1] < outer[1] &&
-        ((innerDef.type === 'FunctionName' &&
+        ((innerDef.type === DefinitionType.FunctionName &&
           innerDef.node.type === AST_NODE_TYPES.FunctionExpression) ||
           innerDef.node.type === AST_NODE_TYPES.ClassExpression) &&
         outerScope === innerScope.upper
