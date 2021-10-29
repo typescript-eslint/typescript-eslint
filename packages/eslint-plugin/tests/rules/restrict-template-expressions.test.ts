2c9a911a7e9b64f7b74dd34152ad2493f912a33d
sosukesuzuki <aosukeke@gmail.com>
2021-10-29 18:50:01 +0900

    test(eslint-plugin): update tests

--- packages/eslint-plugin/tests/rules/restrict-template-expressions.test.ts
+++ packages/eslint-plugin/tests/rules/restrict-template-expressions.test.ts
@@ -361,7 +361,7 @@ ruleTester.run('restrict-template-expressions', rule, {
       errors: [
         {
           messageId: 'invalidType',
-          data: { type: 'any' },
+          data: { type: 'T' },
           line: 3,
           column: 27,
         },
