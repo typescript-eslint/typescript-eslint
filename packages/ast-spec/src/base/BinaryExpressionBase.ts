04b13f68ce8f693a57b354c47f2dd5153a65f541
sosukesuzuki <aosukeke@gmail.com>
2021-10-30 23:19:45 +0900

    feat(ast-spec): allow PrivateIdentifier as a left of BinaryExpr

--- packages/ast-spec/src/base/BinaryExpressionBase.ts
+++ packages/ast-spec/src/base/BinaryExpressionBase.ts
@@ -1,8 +1,9 @@
+import type { PrivateIdentifier } from '../special/PrivateIdentifier/spec';
 import type { Expression } from '../unions/Expression';
 import type { BaseNode } from './BaseNode';
 
 export interface BinaryExpressionBase extends BaseNode {
   operator: string;
-  left: Expression;
+  left: Expression | PrivateIdentifier;
   right: Expression;
 }
