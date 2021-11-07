ecfc859373bea897f7d9193f1956a79722c9aa6e
Kaspar Vollenweider <vok@panter.ch>
2021-11-07 17:12:04 +0100

    refactor(eslint-plugin): member-ordering shorten default order definition

--- packages/eslint-plugin/src/rules/member-ordering.ts
+++ packages/eslint-plugin/src/rules/member-ordering.ts
@@ -117,133 +117,38 @@ const objectConfig = (memberTypes: MemberType[]): JSONSchema.JSONSchema4 => ({
   additionalProperties: false,
 });
 
+/**
+ * Shortcut function to build default order config
+ * @param baseType member base type to build default order for
+ * @returns array with default order for base type
+ */
+const defaultMemberTypeOrder = (baseType: BaseMemberType): MemberType[] => [
+  ...['-static', '-decorated', '-instance', '-abstract', ''].reduce(
+    (acc: string[], scopePart) =>
+      acc.concat(
+        accessabilityTypes.map(accType => `${accType}${scopePart}-${baseType}`),
+      ),
+    [],
+  ),
+  ...scopeTypes.map(scope => `${scope}-${baseType}`),
+  `decorated-${baseType}`,
+  baseType,
+];
+
 export const defaultOrder: MemberType[] = [
   // Index signature
   'signature',
   'call-signature',
 
-  // Fields
-  'public-static-field',
-  'protected-static-field',
-  'private-static-field',
-
-  'public-decorated-field',
-  'protected-decorated-field',
-  'private-decorated-field',
-
-  'public-instance-field',
-  'protected-instance-field',
-  'private-instance-field',
-
-  'public-abstract-field',
-  'protected-abstract-field',
-  'private-abstract-field',
-
-  'public-field',
-  'protected-field',
-  'private-field',
-
-  'static-field',
-  'instance-field',
-  'abstract-field',
-
-  'decorated-field',
-
-  'field',
+  ...defaultMemberTypeOrder('field'), // Fields
 
   // Constructors
-  'public-constructor',
-  'protected-constructor',
-  'private-constructor',
-
+  ...accessabilityTypes.map(accType => `${accType}-constructor`),
   'constructor',
 
-  // Getters
-  'public-static-get',
-  'protected-static-get',
-  'private-static-get',
-
-  'public-decorated-get',
-  'protected-decorated-get',
-  'private-decorated-get',
-
-  'public-instance-get',
-  'protected-instance-get',
-  'private-instance-get',
-
-  'public-abstract-get',
-  'protected-abstract-get',
-  'private-abstract-get',
-
-  'public-get',
-  'protected-get',
-  'private-get',
-
-  'static-get',
-  'instance-get',
-  'abstract-get',
-
-  'decorated-get',
-
-  'get',
-
-  // Setters
-  'public-static-set',
-  'protected-static-set',
-  'private-static-set',
-
-  'public-decorated-set',
-  'protected-decorated-set',
-  'private-decorated-set',
-
-  'public-instance-set',
-  'protected-instance-set',
-  'private-instance-set',
-
-  'public-abstract-set',
-  'protected-abstract-set',
-  'private-abstract-set',
-
-  'public-set',
-  'protected-set',
-  'private-set',
-
-  'static-set',
-  'instance-set',
-  'abstract-set',
-
-  'decorated-set',
-
-  'set',
-
-  // Methods
-  'public-static-method',
-  'protected-static-method',
-  'private-static-method',
-
-  'public-decorated-method',
-  'protected-decorated-method',
-  'private-decorated-method',
-
-  'public-instance-method',
-  'protected-instance-method',
-  'private-instance-method',
-
-  'public-abstract-method',
-  'protected-abstract-method',
-  'private-abstract-method',
-
-  'public-method',
-  'protected-method',
-  'private-method',
-
-  'static-method',
-  'instance-method',
-  'abstract-method',
-
-  'decorated-method',
-
-  'method',
+  ...defaultMemberTypeOrder('get'), // Getters
+  ...defaultMemberTypeOrder('set'), // Setters
+  ...defaultMemberTypeOrder('method'), // Methods
 ];
 
 const functionExpressions = [
