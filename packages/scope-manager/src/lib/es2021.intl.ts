1e96100f62268fc870d37d8988c0f30347bab597
Brad Zacher <brad.zacher@gmail.com>
2021-11-29 22:31:24 -0800

    feat(scope-manager): update lib types

---
+++ packages/scope-manager/src/lib/es2021.intl.ts
@@ -0,0 +1,10 @@
+// THIS CODE WAS AUTOMATICALLY GENERATED
+// DO NOT EDIT THIS CODE BY HAND
+// YOU CAN REGENERATE IT USING yarn generate:lib
+
+import { ImplicitLibVariableOptions } from '../variable';
+import { TYPE_VALUE } from './base-config';
+
+export const es2021_intl = {
+  Intl: TYPE_VALUE,
+} as Record<string, ImplicitLibVariableOptions>;
