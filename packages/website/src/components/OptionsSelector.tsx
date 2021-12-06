dd05d8a3479f2ab78ee594082163f9b70d461df6
Armano <armano2@users.noreply.github.com>
2021-12-06 19:36:53 +0100

    docs(website): use svg loader to load icons

--- packages/website/src/components/OptionsSelector.tsx
+++ packages/website/src/components/OptionsSelector.tsx
@@ -7,8 +7,8 @@ import Expander from './layout/Expander';
 import Dropdown from './inputs/Dropdown';
 import Checkbox from './inputs/Checkbox';
 import Tooltip from './inputs/Tooltip';
-import EditIcon from './icons/EditIcon';
-import CopyIcon from './icons/CopyIcon';
+import EditIcon from '@site/src/icons/edit.svg';
+import CopyIcon from '@site/src/icons/copy.svg';
 
 import { createMarkdown } from './lib/markdown';
 
@@ -167,21 +167,21 @@ function OptionsSelector({
           onClick={(): void => setEslintModal(true)}
         >
           Eslint Config
-          <EditIcon className={styles.clickableIcon} />
+          <EditIcon />
         </button>
         <button
           className={styles.optionLabel}
           onClick={(): void => setTypeScriptModal(true)}
         >
           TypeScript Config
-          <EditIcon className={styles.clickableIcon} />
+          <EditIcon />
         </button>
       </Expander>
       <Expander label="Actions">
         <button className={styles.optionLabel} onClick={copyLinkToClipboard}>
           Copy Link
           <Tooltip open={copyLink} text="Copied" close={setCopyLink}>
-            <CopyIcon className={styles.clickableIcon} />
+            <CopyIcon />
           </Tooltip>
         </button>
         <button
@@ -190,12 +190,12 @@ function OptionsSelector({
         >
           Copy Markdown
           <Tooltip open={copyMarkdown} text="Copied" close={setCopyMarkdown}>
-            <CopyIcon className={styles.clickableIcon} />
+            <CopyIcon />
           </Tooltip>
         </button>
         <button className={styles.optionLabel} onClick={openIssue}>
           Report Issue
-          <CopyIcon className={styles.clickableIcon} />
+          <CopyIcon />
         </button>
       </Expander>
     </>
--- packages/website/src/types.d.ts
+++ packages/website/src/types.d.ts
@@ -1,11 +1,19 @@
-import 'process';
+// TODO: remove after v2.0.0-beta.10
+declare module '*.svg' {
+  import type { ComponentType, SVGProps } from 'react';
+
+  const ReactComponent: ComponentType<SVGProps<SVGSVGElement>>;
+
+  // eslint-disable-next-line import/no-default-export
+  export default ReactComponent;
+}
 
 declare global {
-  namespace NodeJS {
-    interface ProcessEnv {
+  declare const process: {
+    env: {
       TS_VERSION: string;
       ESLINT_VERSION: string;
       TS_ESLINT_VERSION: string;
-    }
-  }
+    };
+  };
 }
