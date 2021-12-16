1c587714aecd75fec161c034ee07c7d1989497cb
Armano <armano2@users.noreply.github.com>
2021-12-16 12:48:29 +0100

    docs(website): print node name in scope manager

--- packages/website/src/components/ASTViewerScope.tsx
+++ packages/website/src/components/ASTViewerScope.tsx
@@ -3,10 +3,44 @@ import React, { useCallback } from 'react';
 import ASTViewer from './ast/ASTViewer';
 import type { ASTViewerBaseProps } from './ast/types';
 
+import { isRecord } from './ast/utils';
+
+export const propsToFilter = [
+  'parent',
+  'comments',
+  'tokens',
+  // 'block',
+  'upper',
+  '$id',
+];
+
+function isScopeNode(node: unknown): node is Record<string, unknown> & {
+  constructor: { name: string };
+} {
+  return (
+    isRecord(node) &&
+    'constructor' in node &&
+    node.constructor.name !== 'Object'
+  );
+}
+
+function getNodeName(value: unknown): string | undefined {
+  if (isScopeNode(value)) {
+    const name = String(value.constructor.name).replace(/\$[0-9]+$/, '');
+    if (value.$id) {
+      return `${name}$${String(value.$id)}`;
+    }
+    return name;
+  }
+  return undefined;
+}
+
 export default function ASTViewerScope(props: ASTViewerBaseProps): JSX.Element {
   const filterProps = useCallback(
     (item: [string, unknown]): boolean =>
-      !item[0].startsWith('_') && item[1] !== undefined,
+      !propsToFilter.includes(item[0]) &&
+      !item[0].startsWith('_') &&
+      item[1] !== undefined,
     [],
   );
 
@@ -14,8 +48,7 @@ export default function ASTViewerScope(props: ASTViewerBaseProps): JSX.Element {
     <ASTViewer
       filterProps={filterProps}
       getRange={(): undefined => undefined}
-      getTooltip={(): undefined => undefined}
-      getNodeName={(): undefined => undefined}
+      getNodeName={getNodeName}
       {...props}
     />
   );
