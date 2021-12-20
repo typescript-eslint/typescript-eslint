f7949b4914982538e02300fd43c8d66a8c624abe
Armano <armano2@users.noreply.github.com>
2021-12-20 04:54:20 +0100

    test: change toMatchSnapshot to toMatchInlineSnapshot

--- packages/parser/tests/lib/tsx.ts
+++ packages/parser/tests/lib/tsx.ts
@@ -1,5 +1,6 @@
 import { parseForESLint } from '../../src/parser';
 import { serializer } from '../tools/ts-error-serializer';
+import type { ParserOptions } from '@typescript-eslint/types';
 
 //------------------------------------------------------------------------------
 // Tests
@@ -7,15 +8,27 @@ import { serializer } from '../tools/ts-error-serializer';
 
 expect.addSnapshotSerializer(serializer);
 
+function parseWithError(code: string, options?: ParserOptions | null): unknown {
+  try {
+    return parseForESLint(code, options);
+  } catch (e) {
+    return e;
+  }
+}
+
 describe('TSX', () => {
   describe("if the filename ends with '.tsx', enable jsx option automatically.", () => {
     it('filePath was not provided', () => {
       const code = 'const element = <T/>';
-      try {
-        parseForESLint(code);
-      } catch (e) {
-        expect(e).toMatchSnapshot();
-      }
+
+      expect(parseWithError(code)).toMatchInlineSnapshot(`
+        TSError {
+          "column": 18,
+          "index": 18,
+          "lineNumber": 1,
+          "message": "'>' expected.",
+        }
+      `);
     });
 
     it("filePath was not provided and 'jsx:true' option", () => {
@@ -31,28 +44,38 @@ describe('TSX', () => {
 
     it('test.ts', () => {
       const code = 'const element = <T/>';
-      try {
-        parseForESLint(code, {
+      expect(
+        parseWithError(code, {
           filePath: 'test.ts',
-        });
-      } catch (e) {
-        expect(e).toMatchSnapshot();
-      }
+        }),
+      ).toMatchInlineSnapshot(`
+        TSError {
+          "column": 18,
+          "index": 18,
+          "lineNumber": 1,
+          "message": "'>' expected.",
+        }
+      `);
     });
 
     it("test.ts with 'jsx:true' option", () => {
       const code = 'const element = <T/>';
 
-      try {
-        parseForESLint(code, {
+      expect(
+        parseWithError(code, {
           filePath: 'test.ts',
           ecmaFeatures: {
             jsx: true,
           },
-        });
-      } catch (e) {
-        expect(e).toMatchSnapshot();
-      }
+        }),
+      ).toMatchInlineSnapshot(`
+        TSError {
+          "column": 18,
+          "index": 18,
+          "lineNumber": 1,
+          "message": "'>' expected.",
+        }
+      `);
     });
 
     it('test.tsx', () => {
