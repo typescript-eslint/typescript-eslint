f8ad84813a4aa3f859b68141f28afd8e03e23be4
Josh Goldberg <joshuakgoldberg@outlook.com>
2021-12-31 18:51:20 -0500

    docs(eslint-plugin): change createRule usage to point to website (#4369)

--- packages/eslint-plugin/src/util/createRule.ts
+++ packages/eslint-plugin/src/util/createRule.ts
@@ -1,10 +1,5 @@
 import { ESLintUtils } from '@typescript-eslint/experimental-utils';
 
-// note - cannot migrate this to an import statement because it will make TSC copy the package.json to the dist folder
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-const version: string = require('../../package.json').version;
-
 export const createRule = ESLintUtils.RuleCreator(
-  name =>
-    `https://github.com/typescript-eslint/typescript-eslint/blob/v${version}/packages/eslint-plugin/docs/rules/${name}.md`,
+  name => `https://typescript-eslint.io/rules/${name}`,
 );
