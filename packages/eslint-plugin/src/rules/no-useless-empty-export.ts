a67eb47ae8a4b6ca22ab8a47f65560ded35055b7
Josh Goldberg <me@joshuakgoldberg.com>
2021-12-31 17:06:25 -0500

    feat(eslint-plugin): add `no-useless-empty-export` rule

---
+++ packages/eslint-plugin/src/rules/no-useless-empty-export.ts
@@ -0,0 +1,73 @@
+import {
+  AST_NODE_TYPES,
+  TSESTree,
+} from '@typescript-eslint/experimental-utils';
+import * as util from '../util';
+
+function isEmptyExport(
+  node: TSESTree.Node,
+): node is TSESTree.ExportNamedDeclaration {
+  return (
+    node.type === AST_NODE_TYPES.ExportNamedDeclaration &&
+    node.specifiers.length === 0 &&
+    !node.declaration
+  );
+}
+
+const exportOrImportNodeTypes = new Set([
+  AST_NODE_TYPES.ExportAllDeclaration,
+  AST_NODE_TYPES.ExportDefaultDeclaration,
+  AST_NODE_TYPES.ExportNamedDeclaration,
+  AST_NODE_TYPES.ExportSpecifier,
+  AST_NODE_TYPES.ImportDeclaration,
+  AST_NODE_TYPES.TSExportAssignment,
+  AST_NODE_TYPES.TSImportEqualsDeclaration,
+]);
+
+export default util.createRule({
+  name: 'no-useless-empty-export',
+  meta: {
+    docs: {
+      description:
+        "Disallow empty exports that don't change anything in a module file",
+      recommended: false,
+      suggestion: true,
+    },
+    fixable: 'code',
+    hasSuggestions: true,
+    messages: {
+      uselessExport: 'Empty export does nothing and can be removed.',
+    },
+    schema: [],
+    type: 'suggestion',
+  },
+  defaultOptions: [],
+  create(context) {
+    return {
+      Program(node): void {
+        let emptyExport: TSESTree.ExportNamedDeclaration | undefined;
+        let foundOtherExport = false;
+
+        for (const statement of node.body) {
+          if (isEmptyExport(statement)) {
+            emptyExport = statement;
+
+            if (foundOtherExport) {
+              break;
+            }
+          } else if (exportOrImportNodeTypes.has(statement.type)) {
+            foundOtherExport = true;
+          }
+        }
+
+        if (emptyExport && foundOtherExport) {
+          context.report({
+            fix: fixer => fixer.remove(emptyExport!),
+            messageId: 'uselessExport',
+            node: emptyExport,
+          });
+        }
+      },
+    };
+  },
+});
