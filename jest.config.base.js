7e0fada0971c694a144368bf269b7063cde680bd
Josh Goldberg <me@joshuakgoldberg.com>
2022-02-22 00:08:13 -0500

    chore: use swc for tests instead of Babel

---
+++ jest.config.base.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// @ts-check
+/** @type {import('@jest/types').Config.InitialOptions} */
+module.exports = {
+  collectCoverage: false,
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+  coverageReporters: ['text-summary', 'lcov'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.(t|j)sx?$': [
+      '@swc/jest',
+      {
+        jsc: {
+          target: 'es2021',
+        },
+      },
+    ],
+  },
+};
--- tests/integration/jest.config.js
+++ tests/integration/jest.config.js
@@ -8,23 +8,7 @@ require('ts-node').register({
 const { tseslintPackages } = require('./pack-packages');
 
 module.exports = {
-  testEnvironment: 'node',
-  transform: {
-    ['^.+\\.tsx?$']: 'ts-jest',
-  },
   testRegex: ['/tests/[^/]+.test.ts$'],
-  collectCoverage: false,
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  globals: {
-    'ts-jest': {
-      isolatedModules: true,
-      diagnostics: {
-        // ignore the diagnostic error for the invalidFileErrors fixtures
-        ignoreCodes: [5056],
-      },
-    },
-    tseslintPackages,
-  },
   rootDir: __dirname,
 
   // TODO(Brad Zacher) - for some reason if we run more than 1 test at a time
