d3f4b5dfa5a06642900d9d71fdd5c869211be70b
Michaël De Boey <info@michaeldeboey.be>
2022-03-07 19:36:18 +0100

    fix: fix `Conditions` type for `isNodeOfTypeWithConditions`

--- packages/utils/src/ast-utils/helpers.ts
+++ packages/utils/src/ast-utils/helpers.ts
@@ -1,6 +1,8 @@
 import { AST_NODE_TYPES, AST_TOKEN_TYPES, TSESTree } from '../ts-estree';
 
-type ObjectEntry<BaseType> = [keyof BaseType, BaseType[keyof BaseType]];
+type ObjectEntry<BaseType> = BaseType extends unknown
+  ? [keyof BaseType, BaseType[keyof BaseType]]
+  : never;
 type ObjectEntries<BaseType> = Array<ObjectEntry<BaseType>>;
 
 export const isNodeOfType =
@@ -19,7 +21,7 @@ export const isNodeOfTypes =
 
 export const isNodeOfTypeWithConditions = <
   NodeType extends AST_NODE_TYPES,
-  Conditions extends Partial<TSESTree.Node & { type: NodeType }>,
+  Conditions extends Partial<Extract<TSESTree.Node, { type: NodeType }>>,
 >(
   nodeType: NodeType,
   conditions: Conditions,
@@ -32,7 +34,7 @@ export const isNodeOfTypeWithConditions = <
     node: TSESTree.Node | null | undefined,
   ): node is Extract<TSESTree.Node, { type: NodeType }> & Conditions =>
     node?.type === nodeType &&
-    entries.every(([key, value]) => node[key] === value);
+    entries.every(([key, value]) => key in node && node[key] === value);
 };
 
 export const isTokenOfTypeWithConditions = <
@@ -50,13 +52,13 @@ export const isTokenOfTypeWithConditions = <
     token: TSESTree.Token | null | undefined,
   ): token is Extract<TSESTree.Token, { type: TokenType }> & Conditions =>
     token?.type === tokenType &&
-    entries.every(([key, value]) => token[key] === value);
+    entries.every(([key, value]) => key in token && token[key] === value);
 };
 
 export const isNotTokenOfTypeWithConditions =
   <
     TokenType extends AST_TOKEN_TYPES,
-    Conditions extends Partial<TSESTree.Token & { type: TokenType }>,
+    Conditions extends Partial<Extract<TSESTree.Token, { type: TokenType }>>,
   >(
     tokenType: TokenType,
     conditions: Conditions,
