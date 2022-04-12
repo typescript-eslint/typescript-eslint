b4c9ac9786544e539ecc2f54ff1e9c9b31302c9b
Sosuke Suzuki <aosukeke@gmail.com>
2022-04-13 00:55:48 +0900

    feat: update to TypeScript 4.7-beta

--- packages/typescript-estree/src/parser.ts
+++ packages/typescript-estree/src/parser.ts
@@ -30,12 +30,12 @@ const log = debug('typescript-eslint:typescript-estree:parser');
  * This needs to be kept in sync with the top-level README.md in the
  * typescript-eslint monorepo
  */
-const SUPPORTED_TYPESCRIPT_VERSIONS = '>=3.3.1 <4.7.0';
+const SUPPORTED_TYPESCRIPT_VERSIONS = '>=3.3.1 <4.8.0';
 /*
  * The semver package will ignore prerelease ranges, and we don't want to explicitly document every one
  * List them all separately here, so we can automatically create the full string
  */
-const SUPPORTED_PRERELEASE_RANGES: string[] = [];
+const SUPPORTED_PRERELEASE_RANGES: string[] = ['4.7.0-beta'];
 const ACTIVE_TYPESCRIPT_VERSION = ts.version;
 const isRunningSupportedTypeScriptVersion = semver.satisfies(
   ACTIVE_TYPESCRIPT_VERSION,
