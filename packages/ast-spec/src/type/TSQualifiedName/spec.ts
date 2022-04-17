ee04ac64fe9f6c629cd4c427f35f33a95d9033c5
Sosuke Suzuki <aosukeke@gmail.com>
2022-04-17 19:49:58 +0900

    feat(ast-spec): add `PrivateIdentifier` to `TSQualifiedName.right`

--- packages/ast-spec/src/type/TSQualifiedName/spec.ts
+++ packages/ast-spec/src/type/TSQualifiedName/spec.ts
@@ -1,10 +1,11 @@
 import type { AST_NODE_TYPES } from '../../ast-node-types';
 import type { BaseNode } from '../../base/BaseNode';
 import type { Identifier } from '../../expression/Identifier/spec';
+import type { PrivateIdentifier } from '../../special/PrivateIdentifier/spec';
 import type { EntityName } from '../../unions/EntityName';
 
 export interface TSQualifiedName extends BaseNode {
   type: AST_NODE_TYPES.TSQualifiedName;
   left: EntityName;
-  right: Identifier;
+  right: Identifier | PrivateIdentifier;
 }
