16965d9811d7febd01f51969f36497b81d965014
James <5511220+Zamiell@users.noreply.github.com>
2022-05-03 17:15:34 -0400

    fix: removing semi refactor

--- packages/eslint-plugin/src/rules/semi.ts
+++ packages/eslint-plugin/src/rules/semi.ts
@@ -2,24 +2,6 @@ import { TSESTree, TSESLint, AST_NODE_TYPES } from '@typescript-eslint/utils';
 import { getESLintCoreRule } from '../util/getESLintCoreRule';
 import * as util from '../util';
 
-/**
- * The following nodes are handled by the `member-delimiter-style` rule:
- * - AST_NODE_TYPES.TSCallSignatureDeclaration
- * - AST_NODE_TYPES.TSConstructSignatureDeclaration
- * - AST_NODE_TYPES.TSIndexSignature
- * - AST_NODE_TYPES.TSMethodSignature
- * - AST_NODE_TYPES.TSPropertySignature
- */
-const SEMI_AST_NODE_TYPES = [
-  AST_NODE_TYPES.PropertyDefinition,
-  AST_NODE_TYPES.TSAbstractPropertyDefinition,
-  AST_NODE_TYPES.TSDeclareFunction,
-  AST_NODE_TYPES.TSExportAssignment,
-  AST_NODE_TYPES.TSImportEqualsDeclaration,
-  AST_NODE_TYPES.TSTypeAliasDeclaration,
-  AST_NODE_TYPES.TSEmptyBodyFunctionExpression,
-];
-
 const baseRule = getESLintCoreRule('semi');
 
 export type Options = util.InferOptionsTypeFromRule<typeof baseRule>;
@@ -56,13 +38,26 @@ export default util.createRule<Options, MessageIds>({
     const checkForSemicolon =
       rules.ExpressionStatement as TSESLint.RuleFunction<TSESTree.Node>;
 
-    const nodesToCheck = SEMI_AST_NODE_TYPES.reduce<TSESLint.RuleListener>(
-      (accumulator, node) => {
-        accumulator[node as string] = checkForSemicolon;
-        return accumulator;
-      },
-      {},
-    );
+    /*
+      The following nodes are handled by the member-delimiter-style rule
+      AST_NODE_TYPES.TSCallSignatureDeclaration,
+      AST_NODE_TYPES.TSConstructSignatureDeclaration,
+      AST_NODE_TYPES.TSIndexSignature,
+      AST_NODE_TYPES.TSMethodSignature,
+      AST_NODE_TYPES.TSPropertySignature,
+    */
+    const nodesToCheck = [
+      AST_NODE_TYPES.PropertyDefinition,
+      AST_NODE_TYPES.TSAbstractPropertyDefinition,
+      AST_NODE_TYPES.TSDeclareFunction,
+      AST_NODE_TYPES.TSExportAssignment,
+      AST_NODE_TYPES.TSImportEqualsDeclaration,
+      AST_NODE_TYPES.TSTypeAliasDeclaration,
+      AST_NODE_TYPES.TSEmptyBodyFunctionExpression,
+    ].reduce<TSESLint.RuleListener>((acc, node) => {
+      acc[node as string] = checkForSemicolon;
+      return acc;
+    }, {});
 
     return {
       ...rules,
