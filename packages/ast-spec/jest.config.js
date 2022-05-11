ac2b486a46c75d66c26e142b7f2eb107e752dcfb
Brad Zacher <brad.zacher@gmail.com>
2022-05-10 21:59:05 -0700

    chore: improve CI by making it a workflow graph

--- packages/ast-spec/jest.config.js
+++ packages/ast-spec/jest.config.js
@@ -4,10 +4,6 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
   ...require('../../jest.config.base.js'),
-  testRegex: ['./tests/.+\\.test\\.ts$'],
   collectCoverage: false,
-  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  coverageReporters: ['text-summary', 'lcov'],
   setupFilesAfterEnv: ['./tests/util/setupJest.ts'],
 };
