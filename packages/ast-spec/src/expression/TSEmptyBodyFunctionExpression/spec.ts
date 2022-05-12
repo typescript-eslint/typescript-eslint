8828ab4829cb14ec9a69b99a022084036d98682e
Brad Zacher <brad.zacher@gmail.com>
2022-05-12 13:38:48 -0700

    test(ast-spec): add fixtures for all declaration nodes

--- packages/ast-spec/src/expression/TSEmptyBodyFunctionExpression/spec.ts
+++ packages/ast-spec/src/expression/TSEmptyBodyFunctionExpression/spec.ts
@@ -1,7 +1,8 @@
 import type { AST_NODE_TYPES } from '../../ast-node-types';
-import type { FunctionDeclarationBase } from '../../base/FunctionDeclarationBase';
+import type { FunctionBase } from '../../base/FunctionBase';
 
-export interface TSEmptyBodyFunctionExpression extends FunctionDeclarationBase {
+export interface TSEmptyBodyFunctionExpression extends FunctionBase {
   type: AST_NODE_TYPES.TSEmptyBodyFunctionExpression;
   body: null;
+  id: null;
 }
