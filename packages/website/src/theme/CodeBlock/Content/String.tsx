ff80b5375516b842ec8a1484f820e542de1e9874
Joshua Chen <sidachen2003@gmail.com>
2022-05-31 16:09:26 +0800

    fix lint again

--- packages/website/src/theme/CodeBlock/Content/String.tsx
+++ packages/website/src/theme/CodeBlock/Content/String.tsx
@@ -80,6 +80,7 @@ export default function CodeBlockString({
             getTokenProps,
           }): JSX.Element => (
             <pre
+              // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
               tabIndex={0}
               ref={wordWrap.codeBlockRef}
               className={clsx(className, styles.codeBlock, 'thin-scrollbar')}
