32f1e2654034de3add18e551ac79fe19105098bf
Armano <armano2@users.noreply.github.com>
2022-06-11 00:37:42 +0200

    docs(website): [playground] remove unused import

--- packages/website/src/components/ErrorsViewer.tsx
+++ packages/website/src/components/ErrorsViewer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import type Monaco from 'monaco-editor';
 import clsx from 'clsx';
 
