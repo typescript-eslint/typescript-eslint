f20883c7ec7ba6db3d2f7e16f78a1f0efc5deb5b
Armano <armano2@users.noreply.github.com>
2022-06-16 12:46:12 +0200

    docs(website): [playground] show tooltips bellow hovered code

--- packages/website/src/components/editor/useSandboxServices.ts
+++ packages/website/src/components/editor/useSandboxServices.ts
@@ -63,6 +63,7 @@ export const useSandboxServices = (
             formatOnPaste: true,
             formatOnType: true,
             wrappingIndent: 'same',
+            hover: { above: false },
           },
           compilerOptions: compilerOptions,
           domID: editorEmbedId,
