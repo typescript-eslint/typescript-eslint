95874b38fdf770c0db9515a743ba89238c9dd0af
andrmaz <60042277+andrmaz@users.noreply.github.com>
2022-07-06 00:39:43 +0200

    docs(website): [Closes #5211] increase 'get started' button contrast color

--- packages/website/src/pages/index.tsx
+++ packages/website/src/pages/index.tsx
@@ -127,7 +127,10 @@ function Home(): JSX.Element {
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
           <div className={styles.buttons}>
-            <Link className="button button--primary" to={useBaseUrl('docs/')}>
+            <Link
+              className={clsx('button button--primary', styles.button_primary)}
+              to={useBaseUrl('docs/')}
+            >
               Get Started
             </Link>
             <Link
