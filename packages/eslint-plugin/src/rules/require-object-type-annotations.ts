0b79c5edb4a0cb99735c0f1cbc17d1d872a23edf
Oliver Joseph Ash <oliverjash@gmail.com>
2022-09-17 17:04:29 +0100

    Inline

--- packages/eslint-plugin/src/rules/require-object-type-annotations.ts
+++ packages/eslint-plugin/src/rules/require-object-type-annotations.ts
@@ -1,4 +1,3 @@
-import type { TSESTree } from '@typescript-eslint/utils';
 import type { RuleListener } from '@typescript-eslint/utils/src/ts-eslint';
 import type * as ts from 'typescript';
 
@@ -21,36 +20,35 @@ export default util.createRule({
   },
   defaultOptions: [],
   create: (context): RuleListener => {
-    const listener = (esNode: TSESTree.ObjectExpression): void => {
-      const parserServices = util.getParserServices(context);
-      const checker = parserServices.program.getTypeChecker();
-
-      const tsNode: ts.ObjectLiteralExpression =
-        parserServices.esTreeNodeToTSNodeMap.get(esNode);
-
-      const type = checker.getTypeAtLocation(tsNode);
-
-      // Allow empty objects
-      if (type.getProperties().length === 0) {
-        return;
-      }
-
-      const contextualType = checker.getContextualType(tsNode);
-
-      if (
-        contextualType === undefined ||
-        // Needed to catch object passed as a function argument where the parameter type is generic,
-        // e.g. an identity function
-        contextualType?.getSymbol()?.name === '__object'
-      ) {
-        context.report({
-          node: esNode,
-          messageId: 'forbidden',
-        });
-      }
-    };
     return {
-      ObjectExpression: listener,
+      ObjectExpression: (esNode): void => {
+        const parserServices = util.getParserServices(context);
+        const checker = parserServices.program.getTypeChecker();
+
+        const tsNode: ts.ObjectLiteralExpression =
+          parserServices.esTreeNodeToTSNodeMap.get(esNode);
+
+        const type = checker.getTypeAtLocation(tsNode);
+
+        // Allow empty objects
+        if (type.getProperties().length === 0) {
+          return;
+        }
+
+        const contextualType = checker.getContextualType(tsNode);
+
+        if (
+          contextualType === undefined ||
+          // Needed to catch object passed as a function argument where the parameter type is generic,
+          // e.g. an identity function
+          contextualType?.getSymbol()?.name === '__object'
+        ) {
+          context.report({
+            node: esNode,
+            messageId: 'forbidden',
+          });
+        }
+      },
     };
   },
 });
