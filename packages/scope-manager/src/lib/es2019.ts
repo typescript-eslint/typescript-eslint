154f540de0c2fcd28a2e85a303d3829b8a877b77
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2022-10-03 02:06:35 +0900

    fix: run `lint-fix`

--- packages/scope-manager/src/lib/es2019.ts
+++ packages/scope-manager/src/lib/es2019.ts
@@ -3,13 +3,13 @@
 // RUN THE FOLLOWING COMMAND FROM THE WORKSPACE ROOT TO REGENERATE:
 // npx nx generate-lib @typescript-eslint/scope-manager
 
-import { ImplicitLibVariableOptions } from '../variable';
+import type { ImplicitLibVariableOptions } from '../variable';
 import { es2018 } from './es2018';
 import { es2019_array } from './es2019.array';
+import { es2019_intl } from './es2019.intl';
 import { es2019_object } from './es2019.object';
 import { es2019_string } from './es2019.string';
 import { es2019_symbol } from './es2019.symbol';
-import { es2019_intl } from './es2019.intl';
 
 export const es2019 = {
   ...es2018,
