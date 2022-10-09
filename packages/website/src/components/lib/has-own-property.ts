38631bcf4ed6316b2256799cc2d21516ddf6dd9f
Josh Goldberg <git@joshuakgoldberg.com>
2022-10-09 15:02:59 -0400

    fix: hasOwnProperty

--- packages/website/src/components/lib/has-own-property.ts
+++ packages/website/src/components/lib/has-own-property.ts
@@ -1,6 +1,9 @@
-export function hasOwnProperty<Container extends object, Key extends PropertyKey>(
-  property: Y,
-  object: X,
-): object is X & Record<Y, unknown> {
+export function hasOwnProperty<
+  Container extends object,
+  Key extends PropertyKey,
+>(
+  property: Key,
+  object: Container,
+): object is Container & Record<Key, unknown> {
   return property in object;
 }
