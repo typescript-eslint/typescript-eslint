9ffdb056f77af235031a0d7bef88c67207a12795
Josh Goldberg <git@joshuakgoldberg.com>
2022-10-13 20:10:44 +0100

    chore(website): switch from Cypress to Playwright (#5814)
    * chore(website): switch from Cypress to Playwright
    
    * Touched up configs
    
    * Fixed linting
    
    * Remove remaining setup cruft
    
    * Update .github/workflows/ci.yml
    
    Co-authored-by: Brad Zacher <brad.zacher@gmail.com>
    
    Co-authored-by: Brad Zacher <brad.zacher@gmail.com>

---
+++ packages/website/playwright.config.ts
@@ -0,0 +1,29 @@
+import type { PlaywrightTestConfig } from '@playwright/test';
+import { devices } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  forbidOnly: !!process.env.CI,
+  fullyParallel: true,
+  reporter: 'html',
+  retries: 0,
+  testDir: './tests',
+  use: {
+    baseURL: 'http://localhost:3000',
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+  ],
+  webServer: {
+    command: 'yarn start',
+    port: 3000,
+  },
+  workers: process.env.CI ? 1 : undefined,
+};
+
+export default config;
