911f0ffa383e126e931164fd3c2b90b22b36f76f
Josh Goldberg <git@joshuakgoldberg.com>
2022-10-12 22:01:38 -0400

    Touched up configs

--- packages/website/tests/index.spec.ts
+++ packages/website/tests/index.spec.ts
@@ -2,6 +2,6 @@ import AxeBuilder from '@axe-core/playwright';
 import { test } from '@playwright/test';
 
 test('Index', async ({ page }) => {
-  await page.goto('http://localhost:3000');
+  await page.goto('/');
   await new AxeBuilder({ page }).analyze();
 });
