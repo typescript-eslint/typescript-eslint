3ae3756314f035a9d3a967ba9a698a2d8fd61018
Josh Goldberg <git@joshuakgoldberg.com>
2022-10-26 17:43:48 -0400

    Fixed up rules exports

--- packages/eslint-plugin/src/rules/index.ts
+++ packages/eslint-plugin/src/rules/index.ts
@@ -116,6 +116,7 @@ import restrictTemplateExpressions from './restrict-template-expressions';
 import returnAwait from './return-await';
 import semi from './semi';
 import sortTypeConstituents from './sort-type-constituents';
+import sortTypeUnionIntersectionMembers from './sort-type-union-intersection-members';
 import spaceBeforeBlocks from './space-before-blocks';
 import spaceBeforeFunctionParen from './space-before-function-paren';
 import spaceInfixOps from './space-infix-ops';
@@ -246,6 +247,7 @@ export default {
   'return-await': returnAwait,
   semi: semi,
   'sort-type-constituents': sortTypeConstituents,
+  'sort-type-union-intersection-members': sortTypeUnionIntersectionMembers,
   'space-before-blocks': spaceBeforeBlocks,
   'space-before-function-paren': spaceBeforeFunctionParen,
   'space-infix-ops': spaceInfixOps,
