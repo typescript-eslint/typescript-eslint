d653607f257410a19d3d5aaf7d5838686964cd20
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2022-11-02 19:50:02 +0900

    feat: update to typescript 4.9.2-rc

--- packages/typescript-estree/src/parseSettings/warnAboutTSVersion.ts
+++ packages/typescript-estree/src/parseSettings/warnAboutTSVersion.ts
@@ -12,7 +12,7 @@ const SUPPORTED_TYPESCRIPT_VERSIONS = '>=3.3.1 <5.0.0';
  * The semver package will ignore prerelease ranges, and we don't want to explicitly document every one
  * List them all separately here, so we can automatically create the full string
  */
-const SUPPORTED_PRERELEASE_RANGES: string[] = ['4.9.1-beta'];
+const SUPPORTED_PRERELEASE_RANGES: string[] = ['4.9.2-rc'];
 const ACTIVE_TYPESCRIPT_VERSION = ts.version;
 const isRunningSupportedTypeScriptVersion = semver.satisfies(
   ACTIVE_TYPESCRIPT_VERSION,
