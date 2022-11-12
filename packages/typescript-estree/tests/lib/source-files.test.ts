10cf6538ae6298aa50cd2a019c893a94c49c387d
Josh Goldberg <git@joshuakgoldberg.com>
2022-11-12 14:38:28 -0500

    More unit tests

--- packages/typescript-estree/tests/lib/source-files.test.ts
+++ packages/typescript-estree/tests/lib/source-files.test.ts
@@ -1,6 +1,6 @@
 import * as ts from 'typescript';
 
-import { isSourceFile } from '../../src/source-files';
+import { getCodeText, isSourceFile } from '../../src/source-files';
 
 describe('isSourceFile', () => {
   it.each([null, undefined, {}, { getFullText: (): string => '', text: '' }])(
@@ -14,5 +14,24 @@ describe('isSourceFile', () => {
     const input = ts.createSourceFile('test.ts', '', ts.ScriptTarget.ESNext);
 
     expect(isSourceFile(input)).toBe(true);
+  });
+});
+
+describe('getCodeText', () => {
+  it('returns the code when code is provided as a string', () => {
+    const code = '// Hello world';
+
+    expect(getCodeText(code)).toBe(code);
+  });
+
+  it('returns the code when code is provided as a source file', () => {
+    const code = '// Hello world';
+    const sourceFile = ts.createSourceFile(
+      'test.ts',
+      code,
+      ts.ScriptTarget.ESNext,
+    );
+
+    expect(getCodeText(sourceFile)).toBe(code);
   });
 });
