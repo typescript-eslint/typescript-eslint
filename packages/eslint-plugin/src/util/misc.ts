be144e328c99c5934cf2c407ec29ed0f25ea8864
Josh Goldberg <git@joshuakgoldberg.com>
2022-11-25 06:11:07 -0500

    Add back getEnumNames

--- packages/eslint-plugin/src/util/misc.ts
+++ packages/eslint-plugin/src/util/misc.ts
@@ -158,6 +158,10 @@ type RequireKeys<
   TKeys extends keyof TObj,
 > = ExcludeKeys<TObj, TKeys> & { [k in TKeys]-?: Exclude<TObj[k], undefined> };
 
+function getEnumNames<T extends string>(myEnum: Record<T, unknown>): T[] {
+  return Object.keys(myEnum).filter(x => isNaN(parseInt(x))) as T[];
+}
+
 /**
  * Given an array of words, returns an English-friendly concatenation, separated with commas, with
  * the `and` clause inserted before the last item.
@@ -183,6 +187,7 @@ export {
   ExcludeKeys,
   findFirstResult,
   formatWordList,
+  getEnumNames,
   getNameFromIndexSignature,
   getNameFromMember,
   isDefinitionFile,
