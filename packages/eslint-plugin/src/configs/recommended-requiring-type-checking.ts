d0211177991779bcdc08347df89051ef8df69948
Josh Goldberg <git@joshuakgoldberg.com>
2022-11-27 17:25:32 -0500

    Updated configs exports

--- packages/eslint-plugin/src/configs/recommended-requiring-type-checking.ts
+++ packages/eslint-plugin/src/configs/recommended-requiring-type-checking.ts
@@ -1,4 +1,6 @@
+import recommendedTypeChecked from './recommended-type-checked';
+
 // As of typescript-eslint@6, recommended-requiring-type-checking is now:
 // ✨ recommended-type-checked ✨
 // Please update any ESLint configurations & plugins.
-export = require('./recommended-type-checked');
+export = recommendedTypeChecked;
--- packages/eslint-plugin/src/index.ts
+++ packages/eslint-plugin/src/index.ts
@@ -2,18 +2,22 @@ import all from './configs/all';
 import base from './configs/base';
 import eslintRecommended from './configs/eslint-recommended';
 import recommended from './configs/recommended';
-import recommendedRequiringTypeChecking from './configs/recommended-type-checked';
+import recommendedRequiringTypeChecking from './configs/recommended-requiring-type-checking';
+import recommendedTypeChecked from './configs/recommended-type-checked';
 import strict from './configs/strict';
+import strictTypeChecked from './configs/strict-type-checked';
 import rules from './rules';
 
 export = {
-  rules,
   configs: {
     all,
     base,
-    recommended,
     'eslint-recommended': eslintRecommended,
-    'recommended-type-checked': recommendedRequiringTypeChecking,
+    recommended,
+    'recommended-requiring-type-checking': recommendedRequiringTypeChecking,
+    'recommended-type-checked': recommendedTypeChecked,
     strict,
+    'strict-type-checked': strictTypeChecked,
   },
+  rules,
 };
