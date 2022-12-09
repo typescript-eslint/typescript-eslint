7e8b5403d0c48b3b7beb6f0a81993ee8c942e636
Omri Luzon <omril@testim.io>
2022-12-10 00:28:50 +0200

    cleanup

--- packages/eslint-plugin/tests/rules/block-spacing.test.ts
+++ packages/eslint-plugin/tests/rules/block-spacing.test.ts
@@ -1,12 +1,7 @@
-/* eslint-disable eslint-comments/no-use */
-// this rule tests the spacing, which prettier will want to fix and break the tests
-/* eslint "@typescript-eslint/internal/plugin-test-formatting": ["error", { formatWithPrettier: false }] */
-/* eslint-enable eslint-comments/no-use */
-import { AST_NODE_TYPES, TSESLint } from '@typescript-eslint/utils';
+import { AST_NODE_TYPES } from '@typescript-eslint/utils';
 
-import type { MessageIds, Options } from '../../src/rules/block-spacing';
 import rule from '../../src/rules/block-spacing';
-import type { InvalidTestCase } from '../RuleTester';
+import type { InvalidTestCase, ValidTestCase } from '../RuleTester';
 import { RuleTester } from '../RuleTester';
 
 const ruleTester = new RuleTester({
@@ -18,7 +13,7 @@ type InvalidBlockSpacingTestCase = InvalidTestCase<
   ['always' | 'never']
 >;
 
-const options = [['always'], ['never']] as const;
+const options = ['always', 'never'] as const;
 const typeDeclaration = [
   {
     nodeType: AST_NODE_TYPES.TSInterfaceBody,
@@ -35,44 +30,41 @@ const singlePropertyBlocks = ['{bar: true}', '{ bar: true }'];
 ruleTester.run('block-spacing', rule, {
   valid: [
     // Empty blocks don't apply
-    ...options.flatMap(options =>
+    ...options.flatMap(option =>
       typeDeclaration.flatMap(typeDec =>
-        emptyBlocks.map(blockType => ({
+        emptyBlocks.map<ValidTestCase<['always' | 'never']>>(blockType => ({
           code: typeDec.stringPrefix + blockType,
-          options,
+          options: [option],
         })),
       ),
     ),
   ],
   invalid: [
-    ...options.flatMap(options =>
+    ...options.flatMap(option =>
       typeDeclaration.flatMap(typeDec =>
         singlePropertyBlocks.flatMap<InvalidBlockSpacingTestCase>(
           (blockType, blockIndex) => {
+            // These are actually valid, so filter them out
             if (
-              (options[0] === 'always' && blockType.startsWith('{ ')) ||
-              (options[0] === 'never' && blockType.startsWith('{bar'))
+              (option === 'always' && blockType.startsWith('{ ')) ||
+              (option === 'never' && blockType.startsWith('{bar'))
             ) {
               return [];
             }
             return {
               code: typeDec.stringPrefix + blockType,
-              options,
+              options: [option],
               output:
                 typeDec.stringPrefix + singlePropertyBlocks[1 - blockIndex],
               errors: [
                 {
                   type: typeDec.nodeType,
-                  // line: 1,
-                  // column: 1,
-                  messageId: options[0] === 'always' ? 'missing' : 'extra',
+                  messageId: option === 'always' ? 'missing' : 'extra',
                   data: { location: 'after', token: '{' },
                 },
                 {
                   type: typeDec.nodeType,
-                  // line: 1,
-                  // column: 8,
-                  messageId: options[0] === 'always' ? 'missing' : 'extra',
+                  messageId: option === 'always' ? 'missing' : 'extra',
                   data: { location: 'before', token: '}' },
                 },
               ],
