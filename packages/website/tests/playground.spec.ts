f6fa30977e79984fa2b45c224adea97c3a53c36d
Tapan Prakash <tapanprakasht@gmail.com>
2022-12-25 22:33:32 +0530

    test(website): Add AST viewer test case

--- packages/website/tests/playground.spec.ts
+++ packages/website/tests/playground.spec.ts
@@ -46,6 +46,19 @@ test.describe('Playground', () => {
     await expect(page.getByText('let value: string[];')).toBeVisible();
     await expect(page.getByText('All is ok!')).toBeVisible();
   });
+
+  test('AST Viewer', async ({ page }) => {
+    // 1. Type some valid code in the playground
+    await writeInEditor(page, 'let value: Array<string>;');
+    // 2. Enable AST viewer
+    await page
+      .getByRole('combobox', { name: 'AST Viewer' })
+      .selectOption({ label: 'ESTree' });
+    // 3. Validate variable declaration block exist in AST viewer
+    await expect(
+      page.getByRole('link', { name: 'VariableDeclaration' }),
+    ).toBeVisible();
+  });
 });
 
 async function writeInEditor(page: Page, text: string): Promise<void> {
