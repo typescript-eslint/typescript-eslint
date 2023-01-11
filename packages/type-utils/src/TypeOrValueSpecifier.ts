5c3f37f1232175f02feeab013cb3d9bb5a77d346
Marek Dědič <developer@dedic.eu>
2023-01-11 16:07:19 +0100

    feat(type-utils): Removed MultiSourceSpecifier

--- packages/type-utils/src/TypeOrValueSpecifier.ts
+++ packages/type-utils/src/TypeOrValueSpecifier.ts
@@ -18,17 +18,11 @@ interface PackageSpecifier {
   source: string;
 }
 
-interface MultiSourceSpecifier {
-  from: Array<'file' | 'lib' | 'package'>;
-  name: string;
-}
-
 export type TypeOrValueSpecifier =
   | string
   | FileSpecifier
   | LibSpecifier
-  | PackageSpecifier
-  | MultiSourceSpecifier;
+  | PackageSpecifier;
 
 export const typeOrValueSpecifierSchema = {
   oneOf: [
@@ -141,12 +135,6 @@ export const typeOrValueSpecifierSchema = {
   ],
 };
 
-function isMultiSourceSpecifier(
-  specifier: TypeOrValueSpecifier,
-): specifier is MultiSourceSpecifier {
-  return typeof specifier === 'object' && Array.isArray(specifier.from);
-}
-
 function specifierNameMatches(
   type: ts.Type,
   name: string | Array<string>,
@@ -199,40 +187,6 @@ function typeMatchesPackageSpecifier(
   );
 }
 
-function typeMatchesMultiSourceSpecifier(
-  specifier: MultiSourceSpecifier,
-  declarationFiles: Array<ts.SourceFile>,
-  program: ts.Program,
-): boolean {
-  if (
-    specifier.from.includes('file') &&
-    typeMatchesFileSpecifier(
-      { from: 'file', name: specifier.name },
-      declarationFiles,
-      program,
-    )
-  ) {
-    return true;
-  }
-  if (
-    specifier.from.includes('lib') &&
-    typeIsFromLib(declarationFiles, program)
-  ) {
-    return true;
-  }
-  if (
-    specifier.from.includes('package') &&
-    typeMatchesPackageSpecifier(
-      // TODO: Solve what to do with the source.
-      { from: 'package', name: specifier.name, source: '' },
-      declarationFiles,
-    )
-  ) {
-    return true;
-  }
-  return false;
-}
-
 export function typeMatchesSpecifier(
   type: ts.Type,
   specifier: TypeOrValueSpecifier,
@@ -249,13 +203,6 @@ export function typeMatchesSpecifier(
       .getSymbol()
       ?.getDeclarations()
       ?.map(declaration => declaration.getSourceFile()) ?? [];
-  if (isMultiSourceSpecifier(specifier)) {
-    return typeMatchesMultiSourceSpecifier(
-      specifier,
-      declarationFiles,
-      program,
-    );
-  }
   switch (specifier.from) {
     case 'file':
       return typeMatchesFileSpecifier(specifier, declarationFiles, program);
