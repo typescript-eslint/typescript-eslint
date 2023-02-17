9fe72ef3689524405e9e8d3d4f8ca4508abea71d
Brad Zacher <brad.zacher@gmail.com>
2023-02-16 22:46:35 -0800

    Update clear-caches.ts

--- packages/typescript-estree/src/clear-caches.ts
+++ packages/typescript-estree/src/clear-caches.ts
@@ -5,7 +5,10 @@ import { clearGlobCache } from './parseSettings/resolveProjectList';
 
 /**
  * Clears all of the internal caches.
- * Generally you shouldn't need or want to use this
+ * Generally you shouldn't need or want to use this.
+ * Examples of intended uses:
+ * - In tests to reset parser state to keep tests isolated.
+ * - In custom lint tooling that iteratively lints one project at a time to prevent OOMs. 
  */
 export function clearCaches(): void {
   clearProgramCacheOriginal();
