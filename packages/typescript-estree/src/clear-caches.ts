5359a6860cfea9b7f8b621fb810ec69676ddce82
Brad Zacher <brad.zacher@gmail.com>
2023-02-20 17:28:52 +1030

    format

--- packages/typescript-estree/src/clear-caches.ts
+++ packages/typescript-estree/src/clear-caches.ts
@@ -8,7 +8,7 @@ import { clearGlobCache } from './parseSettings/resolveProjectList';
  * Generally you shouldn't need or want to use this.
  * Examples of intended uses:
  * - In tests to reset parser state to keep tests isolated.
- * - In custom lint tooling that iteratively lints one project at a time to prevent OOMs. 
+ * - In custom lint tooling that iteratively lints one project at a time to prevent OOMs.
  */
 export function clearCaches(): void {
   clearProgramCacheOriginal();
