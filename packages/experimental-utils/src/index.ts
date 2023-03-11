1c1367076abaa02152f53d5b15414940de2c2804
Josh Goldberg <git@joshuakgoldberg.com>
2023-03-11 18:11:45 -0500

    Revert "feat: remove `experimental-utils` (#6468)"
    This reverts commit 71adbc5119fa55c29d55747a64e9f4e178374c3c.

---
+++ packages/experimental-utils/src/index.ts
@@ -0,0 +1,8 @@
+/* eslint-disable no-console */
+console.warn(
+  'This package is purely a re-export of `@typescript-eslint/utils`.',
+);
+console.warn(
+  'You should switch to importing from that non-experimental package instead.',
+);
+export * from '@typescript-eslint/utils';
