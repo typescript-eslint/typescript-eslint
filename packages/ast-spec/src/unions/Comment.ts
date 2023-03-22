cf17e7a57dd4f1b4683e0e155213512d8c5f17f1
fisker <lionkay@gmail.com>
2023-03-22 10:49:31 +0800

    Minor tweak

--- packages/ast-spec/src/unions/Comment.ts
+++ packages/ast-spec/src/unions/Comment.ts
@@ -1,4 +1,5 @@
 import type { BlockComment } from '../token/BlockComment/spec';
+import type { HashbangComment } from '../token/HashbangComment/spec';
 import type { LineComment } from '../token/LineComment/spec';
 
-export type Comment = BlockComment | LineComment;
+export type Comment = BlockComment | HashbangComment | LineComment;
