2560d89c3a7eaef869e56006360d3298cfe77f14
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2023-04-03 14:02:03 -0700

    chore: Switch to ESLint flat config

---
+++ packages/website/eslint.config.js
@@ -0,0 +1,47 @@
+const baseConfig = require('../../eslint.config.js');
+const jsxA11y = require('eslint-plugin-jsx-a11y');
+const react = require('eslint-plugin-react');
+const reactHooks = require('eslint-plugin-react-hooks');
+const { FlatCompat } = require('@eslint/eslintrc');
+
+const eslintrc = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+module.exports = [
+  ...baseConfig,
+  ...eslintrc.extends([
+    'plugin:jsx-a11y/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ]),
+  {
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'jsx-a11y': jsxA11y,
+    },
+    rules: {
+      'react/jsx-no-target-blank': 'off',
+      'react/no-unescaped-entities': 'off',
+      '@typescript-eslint/internal/prefer-ast-types-enum': 'off',
+      'react/jsx-curly-brace-presence': 'error',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  {
+    files: [
+      './*.config.*',
+      './src/pages/*.tsx',
+      './src/components/**/*.tsx',
+      './src/components/hooks/*.ts',
+    ],
+    rules: {
+      'import/no-default-export': 'off',
+    },
+  },
+];
