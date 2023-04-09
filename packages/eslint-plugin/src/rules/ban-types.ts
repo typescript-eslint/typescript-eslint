ff749dd8cf3b70ffdd69b61b7afc73cf54f2c516
Tiger Oakes <contact@tigeroakes.com>
2023-04-09 15:12:29 -0700

    Update ban-types error message

--- packages/eslint-plugin/src/rules/ban-types.ts
+++ packages/eslint-plugin/src/rules/ban-types.ts
@@ -88,6 +88,7 @@ const defaultTypes: Types = {
       'The `Object` type actually means "any non-nullish value", so it is marginally better than `unknown`.',
       '- If you want a type meaning "any object", you probably want `object` instead.',
       '- If you want a type meaning "any value", you probably want `unknown` instead.',
+      '- If you really want a type meaning "any non-nullish value", you probably want `NonNullable<unknown>` instead.',
     ].join('\n'),
   },
   '{}': {
@@ -96,6 +97,7 @@ const defaultTypes: Types = {
       '- If you want a type meaning "any object", you probably want `object` instead.',
       '- If you want a type meaning "any value", you probably want `unknown` instead.',
       '- If you want a type meaning "empty object", you probably want `Record<string, never>` instead.',
+      '- If you really want a type meaning "any non-nullish value", you probably want `NonNullable<unknown>` instead.',
     ].join('\n'),
   },
 };
