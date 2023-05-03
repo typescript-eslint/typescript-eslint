00ddecdcce6c56b6d71fe1b515bbe91a80dc53cd
Josh Goldberg <git@joshuakgoldberg.com>
2023-05-03 18:19:41 -0400

    rename: tsserverType -> ts

--- packages/typescript-estree/src/create-program/createProjectService.ts
+++ packages/typescript-estree/src/create-program/createProjectService.ts
@@ -1,21 +1,20 @@
-import type * as tsserverType from 'typescript/lib/tsserverlibrary';
+import type * as ts from 'typescript/lib/tsserverlibrary';
 
 const doNothing = (): void => {};
 
-const createStubFileWatcher = (): tsserverType.FileWatcher => ({
+const createStubFileWatcher = (): ts.FileWatcher => ({
   close: doNothing,
 });
 
-export function createProjectService(): tsserverType.server.ProjectService {
+export function createProjectService(): ts.server.ProjectService {
   // We import this lazily to avoid its cost for users who don't use the service
-  const tsserver =
-    require('typescript/lib/tsserverlibrary') as typeof tsserverType;
+  const tsserver = require('typescript/lib/tsserverlibrary') as typeof ts;
 
   // TODO: see getWatchProgramsForProjects
   // We don't watch the disk, we just refer to these when ESLint calls us
   // there's a whole separate update pass in maybeInvalidateProgram at the bottom of getWatchProgramsForProjects
   // (this "goes nuclear on TypeScript")
-  const system: tsserverType.server.ServerHost = {
+  const system: ts.server.ServerHost = {
     ...tsserver.sys,
     clearImmediate,
     clearTimeout,
