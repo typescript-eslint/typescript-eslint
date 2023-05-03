37d3548c85e691946d3ac4f52ff5073a87aee8dc
Josh Goldberg <git@joshuakgoldberg.com>
2023-05-03 17:58:17 -0400

    Move tsserver import to a require

--- packages/typescript-estree/src/create-program/createProjectService.ts
+++ packages/typescript-estree/src/create-program/createProjectService.ts
@@ -1,17 +1,21 @@
-import * as tsserver from 'typescript/lib/tsserverlibrary';
+import type * as tsserverType from 'typescript/lib/tsserverlibrary';
 
 const doNothing = (): void => {};
 
-const createStubFileWatcher = (): tsserver.FileWatcher => ({
+const createStubFileWatcher = (): tsserverType.FileWatcher => ({
   close: doNothing,
 });
 
-export function createProjectService(): tsserver.server.ProjectService {
+export function createProjectService(): tsserverType.server.ProjectService {
+  // We import this lazily to avoid its cost for users who don't use the service
+  const tsserver =
+    require('typescript/lib/tsserverlibrary') as typeof tsserverType;
+
   // TODO: see getWatchProgramsForProjects
   // We don't watch the disk, we just refer to these when ESLint calls us
   // there's a whole separate update pass in maybeInvalidateProgram at the bottom of getWatchProgramsForProjects
   // (this "goes nuclear on TypeScript")
-  const system: tsserver.server.ServerHost = {
+  const system: tsserverType.server.ServerHost = {
     ...tsserver.sys,
     clearImmediate,
     clearTimeout,
