3d812c69909794f8ede28c717cf86fbae5cabf63
Melanie Calame <melanie.calame@gmail.com>
2023-05-31 13:22:17 +0200

    fix: extend regex to replace if statement

--- packages/eslint-plugin/src/rules/no-unsafe-enum-comparison.ts
+++ packages/eslint-plugin/src/rules/no-unsafe-enum-comparison.ts
@@ -63,22 +63,9 @@ export default util.createRule({
     }
 
     return {
-      'BinaryExpression[operator=/=|<|>/]'(
+      'BinaryExpression[operator=/^[<>!]?={0,3}$/]'(
         node: TSESTree.BinaryExpression,
       ): void {
-        // Exempt bit shift operators since they are not comparisons
-        //
-        // ```ts
-        // enum Fruit {
-        //  Apple,
-        // }
-        //
-        // const myBitShift = 1 << Fruit.Apple;
-        // ```
-        if (node.operator === '>>' || node.operator === '<<') {
-          return;
-        }
-
         const left = getTypeFromNode(node.left);
         const right = getTypeFromNode(node.right);
 
