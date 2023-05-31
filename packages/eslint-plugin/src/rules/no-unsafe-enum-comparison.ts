68938c58a958a34ad1e102047a937a3a4be8dc3a
Melanie Calame <melanie.calame@gmail.com>
2023-05-31 21:35:43 +0200

    fix: use more precise regex

--- packages/eslint-plugin/src/rules/no-unsafe-enum-comparison.ts
+++ packages/eslint-plugin/src/rules/no-unsafe-enum-comparison.ts
@@ -63,7 +63,7 @@ export default util.createRule({
     }
 
     return {
-      'BinaryExpression[operator=/^[<>!]?={0,3}$/]'(
+      'BinaryExpression[operator=/^[<>!=]?={0,2}$/]'(
         node: TSESTree.BinaryExpression,
       ): void {
         const left = getTypeFromNode(node.left);
