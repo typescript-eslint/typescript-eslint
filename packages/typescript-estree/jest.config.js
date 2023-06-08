5bfbdacd197a95ffd57d63ec38e9b1af498d2469
Josh Goldberg <git@joshuakgoldberg.com>
2023-06-08 14:14:13 +1000

    Remove jest.config.js shenanigans

--- packages/typescript-estree/jest.config.js
+++ packages/typescript-estree/jest.config.js
@@ -1,19 +1,8 @@
 'use strict';
 // @ts-check
-
-console.log(
-  'Running with',
-  process.version,
-  'which startsWith:',
-  process.version.startsWith('14'),
-);
-
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
   ...require('../../jest.config.base.js'),
-  testPathIgnorePatterns: process.version.startsWith('14')
-    ? ['/node_modules/', '/parse\\.moduleResolver/']
-    : ['/node_modules/'],
   testRegex: [
     './tests/lib/.*\\.ts$',
     './tests/ast-alignment/spec\\.ts$',
