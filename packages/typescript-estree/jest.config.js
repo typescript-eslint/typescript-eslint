50a51aea7aaf2ac79b744985eed51fcb71e4a1e0
Josh Goldberg <git@joshuakgoldberg.com>
2023-06-08 11:16:50 +1000

    more logging on process version

--- packages/typescript-estree/jest.config.js
+++ packages/typescript-estree/jest.config.js
@@ -1,11 +1,18 @@
 'use strict';
-
 // @ts-check
+
+console.log(
+  'Running with',
+  process.version,
+  'which startsWith:',
+  process.version.startsWith('14'),
+);
+
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
   ...require('../../jest.config.base.js'),
   testPathIgnorePatterns: process.version.startsWith('14')
-    ? ['/node_modules/', '/parse.moduleResolver/']
+    ? ['/node_modules/', '/parse\\.moduleResolver/']
     : ['/node_modules/'],
   testRegex: [
     './tests/lib/.*\\.ts$',
