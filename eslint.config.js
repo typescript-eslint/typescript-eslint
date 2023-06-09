009d29cbbc34e590ed167a4df99c5584188360a2
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2023-06-09 13:53:26 -0400

    Fixup config file to remove overrides

--- eslint.config.js
+++ eslint.config.js
@@ -245,178 +245,177 @@ module.exports = [
 
       'unicorn/no-typeof-undefined': 'error',
     },
-    overrides: [
-      // all test files
-      {
-        files: [
-          './packages/*/tests/**/*.spec.ts',
-          './packages/*/tests/**/*.test.ts',
-          './packages/*/tests/**/spec.ts',
-          './packages/*/tests/**/test.ts',
-          './packages/parser/tests/**/*.ts',
-          './tests/integration/**/*.test.ts',
-          './tests/integration/integration-test-base.ts',
-          './tests/integration/pack-packages.ts',
-        ],
-        env: {
-          'jest/globals': true,
-        },
-        rules: {
-          '@typescript-eslint/no-unsafe-assignment': 'off',
-          '@typescript-eslint/no-unsafe-call': 'off',
-          '@typescript-eslint/no-unsafe-member-access': 'off',
-          '@typescript-eslint/no-unsafe-return': 'off',
-          'eslint-plugin/consistent-output': 'off', // Might eventually be removed from `eslint-plugin/recommended`: https://github.com/not-an-aardvark/eslint-plugin-eslint-plugin/issues/284
-          'jest/no-disabled-tests': 'warn',
-          'jest/no-focused-tests': 'error',
-          'jest/no-alias-methods': 'error',
-          'jest/no-identical-title': 'error',
-          'jest/no-jasmine-globals': 'error',
-          'jest/no-test-prefixes': 'error',
-          'jest/no-done-callback': 'error',
-          'jest/no-test-return-statement': 'error',
-          'jest/prefer-to-be': 'warn',
-          'jest/prefer-to-contain': 'warn',
-          'jest/prefer-to-have-length': 'warn',
-          'jest/prefer-spy-on': 'error',
-          'jest/valid-expect': 'error',
-          'jest/no-deprecated-functions': 'error',
-        },
-      },
-      // test utility scripts and website js files
-      {
-        files: ['tests/**/*.js'],
-        rules: {
-          '@typescript-eslint/explicit-function-return-type': 'off',
-          '@typescript-eslint/no-unsafe-call': 'off',
-          '@typescript-eslint/no-unsafe-member-access': 'off',
-          '@typescript-eslint/no-unsafe-return': 'off',
-          '@typescript-eslint/restrict-plus-operands': 'off',
-        },
-      },
-      // plugin source files
-      {
-        files: [
-          './packages/eslint-plugin-internal/**/*.ts',
-          './packages/eslint-plugin-tslint/**/*.ts',
-          './packages/eslint-plugin/**/*.ts',
-        ],
-        rules: {
-          '@typescript-eslint/internal/no-typescript-estree-import': 'error',
-        },
-      },
-      // plugin rule source files
-      {
-        files: [
-          './packages/eslint-plugin-internal/src/rules/**/*.ts',
-          './packages/eslint-plugin-tslint/src/rules/**/*.ts',
-          './packages/eslint-plugin/src/configs/**/*.ts',
-          './packages/eslint-plugin/src/rules/**/*.ts',
-        ],
-        rules: {
-          'eslint-plugin/require-meta-docs-description': [
-            'error',
-            { pattern: '^(Enforce|Require|Disallow) .+[^. ]$' },
-          ],
+  },
 
-          // specifically for rules - default exports makes the tooling easier
-          'import/no-default-export': 'off',
-
-          'no-restricted-syntax': [
-            'error',
-            {
-              selector:
-                'ExportDefaultDeclaration Property[key.name="create"] MemberExpression[object.name="context"][property.name="options"]',
-              message:
-                "Retrieve options from create's second parameter so that defaultOptions are applied.",
-            },
-          ],
-        },
-      },
-      // plugin rule tests
-      {
-        files: [
-          './packages/eslint-plugin-internal/tests/rules/**/*.test.ts',
-          './packages/eslint-plugin-tslint/tests/rules/**/*.test.ts',
-          './packages/eslint-plugin/tests/rules/**/*.test.ts',
-          './packages/eslint-plugin/tests/eslint-rules/**/*.test.ts',
-        ],
-        rules: {
-          '@typescript-eslint/internal/plugin-test-formatting': 'error',
-        },
-      },
-      // files which list all the things
-      {
-        files: ['./packages/eslint-plugin/src/rules/index.ts'],
-        rules: {
-          // enforce alphabetical ordering
-          'sort-keys': 'error',
-          'import/order': ['error', { alphabetize: { order: 'asc' } }],
-        },
-      },
-      // tools and tests
-      {
-        files: ['**/tools/**/*.ts', '**/tests/**/*.ts'],
-        rules: {
-          // allow console logs in tools and tests
-          'no-console': 'off',
-        },
-      },
-      // generated files
-      {
-        files: [
-          './packages/scope-manager/src/lib/*.ts',
-          './packages/eslint-plugin/src/configs/*.ts',
-        ],
-        rules: {
-          '@typescript-eslint/internal/no-poorly-typed-ts-props': 'off',
-          '@typescript-eslint/internal/no-typescript-default-import': 'off',
-          '@typescript-eslint/internal/prefer-ast-types-enum': 'off',
-        },
-      },
-      // ast spec specific standardization
-      {
-        files: ['./packages/ast-spec/src/**/*.ts'],
-        rules: {
-          // disallow ALL unused vars
-          '@typescript-eslint/no-unused-vars': 'error',
-          '@typescript-eslint/sort-type-constituents': 'error',
-        },
-      },
-      {
-        files: ['rollup.config.ts'],
-        rules: {
-          'import/no-default-export': 'off',
-        },
-      },
-      {
-        files: ['./packages/website/'],
-        extends: [
-          'plugin:jsx-a11y/recommended',
-          'plugin:react/recommended',
-          'plugin:react-hooks/recommended',
-        ],
-        plugins: ['jsx-a11y', 'react', 'react-hooks'],
-        rules: {
-          'react/jsx-no-target-blank': 'off',
-          'react/no-unescaped-entities': 'off',
-          '@typescript-eslint/internal/prefer-ast-types-enum': 'off',
-          'react-hooks/exhaustive-deps': 'off', // TODO: enable it later
-        },
-        settings: {
-          react: {
-            version: 'detect',
-          },
-        },
-      },
-      {
-        files: ['./packages/website/src/**/*.{ts,tsx}'],
-        rules: {
-          'import/no-default-export': 'off',
-          // allow console logs in the website to help with debugging things in production
-          'no-console': 'off',
-        },
-      },
+  // all test files
+  {
+    files: [
+      './packages/*/tests/**/*.spec.ts',
+      './packages/*/tests/**/*.test.ts',
+      './packages/*/tests/**/spec.ts',
+      './packages/*/tests/**/test.ts',
+      './packages/parser/tests/**/*.ts',
+      './tests/integration/**/*.test.ts',
+      './tests/integration/integration-test-base.ts',
+      './tests/integration/pack-packages.ts',
     ],
+    languageOptions: {
+      globals: jest.environments.globals.globals,
+    },
+    rules: {
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      'eslint-plugin/consistent-output': 'off', // Might eventually be removed from `eslint-plugin/recommended`: https://github.com/not-an-aardvark/eslint-plugin-eslint-plugin/issues/284
+      'jest/no-disabled-tests': 'warn',
+      'jest/no-focused-tests': 'error',
+      'jest/no-alias-methods': 'error',
+      'jest/no-identical-title': 'error',
+      'jest/no-jasmine-globals': 'error',
+      'jest/no-test-prefixes': 'error',
+      'jest/no-done-callback': 'error',
+      'jest/no-test-return-statement': 'error',
+      'jest/prefer-to-be': 'warn',
+      'jest/prefer-to-contain': 'warn',
+      'jest/prefer-to-have-length': 'warn',
+      'jest/prefer-spy-on': 'error',
+      'jest/valid-expect': 'error',
+      'jest/no-deprecated-functions': 'error',
+    },
+  },
+  // test utility scripts and website js files
+  {
+    files: ['tests/**/*.js'],
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/restrict-plus-operands': 'off',
+    },
+  },
+  // plugin source files
+  {
+    files: [
+      './packages/eslint-plugin-internal/**/*.ts',
+      './packages/eslint-plugin-tslint/**/*.ts',
+      './packages/eslint-plugin/**/*.ts',
+    ],
+    rules: {
+      '@typescript-eslint/internal/no-typescript-estree-import': 'error',
+    },
+  },
+  // plugin rule source files
+  {
+    files: [
+      './packages/eslint-plugin-internal/src/rules/**/*.ts',
+      './packages/eslint-plugin-tslint/src/rules/**/*.ts',
+      './packages/eslint-plugin/src/configs/**/*.ts',
+      './packages/eslint-plugin/src/rules/**/*.ts',
+    ],
+    rules: {
+      'eslint-plugin/require-meta-docs-description': [
+        'error',
+        { pattern: '^(Enforce|Require|Disallow) .+[^. ]$' },
+      ],
+
+      // specifically for rules - default exports makes the tooling easier
+      'import/no-default-export': 'off',
+
+      'no-restricted-syntax': [
+        'error',
+        {
+          selector:
+            'ExportDefaultDeclaration Property[key.name="create"] MemberExpression[object.name="context"][property.name="options"]',
+          message:
+            "Retrieve options from create's second parameter so that defaultOptions are applied.",
+        },
+      ],
+    },
+  },
+  // plugin rule tests
+  {
+    files: [
+      './packages/eslint-plugin-internal/tests/rules/**/*.test.ts',
+      './packages/eslint-plugin-tslint/tests/rules/**/*.test.ts',
+      './packages/eslint-plugin/tests/rules/**/*.test.ts',
+      './packages/eslint-plugin/tests/eslint-rules/**/*.test.ts',
+    ],
+    rules: {
+      '@typescript-eslint/internal/plugin-test-formatting': 'error',
+    },
+  },
+  // files which list all the things
+  {
+    files: ['./packages/eslint-plugin/src/rules/index.ts'],
+    rules: {
+      // enforce alphabetical ordering
+      'sort-keys': 'error',
+      'import/order': ['error', { alphabetize: { order: 'asc' } }],
+    },
+  },
+  // tools and tests
+  {
+    files: ['**/tools/**/*.ts', '**/tests/**/*.ts'],
+    rules: {
+      // allow console logs in tools and tests
+      'no-console': 'off',
+    },
+  },
+  // generated files
+  {
+    files: [
+      './packages/scope-manager/src/lib/*.ts',
+      './packages/eslint-plugin/src/configs/*.ts',
+    ],
+    rules: {
+      '@typescript-eslint/internal/no-poorly-typed-ts-props': 'off',
+      '@typescript-eslint/internal/no-typescript-default-import': 'off',
+      '@typescript-eslint/internal/prefer-ast-types-enum': 'off',
+    },
+  },
+  // ast spec specific standardization
+  {
+    files: ['./packages/ast-spec/src/**/*.ts'],
+    rules: {
+      // disallow ALL unused vars
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/sort-type-constituents': 'error',
+    },
+  },
+  {
+    files: ['rollup.config.ts'],
+    rules: {
+      'import/no-default-export': 'off',
+    },
+  },
+  {
+    files: ['./packages/website/'],
+    extends: [
+      'plugin:jsx-a11y/recommended',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
+    ],
+    plugins: ['jsx-a11y', 'react', 'react-hooks'],
+    rules: {
+      'react/jsx-no-target-blank': 'off',
+      'react/no-unescaped-entities': 'off',
+      '@typescript-eslint/internal/prefer-ast-types-enum': 'off',
+      'react-hooks/exhaustive-deps': 'off', // TODO: enable it later
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  {
+    files: ['./packages/website/src/**/*.{ts,tsx}'],
+    rules: {
+      'import/no-default-export': 'off',
+      // allow console logs in the website to help with debugging things in production
+      'no-console': 'off',
+    },
   },
 ];
