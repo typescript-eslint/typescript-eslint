41d68ea338044e7d199d4d915205c1ff83abb24b
Josh Goldberg <git@joshuakgoldberg.com>
2023-07-06 18:07:08 -0400

    ts-expect-error for #7166

--- packages/ast-spec/src/jsx/JSXNamespacedName/fixtures/component-dashed/fixture.tsx
+++ packages/ast-spec/src/jsx/JSXNamespacedName/fixtures/component-dashed/fixture.tsx
@@ -5,5 +5,7 @@ declare namespace JSX {
   }
 }
 
+// @ts-expect-error -- https://github.com/typescript-eslint/typescript-eslint/issues/7166
 const componentBasic = <foo />;
+// @ts-expect-error -- https://github.com/typescript-eslint/typescript-eslint/issues/7166
 const componentDashed = <foo-bar:baz-bam />;
