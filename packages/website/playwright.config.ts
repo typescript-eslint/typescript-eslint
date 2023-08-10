5f07b1b40f3ea920568273681834b772f53dcd4e
rubiesonthesky <2591240+rubiesonthesky@users.noreply.github.com>
2023-08-10 10:47:15 +0300

    Revert retries change

--- packages/website/playwright.config.ts
+++ packages/website/playwright.config.ts
@@ -5,7 +5,7 @@ const config: PlaywrightTestConfig = {
   forbidOnly: !!process.env.CI,
   fullyParallel: true,
   reporter: 'html',
-  retries: 1,
+  retries: 0,
   testDir: './tests',
   use: {
     baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL ?? 'http://localhost:3000',
