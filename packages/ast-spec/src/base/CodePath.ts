e20db45ed10acbc16b7d26d2dd12f5d5501ab7c4
James <5511220+Zamiell@users.noreply.github.com>
2023-08-29 15:30:50 -0400

    feat: export CodePathFunction

--- packages/ast-spec/src/base/CodePath.ts
+++ packages/ast-spec/src/base/CodePath.ts
@@ -1,9 +1,11 @@
+import type { TSESTree } from '@typescript-eslint/types';
+
 /**
  * Part of the code path analysis feature of ESLint:
  * https://eslint.org/docs/latest/extend/code-path-analysis
  *
  * These are used in the `onCodePath*` methods. (Note that the `node` parameter
- * of these methods is intentionally typed as `unknown`.)
+ * of these methods is intentionally omitted.)
  *
  * @see https://github.com/typescript-eslint/typescript-eslint/issues/6993
  */
@@ -23,7 +25,7 @@ export interface CodePath {
  * https://eslint.org/docs/latest/extend/code-path-analysis
  *
  * These are used in the `onCodePath*` methods. (Note that the `node` parameter
- * of these methods is intentionally typed as `unknown`.)
+ * of these methods is intentionally omitted.)
  *
  * @see https://github.com/typescript-eslint/typescript-eslint/issues/6993
  */
@@ -33,3 +35,22 @@ export interface CodePathSegment {
   prevSegments: CodePathSegment[];
   reachable: boolean;
 }
+
+/**
+ * Part of the code path analysis feature of ESLint:
+ * https://eslint.org/docs/latest/extend/code-path-analysis
+ *
+ * This type is unused in the `typescript-eslint` codebase since putting it on
+ * the `nodeSelector` for `RuleListener` would break the existing definition.
+ * However, it is exported here for the purposes of manual type-assertion.
+ *
+ * @see https://github.com/typescript-eslint/typescript-eslint/issues/6993
+ */
+export type CodePathFunction =
+  | ((
+      fromSegment: CodePathSegment,
+      toSegment: CodePathSegment,
+      node: TSESTree.Node,
+    ) => void)
+  | ((codePath: CodePath, node: TSESTree.Node) => void)
+  | ((segment: CodePathSegment, node: TSESTree.Node) => void);
