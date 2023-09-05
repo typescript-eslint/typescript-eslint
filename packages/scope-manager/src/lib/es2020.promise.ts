58f5905171bcc11470b4ca4e57f4143004c51053
Brad Zacher <brad.zacher@gmail.com>
2023-09-05 15:16:14 +0930

    chore: fix cyclic dependency in repo caused by generate-lib script changes

--- packages/scope-manager/src/lib/es2020.promise.ts
+++ packages/scope-manager/src/lib/es2020.promise.ts
@@ -1,7 +1,7 @@
 // THIS CODE WAS AUTOMATICALLY GENERATED
 // DO NOT EDIT THIS CODE BY HAND
 // RUN THE FOLLOWING COMMAND FROM THE WORKSPACE ROOT TO REGENERATE:
-// npx nx generate-lib @typescript-eslint/scope-manager
+// npx nx generate-lib @typescript-eslint/repo-tools
 
 import type { ImplicitLibVariableOptions } from '../variable';
 import { TYPE } from './base-config';
