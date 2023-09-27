b14433c736e05b7fbf2c68b420d4961a3de23d79
liuxingbaoyu <30521560+liuxingbaoyu@users.noreply.github.com>
2023-09-27 13:53:52 +0800

    add windows ci

--- packages/rule-tester/tests/RuleTester.test.ts
+++ packages/rule-tester/tests/RuleTester.test.ts
@@ -134,7 +134,9 @@ const NOOP_RULE: RuleModule<'error', []> = {
 };
 
 function getTestConfigFromCall(): unknown[] {
-  return runRuleForItemSpy.mock.calls.map(c => c[2]);
+  return runRuleForItemSpy.mock.calls.map(c => {
+    return { ...c[2], filename: c[2].filename?.replaceAll('\\', '/') };
+  });
 }
 
 describe('RuleTester', () => {
