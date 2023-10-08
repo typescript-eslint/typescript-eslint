bba2bba5f7af444f487e47ceac5f14474f9a2939
Josh Goldberg <git@joshuakgoldberg.com>
2023-10-08 18:41:04 -0400

    chore(website): swizzle 404 page with editor-like squiggle

---
+++ packages/website/src/theme/NotFound/index.tsx
@@ -0,0 +1,42 @@
+import { PageMetadata } from '@docusaurus/theme-common';
+import Layout from '@theme/Layout';
+import React from 'react';
+
+import styles from './styles.module.css';
+
+export default function NotFound(): React.JSX.Element {
+  return (
+    <>
+      <PageMetadata title="Page Not Found" />
+      <Layout>
+        <main className="container margin-vert--xl">
+          <div className="row">
+            <div className="col col--8 col--offset-2">
+              <h1 className={styles.title}>
+                <strong>
+                  {`'${location.pathname}'`.split('').map((letter, i) => (
+                    <span className={styles.word} key={i}>
+                      {letter}
+                    </span>
+                  ))}
+                </strong>{' '}
+                is not defined.
+              </h1>
+              <p className="hero__subtitle">
+                Looks like the page you're looking for doesn't exist. 😥
+              </p>
+              <p>
+                If you were linked here within typescript-eslint.io, there's
+                probably a bug in the site. Please{' '}
+                <a href="https://github.com/typescript-eslint/typescript-eslint/issues/new/choose">
+                  file an issue on GitHub
+                </a>
+                .
+              </p>
+            </div>
+          </div>
+        </main>
+      </Layout>
+    </>
+  );
+}
