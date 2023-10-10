9aa162ec48863a6ff85e516485478a8ffcadf8a8
Edwin Kofler <edwin@kofler.dev>
2023-10-10 02:42:39 -0700

    fix: Skip `isTTY` version check if user passes `loggerFn`

--- packages/typescript-estree/src/parseSettings/warnAboutTSVersion.ts
+++ packages/typescript-estree/src/parseSettings/warnAboutTSVersion.ts
@@ -23,23 +23,32 @@ const isRunningSupportedTypeScriptVersion = semver.satisfies(
 
 let warnedAboutTSVersion = false;
 
-export function warnAboutTSVersion(parseSettings: ParseSettings): void {
+export function warnAboutTSVersion(
+  parseSettings: ParseSettings,
+  passedLoggerFn: boolean,
+): void {
   if (!isRunningSupportedTypeScriptVersion && !warnedAboutTSVersion) {
-    const isTTY =
-      typeof process === 'undefined' ? false : process.stdout?.isTTY;
-    if (isTTY) {
-      const border = '=============';
-      const versionWarning = [
-        border,
-        'WARNING: You are currently running a version of TypeScript which is not officially supported by @typescript-eslint/typescript-estree.',
-        'You may find that it works just fine, or you may not.',
-        `SUPPORTED TYPESCRIPT VERSIONS: ${SUPPORTED_TYPESCRIPT_VERSIONS}`,
-        `YOUR TYPESCRIPT VERSION: ${ACTIVE_TYPESCRIPT_VERSION}`,
-        'Please only submit bug reports when using the officially supported version.',
-        border,
-      ];
-      parseSettings.log(versionWarning.join('\n\n'));
+    const border = '=============';
+    const versionWarning = [
+      border,
+      'WARNING: You are currently running a version of TypeScript which is not officially supported by @typescript-eslint/typescript-estree.',
+      'You may find that it works just fine, or you may not.',
+      `SUPPORTED TYPESCRIPT VERSIONS: ${SUPPORTED_TYPESCRIPT_VERSIONS}`,
+      `YOUR TYPESCRIPT VERSION: ${ACTIVE_TYPESCRIPT_VERSION}`,
+      'Please only submit bug reports when using the officially supported version.',
+      border,
+    ].join('\n\n');
+
+    if (passedLoggerFn) {
+      parseSettings.log(versionWarning);
+    } else {
+      const isTTY =
+        typeof process === 'undefined' ? false : process.stdout?.isTTY;
+      if (isTTY) {
+        parseSettings.log(versionWarning);
+      }
     }
+
     warnedAboutTSVersion = true;
   }
 }
--- packages/typescript-estree/tests/lib/warn-on-unsupported-ts.test.ts
+++ packages/typescript-estree/tests/lib/warn-on-unsupported-ts.test.ts
@@ -1,12 +1,18 @@
 import semver from 'semver';
 
-import * as parser from '../../src/parser';
+import type * as Parser from '../../src/parser';
 
 jest.mock('semver');
 
 const resetIsTTY = process.stdout.isTTY;
 
 describe('Warn on unsupported TypeScript version', () => {
+  let parser: typeof Parser;
+
+  beforeEach(async () => {
+    // @ts-expect-error -- We don't support ESM imports of local code yet.
+    parser = await import('../../src/parser.ts');
+  });
   afterEach(() => {
     jest.resetModules();
     jest.resetAllMocks();
@@ -26,7 +32,18 @@ describe('Warn on unsupported TypeScript version', () => {
     );
   });
 
-  it('should not warn the user when the user is running on a non TTY process', () => {
+  it('should warn the user if they are running on a non TTY process and a custom loggerFn was passed', () => {
+    (semver.satisfies as jest.Mock).mockReturnValue(false);
+    const loggerFn = jest.fn();
+    process.stdout.isTTY = false;
+
+    parser.parse('', {
+      loggerFn,
+    });
+    expect(loggerFn).toHaveBeenCalled();
+  });
+
+  it('should not warn the user if they are running on a non TTY process and a custom loggerFn was not passed', () => {
     (semver.satisfies as jest.Mock).mockReturnValue(false);
     jest.spyOn(console, 'log').mockImplementation();
     process.stdout.isTTY = false;
