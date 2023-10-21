a19a9f144fc92e3c829edf0281bb5cda2df842cb
Gareth Jones <Jones258@Gmail.com>
2023-10-21 18:48:35 +0000

    chore: ignore usage of deprecated property for now

--- packages/utils/src/eslint-utils/getParserServices.ts
+++ packages/utils/src/eslint-utils/getParserServices.ts
@@ -67,7 +67,9 @@ function getParserServices(
   // This check allows us to handle bad user setups whilst providing a nice user-facing
   // error message explaining the problem.
   if (
+    // eslint-disable-next-line deprecation/deprecation
     context.parserServices?.esTreeNodeToTSNodeMap == null ||
+    // eslint-disable-next-line deprecation/deprecation
     context.parserServices.tsNodeToESTreeNodeMap == null
   ) {
     throw new Error(ERROR_MESSAGE);
@@ -76,12 +78,14 @@ function getParserServices(
   // if a rule requires full type information, then hard fail if it doesn't exist
   // this forces the user to supply parserOptions.project
   if (
+    // eslint-disable-next-line deprecation/deprecation
     context.parserServices.program == null &&
     !allowWithoutFullTypeInformation
   ) {
     throw new Error(ERROR_MESSAGE);
   }
 
+  // eslint-disable-next-line deprecation/deprecation
   return context.parserServices;
 }
 /* eslint-enable @typescript-eslint/unified-signatures */
