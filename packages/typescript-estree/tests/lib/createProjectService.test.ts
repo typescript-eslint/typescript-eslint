7130d6c3cf2c1b205c70956ddee6be380b0b9c75
Josh Goldberg <git@joshuakgoldberg.com>
2023-11-12 20:32:50 -0500

    Added a bit more testing

--- packages/typescript-estree/tests/lib/createProjectService.test.ts
+++ packages/typescript-estree/tests/lib/createProjectService.test.ts
@@ -1,7 +1,19 @@
 import { createProjectService } from '../../src/create-program/createProjectService';
 
 describe('createProjectService', () => {
-  it('does not crash', () => {
-    createProjectService();
+  it('sets allowDefaultProjectForFiles to a populated Set when options.allowDefaultProjectForFiles is defined', () => {
+    const allowDefaultProjectForFiles = ['./*.js'];
+    const settings = createProjectService(
+      { allowDefaultProjectForFiles },
+      __dirname,
+    );
+
+    expect(settings.allowDefaultProjectForFiles.size).toBe(1);
+  });
+
+  it('sets allowDefaultProjectForFiles to an empty Set when options.allowDefaultProjectForFiles is not defined', () => {
+    const settings = createProjectService(undefined, __dirname);
+
+    expect(settings.allowDefaultProjectForFiles.size).toBe(0);
   });
 });
