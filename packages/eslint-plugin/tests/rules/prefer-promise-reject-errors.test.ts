e1db9888cf3b8cd38eab2127e2a147ed1134a094
auvred <aauvred@gmail.com>
2023-12-06 16:36:29 +0000

    test: specify error ranges for invalid test cases

--- packages/eslint-plugin/tests/rules/prefer-promise-reject-errors.test.ts
+++ packages/eslint-plugin/tests/rules/prefer-promise-reject-errors.test.ts
@@ -137,97 +137,614 @@ ruleTester.run('prefer-promise-reject-errors', rule, {
     `,
   ],
   invalid: [
-    'Promise.reject(5)',
-    "Promise.reject('foo')",
-    'Promise.reject(`foo`)',
-    "Promise.reject('foo', somethingElse)",
-    'Promise.reject(false)',
-    'Promise.reject(void `foo`)',
-    'Promise.reject()',
-    'Promise.reject(undefined)',
+    {
+      code: 'Promise.reject(5)',
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 1,
+          endColumn: 18,
+        },
+      ],
+    },
+    {
+      code: "Promise.reject('foo')",
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 1,
+          endColumn: 22,
+        },
+      ],
+    },
+    {
+      code: 'Promise.reject(`foo`)',
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 1,
+          endColumn: 22,
+        },
+      ],
+    },
+    {
+      code: "Promise.reject('foo', somethingElse)",
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 1,
+          endColumn: 37,
+        },
+      ],
+    },
+    {
+      code: 'Promise.reject(false)',
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 1,
+          endColumn: 22,
+        },
+      ],
+    },
+    {
+      code: 'Promise.reject(void `foo`)',
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 1,
+          endColumn: 27,
+        },
+      ],
+    },
+    {
+      code: 'Promise.reject()',
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 1,
+          endColumn: 17,
+        },
+      ],
+    },
     {
       code: 'Promise.reject(undefined)',
-      options: [{ allowEmptyReject: true }] as const,
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 1,
+          endColumn: 26,
+        },
+      ],
     },
-    'Promise.reject(null)',
-    'Promise.reject({ foo: 1 })',
-    'Promise.reject([1, 2, 3])',
-    `
-      declare const foo: Error | undefined
-      Promise.reject(foo)
-    `,
-    `
-      declare const foo: () => Promise<string>
-      Promise.reject(await foo())
-    `,
-    `
-      declare const foo: boolean
-      Promise.reject(foo && new Error())
-    `,
-    `
-      const foo = Promise
-      foo.reject()
-    `,
+    {
+      code: 'Promise.reject(undefined)',
+      options: [{ allowEmptyReject: true }],
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 1,
+          endColumn: 26,
+        },
+      ],
+    },
+    {
+      code: 'Promise.reject(null)',
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 1,
+          endColumn: 21,
+        },
+      ],
+    },
+    {
+      code: 'Promise.reject({ foo: 1 })',
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 1,
+          endColumn: 27,
+        },
+      ],
+    },
+    {
+      code: 'Promise.reject([1, 2, 3])',
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 1,
+          endColumn: 26,
+        },
+      ],
+    },
+    {
+      code: `
+declare const foo: Error | undefined
+Promise.reject(foo)
+      `,
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 3,
+          endLine: 3,
+          column: 1,
+          endColumn: 20,
+        },
+      ],
+    },
+    {
+      code: `
+declare const foo: () => Promise<string>
+Promise.reject(await foo())
+      `,
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 3,
+          endLine: 3,
+          column: 1,
+          endColumn: 28,
+        },
+      ],
+    },
+    {
+      code: `
+declare const foo: boolean
+Promise.reject(foo && new Error())
+      `,
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 3,
+          endLine: 3,
+          column: 1,
+          endColumn: 35,
+        },
+      ],
+    },
+    {
+      code: `
+const foo = Promise
+foo.reject()
+      `,
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 3,
+          endLine: 3,
+          column: 1,
+          endColumn: 13,
+        },
+      ],
+    },
 
-    'Promise.reject?.(5)',
-    'Promise?.reject(5)',
-    'Promise?.reject?.(5)',
-    '(Promise?.reject)(5)',
-    '(Promise?.reject)?.(5)',
-    `Promise['reject'](5)`,
+    {
+      code: 'Promise.reject?.(5)',
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 1,
+          endColumn: 20,
+        },
+      ],
+    },
+    {
+      code: 'Promise?.reject(5)',
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 1,
+          endColumn: 19,
+        },
+      ],
+    },
+    {
+      code: 'Promise?.reject?.(5)',
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 1,
+          endColumn: 21,
+        },
+      ],
+    },
+    {
+      code: '(Promise?.reject)(5)',
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 1,
+          endColumn: 21,
+        },
+      ],
+    },
+    {
+      code: '(Promise?.reject)?.(5)',
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 1,
+          endColumn: 23,
+        },
+      ],
+    },
+    {
+      code: `Promise['reject'](5)`,
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 1,
+          endColumn: 21,
+        },
+      ],
+    },
 
     // Assignments with mathematical operators will either evaluate to a primitive value or throw a TypeError
-    'Promise.reject(foo += new Error())',
-    'Promise.reject(foo -= new Error())',
-    'Promise.reject(foo **= new Error())',
-    'Promise.reject(foo <<= new Error())',
-    'Promise.reject(foo |= new Error())',
-    'Promise.reject(foo &= new Error())',
-
-    'new Promise(function(resolve, reject) { reject() })',
-    'new Promise(function(resolve, reject) { reject(5) })',
-    'new Promise((resolve, reject) => { reject() })',
-    'new Promise((resolve, reject) => reject(5))',
-    `
-      new Promise((resolve, reject) => {
-        fs.readFile('foo.txt', (err, file) => {
-          if (err) reject('File not found')
-          else resolve(file)
-        })
-      })
-    `,
-    'new Promise((yes, no) => no(5))',
-    'new Promise(({foo, bar, baz}, reject) => reject(5))',
-    'new Promise(function(reject, reject) { reject(5) })',
-    'new Promise(function(foo, arguments) { arguments(5) })',
-    'new Promise((foo, arguments) => arguments(5))',
-    'new Promise(function({}, reject) { reject(5) })',
-    'new Promise(({}, reject) => reject(5))',
-    'new Promise((resolve, reject, somethingElse = reject(5)) => {})',
-    `
-      declare const foo: {
-        bar: PromiseConstructor
-      }
-      new foo.bar((resolve, reject) => reject(5))
-    `,
-    `
-      declare const foo: {
-        bar: PromiseConstructor
-      }
-      new (foo?.bar)((resolve, reject) => reject(5))
-    `,
-    `
-      const foo = Promise
-      new foo((resolve, reject) => reject(5))
-    `,
-  ].map<TSESLint.InvalidTestCase<MessageIds, Options>>(invalidCase => {
-    return {
+    {
+      code: 'Promise.reject(foo += new Error())',
       errors: [
-        { messageId: 'rejectAnError', type: AST_NODE_TYPES.CallExpression },
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 1,
+          endColumn: 35,
+        },
       ],
-      ...(typeof invalidCase === 'string'
-        ? { code: invalidCase }
-        : invalidCase),
-    };
-  }),
+    },
+    {
+      code: 'Promise.reject(foo -= new Error())',
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 1,
+          endColumn: 35,
+        },
+      ],
+    },
+    {
+      code: 'Promise.reject(foo **= new Error())',
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 1,
+          endColumn: 36,
+        },
+      ],
+    },
+    {
+      code: 'Promise.reject(foo <<= new Error())',
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 1,
+          endColumn: 36,
+        },
+      ],
+    },
+    {
+      code: 'Promise.reject(foo |= new Error())',
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 1,
+          endColumn: 35,
+        },
+      ],
+    },
+    {
+      code: 'Promise.reject(foo &= new Error())',
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 1,
+          endColumn: 35,
+        },
+      ],
+    },
+
+    {
+      code: 'new Promise(function(resolve, reject) { reject() })',
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 41,
+          endColumn: 49,
+        },
+      ],
+    },
+    {
+      code: 'new Promise(function(resolve, reject) { reject(5) })',
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 41,
+          endColumn: 50,
+        },
+      ],
+    },
+    {
+      code: 'new Promise((resolve, reject) => { reject() })',
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 36,
+          endColumn: 44,
+        },
+      ],
+    },
+    {
+      code: 'new Promise((resolve, reject) => reject(5))',
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 34,
+          endColumn: 43,
+        },
+      ],
+    },
+    {
+      code: `
+new Promise((resolve, reject) => {
+  fs.readFile('foo.txt', (err, file) => {
+    if (err) reject('File not found')
+    else resolve(file)
+  })
+})
+      `,
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 4,
+          endLine: 4,
+          column: 14,
+          endColumn: 38,
+        },
+      ],
+    },
+    {
+      code: 'new Promise((yes, no) => no(5))',
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 26,
+          endColumn: 31,
+        },
+      ],
+    },
+    {
+      code: 'new Promise(({foo, bar, baz}, reject) => reject(5))',
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 42,
+          endColumn: 51,
+        },
+      ],
+    },
+    {
+      code: 'new Promise(function(reject, reject) { reject(5) })',
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 40,
+          endColumn: 49,
+        },
+      ],
+    },
+    {
+      code: 'new Promise(function(foo, arguments) { arguments(5) })',
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 40,
+          endColumn: 52,
+        },
+      ],
+    },
+    {
+      code: 'new Promise((foo, arguments) => arguments(5))',
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 33,
+          endColumn: 45,
+        },
+      ],
+    },
+    {
+      code: 'new Promise(function({}, reject) { reject(5) })',
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 36,
+          endColumn: 45,
+        },
+      ],
+    },
+    {
+      code: 'new Promise(({}, reject) => reject(5))',
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 29,
+          endColumn: 38,
+        },
+      ],
+    },
+    {
+      code: 'new Promise((resolve, reject, somethingElse = reject(5)) => {})',
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 1,
+          endLine: 1,
+          column: 47,
+          endColumn: 56,
+        },
+      ],
+    },
+    {
+      code: `
+declare const foo: {
+  bar: PromiseConstructor
+}
+new foo.bar((resolve, reject) => reject(5))
+      `,
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 5,
+          endLine: 5,
+          column: 34,
+          endColumn: 43,
+        },
+      ],
+    },
+    {
+      code: `
+declare const foo: {
+  bar: PromiseConstructor
+}
+new (foo?.bar)((resolve, reject) => reject(5))
+      `,
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 5,
+          endLine: 5,
+          column: 37,
+          endColumn: 46,
+        },
+      ],
+    },
+    {
+      code: `
+const foo = Promise
+new foo((resolve, reject) => reject(5))
+      `,
+      errors: [
+        {
+          messageId: 'rejectAnError',
+          type: AST_NODE_TYPES.CallExpression,
+          line: 3,
+          endLine: 3,
+          column: 30,
+          endColumn: 39,
+        },
+      ],
+    },
+  ],
 });
