67bbb5d27db09e420e5626303f220df9f94a3ccd
Josh Goldberg <git@joshuakgoldberg.com>
2023-12-23 20:53:21 -0500

    Update formatting admonition

--- packages/website/plugins/generated-rule-docs/insertions/insertFormattingNotice.ts
+++ packages/website/plugins/generated-rule-docs/insertions/insertFormattingNotice.ts
@@ -6,8 +6,8 @@ export function insertFormattingNotice(page: RuleDocsPage): void {
   if (page.rule.meta.type === 'layout') {
     page.spliceChildren(0, 0, {
       value: `
-<admonition type="warning">
-This rule will soon be moved to <a href="https://eslint.style">eslint-stylistic</a>.
+<admonition title="Deprecated" type="warning">
+This rule is deprecated. Formatting rules now live in <a href="https://eslint.style">eslint-stylistic</a>.
 See <a href="/linting/troubleshooting/formatting">What About Formatting?</a> for more information.
 </admonition>
 `,
