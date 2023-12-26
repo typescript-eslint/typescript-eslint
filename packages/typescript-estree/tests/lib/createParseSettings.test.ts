4db87f8448700d6745b9bc6fd7ee70806991a1a8
Josh Goldberg <git@joshuakgoldberg.com>
2023-12-26 13:05:47 -0500

    fix(typescript-estree): only create project serve from env setting if project is enabled

--- packages/typescript-estree/tests/lib/createParseSettings.test.ts
+++ packages/typescript-estree/tests/lib/createParseSettings.test.ts
@@ -1,6 +1,56 @@
 import { createParseSettings } from '../../src/parseSettings/createParseSettings';
 
+const projectService = { service: true };
+
+jest.mock('../../src/create-program/createProjectService', () => ({
+  createProjectService: () => projectService,
+}));
+
 describe('createParseSettings', () => {
+  describe('EXPERIMENTAL_projectService', () => {
+    it('is created when options.EXPERIMENTAL_useProjectService is enabled', () => {
+      process.env.TYPESCRIPT_ESLINT_EXPERIMENTAL_TSSERVER = 'false';
+
+      const parseSettings = createParseSettings('', {
+        EXPERIMENTAL_useProjectService: true,
+      });
+
+      expect(parseSettings.EXPERIMENTAL_projectService).toBe(projectService);
+    });
+
+    it('is created when options.EXPERIMENTAL_useProjectService is undefined, options.project is true, and process.env.TYPESCRIPT_ESLINT_EXPERIMENTAL_TSSERVER is true', () => {
+      process.env.TYPESCRIPT_ESLINT_EXPERIMENTAL_TSSERVER = 'true';
+
+      const parseSettings = createParseSettings('', {
+        EXPERIMENTAL_useProjectService: undefined,
+        project: true,
+      });
+
+      expect(parseSettings.EXPERIMENTAL_projectService).toBe(projectService);
+    });
+
+    it('is not created when options.EXPERIMENTAL_useProjectService is undefined, options.project is falsy, and process.env.TYPESCRIPT_ESLINT_EXPERIMENTAL_TSSERVER is true', () => {
+      process.env.TYPESCRIPT_ESLINT_EXPERIMENTAL_TSSERVER = 'true';
+
+      const parseSettings = createParseSettings('', {
+        EXPERIMENTAL_useProjectService: undefined,
+      });
+
+      expect(parseSettings.EXPERIMENTAL_projectService).toBeUndefined();
+    });
+
+    it('is not created when options.EXPERIMENTAL_useProjectService is false, options.project is true, and process.env.TYPESCRIPT_ESLINT_EXPERIMENTAL_TSSERVER is true', () => {
+      process.env.TYPESCRIPT_ESLINT_EXPERIMENTAL_TSSERVER = 'true';
+
+      const parseSettings = createParseSettings('', {
+        EXPERIMENTAL_useProjectService: false,
+        project: true,
+      });
+
+      expect(parseSettings.EXPERIMENTAL_projectService).toBeUndefined();
+    });
+  });
+
   describe('tsconfigMatchCache', () => {
     it('reuses the TSConfig match cache when called a subsequent time', () => {
       const parseSettings1 = createParseSettings('input.ts');
