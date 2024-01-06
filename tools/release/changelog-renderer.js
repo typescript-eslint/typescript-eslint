d0977abb4cf822f415e1f8a09c8e2b4c2b329555
James Henry <james@henry.sc>
2024-01-06 15:51:48 +0400

    chore: use nx release (#8194)

---
+++ tools/release/changelog-renderer.js
@@ -0,0 +1,26 @@
+const { default: defaultChangelogRenderer } = require('nx/changelog-renderer');
+
+const changelogRenderer = async ({
+  projectGraph,
+  commits,
+  releaseVersion,
+  project,
+  entryWhenNoChanges,
+  changelogRenderOptions,
+  repoSlug,
+}) => {
+  const defaultChangelog = await defaultChangelogRenderer({
+    projectGraph,
+    commits,
+    releaseVersion,
+    project,
+    entryWhenNoChanges,
+    changelogRenderOptions,
+    repoSlug,
+  });
+
+  // Append our custom messaging to the generated changelog entry
+  return `${defaultChangelog}\n\nYou can read about our [versioning strategy](https://main--typescript-eslint.netlify.app/users/versioning) and [releases](https://main--typescript-eslint.netlify.app/users/releases) on our website.`;
+};
+
+module.exports = changelogRenderer;
