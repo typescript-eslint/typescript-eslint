a1312504a2cfb819e54a5fb2b336236825e69db0
Josh Goldberg <git@joshuakgoldberg.com>
2024-03-05 08:42:15 -0500

    Fix: BaseRuleReference

--- packages/website/src/theme/MDXComponents/index.tsx
+++ packages/website/src/theme/MDXComponents/index.tsx
@@ -1,10 +1,12 @@
 import MDXComponents from '@theme-original/MDXComponents';
 
+import { BaseRuleReference } from './BaseRuleReference';
 import { RuleAttributes } from './RuleAttributes';
 import { TryInPlayground } from './TryInPlayground';
 
 export default {
   ...MDXComponents,
+  BaseRuleReference,
   RuleAttributes,
   TryInPlayground,
 };
