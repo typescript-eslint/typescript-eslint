afcca53996d95b538b98adef00f0044bc6dc60a4
Mark de Dios <mhdedios@gmail.com>
2024-03-27 10:40:47 -0700

    Move assignment of errors on ts syntactic and semantic issues to object assign

--- packages/parser/src/parser.ts
+++ packages/parser/src/parser.ts
@@ -103,15 +103,14 @@ function parseForESLint(
     options.ecmaFeatures = {};
   }
 
-  /**
-   * Override errorOnTypeScriptSyntacticAndSemanticIssues and set it to false to prevent use from user config
-   * https://github.com/typescript-eslint/typescript-eslint/issues/8681#issuecomment-2000411834
-   */
-  options.errorOnTypeScriptSyntacticAndSemanticIssues = false;
-
   const parserOptions: TSESTreeOptions = {};
   Object.assign(parserOptions, options, {
     jsx: validateBoolean(options.ecmaFeatures.jsx),
+    /**
+     * Override errorOnTypeScriptSyntacticAndSemanticIssues and set it to false to prevent use from user config
+     * https://github.com/typescript-eslint/typescript-eslint/issues/8681#issuecomment-2000411834
+     */
+    errorOnTypeScriptSyntacticAndSemanticIssues: false,
   });
   const analyzeOptions: AnalyzeOptions = {
     globalReturn: options.ecmaFeatures.globalReturn,
