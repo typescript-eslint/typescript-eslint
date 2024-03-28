75f552edadcc6489d62e45d4a678f7fd348ebc14
Abraham Guo <aguo@precisionplanting.com>
2024-03-28 19:09:13 -0400

    begin new integration tests

--- packages/integration-tests/tests/flat-config-types.test.ts
+++ packages/integration-tests/tests/flat-config-types.test.ts
@@ -3,19 +3,25 @@ import {
   typescriptIntegrationTest,
 } from '../tools/integration-test-base';
 
-typescriptIntegrationTest(
-  __filename,
-  ['--allowJs', '--esModuleInterop', 'eslint.config.js'],
-  out => {
-    const lines = out.split('\n').filter(
-      line =>
-        // error TS18028: Private identifiers are only available when targeting ECMAScript 2015 and higher.
-        // this is fine for us to ignore in this context
-        line && !line.includes('error TS18028'),
-    );
+for (const additionalFlags of [
+  [],
+  ['--strictNullChecks'],
+  ['--strictNullChecks', '--exactOptionalPropertyTypes'],
+]) {
+  typescriptIntegrationTest(
+    __filename,
+    ['--allowJs', '--esModuleInterop', ...additionalFlags, 'eslint.config.js'],
+    out => {
+      const lines = out.split('\n').filter(
+        line =>
+          // error TS18028: Private identifiers are only available when targeting ECMAScript 2015 and higher.
+          // this is fine for us to ignore in this context
+          line && !line.includes('error TS18028'),
+      );
 
-    // The types should not error (e.g. https://github.com/eslint-stylistic/eslint-stylistic/issues/276)
-    expect(lines).toHaveLength(0);
-  },
-);
+      // The types should not error (e.g. https://github.com/eslint-stylistic/eslint-stylistic/issues/276)
+      expect(lines).toHaveLength(0);
+    },
+  );
+}
 eslintIntegrationTest(__filename, 'eslint.config.js', true);
