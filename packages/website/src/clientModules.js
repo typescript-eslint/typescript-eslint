cf12e820fa89c258d1e6163206bd9d727418816b
arka1002 <arka.mcu700@gmail.com>
2024-03-31 11:18:53 +0530

    chore: eslint comments

--- packages/website/src/clientModules.js
+++ packages/website/src/clientModules.js
@@ -1,5 +1,6 @@
 import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 
 if (ExecutionEnvironment.canUseDOM) {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
   require('konamimojisplosion').initializeKonamimojisplosion();
 }
