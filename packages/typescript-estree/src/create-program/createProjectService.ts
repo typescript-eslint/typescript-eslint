5542aeb9440280dd4c0be529e9842f12221737ff
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2024-04-15 09:39:50 -0400

    feat(typescript-estree): add defaultProject for project service (#8815)
    * feat(typescript-estree): add defaultProject for project service
    
    * Thanks Jake, service.setCompilerOptionsForInferredProjects
    
    * Cleaned up a bit and added happy path testing
    
    * Update packages/typescript-estree/src/parser-options.ts
    
    Co-authored-by: Brad Zacher <brad.zacher@gmail.com>
    
    ---------
    
    Co-authored-by: Brad Zacher <brad.zacher@gmail.com>

--- packages/typescript-estree/src/create-program/createProjectService.ts
+++ packages/typescript-estree/src/create-program/createProjectService.ts
@@ -1,4 +1,6 @@
 /* eslint-disable @typescript-eslint/no-empty-function -- for TypeScript APIs*/
+import os from 'node:os';
+
 import type * as ts from 'typescript/lib/tsserverlibrary';
 
 import type { ProjectServiceOptions } from '../parser-options';
@@ -57,6 +59,42 @@ export function createProjectService(
     session: undefined,
     jsDocParsingMode,
   });
+
+  if (typeof options === 'object' && options.defaultProject) {
+    let configRead;
+
+    try {
+      configRead = tsserver.readConfigFile(
+        options.defaultProject,
+        system.readFile,
+      );
+    } catch (error) {
+      throw new Error(
+        `Could not parse default project '${options.defaultProject}': ${(error as Error).message}`,
+      );
+    }
+
+    if (configRead.error) {
+      throw new Error(
+        `Could not read default project '${options.defaultProject}': ${tsserver.formatDiagnostic(
+          configRead.error,
+          {
+            getCurrentDirectory: system.getCurrentDirectory,
+            getCanonicalFileName: fileName => fileName,
+            getNewLine: () => os.EOL,
+          },
+        )}`,
+      );
+    }
+
+    service.setCompilerOptionsForInferredProjects(
+      (
+        configRead.config as {
+          compilerOptions: ts.server.protocol.InferredProjectCompilerOptions;
+        }
+      ).compilerOptions,
+    );
+  }
 
   return {
     allowDefaultProjectForFiles:
