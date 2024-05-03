6693ea30f71963cabd8ff6175cdb898b6f89634a
Abraham Guo <aguo@precisionplanting.com>
2024-05-03 06:22:39 -0500

    remove debugging

--- packages/eslint-plugin/tests/dedupeTestCases.ts
+++ packages/eslint-plugin/tests/dedupeTestCases.ts
@@ -1,4 +1,3 @@
-let numCalls = 0;
 export const dedupeTestCases = <T>(...caseArrays: (readonly T[])[]): T[] => {
   const cases = caseArrays.flat();
   const dedupedCases = Object.values(
@@ -6,10 +5,9 @@ export const dedupeTestCases = <T>(...caseArrays: (readonly T[])[]): T[] => {
       cases.map(testCase => [JSON.stringify(testCase), testCase]),
     ),
   );
-  numCalls++;
   if (cases.length === dedupedCases.length) {
     throw new Error(
-      `Call #${numCalls} of \`dedupeTestCases\` is not necessary — no duplicate test cases detected!`,
+      `\`dedupeTestCases\` is not necessary — no duplicate test cases detected!`,
     );
   }
   return dedupedCases;
