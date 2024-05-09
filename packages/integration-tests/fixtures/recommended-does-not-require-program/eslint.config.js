88024e2ecd1c51c1dbd6382c5c0c8eb1587248f7
Josh Goldberg <git@joshuakgoldberg.com>
2024-05-08 17:45:02 -0700

    Some more reverts

--- packages/integration-tests/fixtures/recommended-does-not-require-program/eslint.config.js
+++
@@ -1,6 +0,0 @@
-// This integration test exists to make sure that the recommended config does
-// not require a program to be specified to ensure a fast and simple initial
-// setup. Users can add on one of our other configs if they want to opt in to
-// more expensive checks.
-const tseslint = require('typescript-eslint');
-module.exports = tseslint.config(...tseslint.configs.recommended);
