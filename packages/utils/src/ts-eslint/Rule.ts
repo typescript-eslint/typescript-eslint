3858d6531d45b81627e9cd185e5ce6cd14719219
Josh Goldberg <git@joshuakgoldberg.com>
2024-06-02 22:08:28 -0400

    fix lint complaint

--- packages/utils/src/ts-eslint/Rule.ts
+++ packages/utils/src/ts-eslint/Rule.ts
@@ -690,7 +690,7 @@ never only allow unidirectional)
 export type LooseRuleCreateFunction = (context: any) => Record<
   string,
   /*
-  eslint-disable-next-line @typescript-eslint/no-wrapper-object-types --
+  eslint-disable-next-line @typescript-eslint/no-unsafe-function-type --
   intentionally use Function here to give us the basic "is a function" validation
   without enforcing specific argument types so that different AST types can still
   be passed to configs
