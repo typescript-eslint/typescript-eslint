beab0dd9ce07b15593a443f1d52e1d1c0bb42a19
Joshua Chen <sidachen2003@gmail.com>
2024-06-03 23:15:17 +0800

    fix(ast-spec): function-call-like callee should be Expression not LeftHandSideExpression

--- packages/ast-spec/src/base/UnaryExpressionBase.ts
+++ packages/ast-spec/src/base/UnaryExpressionBase.ts
@@ -1,10 +1,8 @@
-import type { UnaryExpression } from '../expression/UnaryExpression/spec';
-import type { LeftHandSideExpression } from '../unions/LeftHandSideExpression';
-import type { Literal } from '../unions/Literal';
+import type { Expression } from '../unions/Expression';
 import type { BaseNode } from './BaseNode';
 
 export interface UnaryExpressionBase extends BaseNode {
   operator: string;
   prefix: boolean;
-  argument: LeftHandSideExpression | Literal | UnaryExpression;
+  argument: Expression;
 }
