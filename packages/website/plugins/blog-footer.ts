47d5a5fdbfda6910f6dbb7ca6ce3839c8f175025
Josh Goldberg <git@joshuakgoldberg.com>
2024-06-04 07:13:41 -0400

    Move heading out

--- packages/website/plugins/blog-footer.ts
+++ packages/website/plugins/blog-footer.ts
@@ -4,44 +4,46 @@ import type { Plugin } from 'unified';
 import { nodeIsParent } from './utils/nodes';
 
 export const blogFooter: Plugin = () => {
-  return root => {
+  return (root, file) => {
     if (!nodeIsParent(root)) {
       return;
     }
 
-    root.children.push({
-      children: [
-        {
-          children: [
-            {
-              type: 'text',
-              value: 'Supporting typescript-eslint',
-            },
-          ],
-          depth: 2,
-          type: 'heading',
-        },
-        {
-          type: 'text',
-          value:
-            'If you enjoyed this blog post and/or use typescript-eslint, please consider ',
-        },
-        {
-          children: [
-            {
-              type: 'text',
-              value: 'supporting us on Open Collective',
-            },
-          ],
-          url: 'https://opencollective.com/typescript-eslint',
-          type: 'link',
-        },
-        {
-          type: 'text',
-          value: `. We're a small volunteer team and could use your support to make the ESLint experience on TypeScript great. Thanks! 💖`,
-        },
-      ],
-      type: 'paragraph',
-    } as mdast.Paragraph);
+    root.children.push(
+      {
+        children: [
+          {
+            type: 'text',
+            value: 'Supporting typescript-eslint',
+          },
+        ],
+        depth: 2,
+        type: 'heading',
+      } as mdast.Heading,
+      {
+        children: [
+          {
+            type: 'text',
+            value:
+              'If you enjoyed this blog post and/or use typescript-eslint, please consider ',
+          },
+          {
+            children: [
+              {
+                type: 'text',
+                value: 'supporting us on Open Collective',
+              },
+            ],
+            url: 'https://opencollective.com/typescript-eslint',
+            type: 'link',
+          },
+          {
+            type: 'text',
+            value: `. We're a small volunteer team and could use your support to make the ESLint experience on TypeScript great. Thanks! 💖`,
+          },
+        ],
+        type: 'paragraph',
+      } as mdast.Paragraph,
+    );
   };
 };
