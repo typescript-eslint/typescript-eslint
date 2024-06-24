635133a293cbea63447f7b579e8c51306e01cd7f
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2024-06-24 13:50:31 -0400

    docs: split troubleshooting into granular sections (#9024)
    * docs: split troubleshooting into granular sections
    
    * Merged frameworks back into FAQs, and moved Formatting and TSLint inside it
    
    * Set up redirects and corrected some slugs
    
    * A couple more redirects
    
    * A redirect for monorepos
    
    * Sidebar naming: Troubleshooting > Type Information
    
    * Clarified Monorepos troubleshooting re projectService
    
    * Pagination and sidebar fixing
    
    * Reorganize the what-abouts to Users
    
    * Fixed failing links blocking build
    
    * fix lint complaints
    
    * fix lint complaints (again)
    
    * Re-apply changes: Troubleshooting & FAQs starting on General
    
    * Fix build links
    
    * Fix links post-merge
    
    * Move a general FAQ to General
    
    * Save a file
    
    * Re-apply: Netlify
    
    * A few reverts
    
    * Slightly more reverting
    
    * Slightly more reverting
    
    * fix: unit tests
    
    * More test fixes

--- packages/typescript-estree/src/convert.ts
+++ packages/typescript-estree/src/convert.ts
@@ -3529,7 +3529,7 @@ export class Converter {
         : (): Properties[typeof valueKey] => {
             if (!warned) {
               process.emitWarning(
-                `The '${aliasKey}' property is deprecated on ${node.type} nodes. Use '${valueKey}' instead. See https://typescript-eslint.io/linting/troubleshooting#the-key-property-is-deprecated-on-type-nodes-use-key-instead-warnings.`,
+                `The '${aliasKey}' property is deprecated on ${node.type} nodes. Use '${valueKey}' instead. See https://typescript-eslint.io/troubleshooting/faqs/general#the-key-property-is-deprecated-on-type-nodes-use-key-instead-warnings.`,
                 'DeprecationWarning',
               );
               warned = true;
