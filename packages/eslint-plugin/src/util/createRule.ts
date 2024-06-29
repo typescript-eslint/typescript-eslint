b3d2b25f74588d4b0259f0557b8de1e5194c88a2
Josh Goldberg <git@joshuakgoldberg.com>
2024-06-29 09:19:52 -0400

    import from rules, heck yes

--- packages/eslint-plugin/src/util/createRule.ts
+++ packages/eslint-plugin/src/util/createRule.ts
@@ -1,43 +1,7 @@
 import { ESLintUtils } from '@typescript-eslint/utils';
-import type {
-  RuleModuleWithMetaDocs,
-  RuleRecommendation,
-  RuleRecommendationAcrossConfigs,
-} from '@typescript-eslint/utils/ts-eslint';
 
-/**
- * This must be kept up-to-date with rules.d.ts.
- */
-export interface ESLintPluginDocs {
-  /**
-   * Does the rule extend (or is it based off of) an ESLint code rule?
-   * Alternately accepts the name of the base rule, in case the rule has been renamed.
-   * This is only used for documentation purposes.
-   */
-  extendsBaseRule?: boolean | string;
-
-  /**
-   * If a string config name, which starting config this rule is enabled in.
-   * If an object, which settings it has enabled in each of those configs.
-   */
-  recommended?: RuleRecommendation | RuleRecommendationAcrossConfigs<unknown[]>;
-
-  /**
-   * Does the rule require us to create a full TypeScript Program in order for it
-   * to type-check code. This is only used for documentation purposes.
-   */
-  requiresTypeChecking?: boolean;
-}
+import type { ESLintPluginDocs } from '../../rules';
 
 export const createRule = ESLintUtils.RuleCreator<ESLintPluginDocs>(
   name => `https://typescript-eslint.io/rules/${name}`,
 );
-
-/**
- * This must be kept up-to-date with rules.d.ts.
- */
-export type ESLintPluginRuleModule = RuleModuleWithMetaDocs<
-  string,
-  readonly unknown[],
-  ESLintPluginDocs
->;
