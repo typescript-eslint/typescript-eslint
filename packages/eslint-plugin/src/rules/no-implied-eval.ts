6d2d958543c348cd729238a14ef8b786b40f158d
Yutong Zhu <Yutong2022@gmail.com>
2024-06-30 23:08:02 -0400

    re-using logic from builtinSymbolLike in no-implied-eval

--- packages/eslint-plugin/src/rules/no-implied-eval.ts
+++ packages/eslint-plugin/src/rules/no-implied-eval.ts
@@ -4,6 +4,7 @@ import * as tsutils from 'ts-api-utils';
 import * as ts from 'typescript';
 
 import { createRule, getParserServices } from '../util';
+import { isBuiltinSymbolLike } from '../../../type-utils/src/builtinSymbolLikes';
 
 const FUNCTION_CONSTRUCTOR = 'Function';
 const GLOBAL_CANDIDATES = new Set(['global', 'window', 'globalThis']);
@@ -76,14 +77,8 @@ export default createRule({
       }
 
       // eslint-disable-next-line @typescript-eslint/no-unsafe-enum-comparison
-      if (symbol && symbol.escapedName === FUNCTION_CONSTRUCTOR) {
-        const declarations = symbol.getDeclarations() ?? [];
-        for (const declaration of declarations) {
-          const sourceFile = declaration.getSourceFile();
-          if (services.program.isSourceFileDefaultLibrary(sourceFile)) {
-            return true;
-          }
-        }
+      if (isBuiltinSymbolLike(services.program, type, FUNCTION_CONSTRUCTOR)) {
+        return true;
       }
 
       const signatures = checker.getSignaturesOfType(
@@ -143,13 +138,11 @@ export default createRule({
         const type = services.getTypeAtLocation(node.callee);
         const symbol = type.getSymbol();
         if (symbol) {
-          const declarations = symbol.getDeclarations() ?? [];
-          for (const declaration of declarations) {
-            const sourceFile = declaration.getSourceFile();
-            if (services.program.isSourceFileDefaultLibrary(sourceFile)) {
-              context.report({ node, messageId: 'noFunctionConstructor' });
-              return;
-            }
+          if (
+            isBuiltinSymbolLike(services.program, type, 'FunctionConstructor')
+          ) {
+            context.report({ node, messageId: 'noFunctionConstructor' });
+            return;
           }
         } else {
           context.report({ node, messageId: 'noFunctionConstructor' });
