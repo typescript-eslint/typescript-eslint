9afc572a30b0104d2a6739f761a8f161f131a173
Juan Sanchez <11725911+JSanchezIO@users.noreply.github.com>
2024-07-02 03:20:08 -0700

    feat(eslint-plugin): add support for nested namespaces to unsafe-member-access

--- packages/eslint-plugin/src/rules/no-unsafe-member-access.ts
+++ packages/eslint-plugin/src/rules/no-unsafe-member-access.ts
@@ -107,8 +107,8 @@ export default createRule({
     }
 
     return {
-      // ignore MemberExpression if it's parent is TSClassImplements or TSInterfaceHeritage
-      ':not(TSClassImplements, TSInterfaceHeritage) > MemberExpression':
+      // ignore MemberExpression's with ancestor's of type `TSClassImplements` or `TSInterfaceHeritage`
+      'MemberExpression:not(TSClassImplements MemberExpression, TSInterfaceHeritage MemberExpression)':
         checkMemberExpression,
       'MemberExpression[computed = true] > *.property'(
         node: TSESTree.Expression,
