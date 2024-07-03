785f6e987992d811cd96075a23cd48d8b0c47b77
Josh Goldberg <git@joshuakgoldberg.com>
2024-07-03 15:05:59 -0400

    feat(ast-spec): add parent property to AccessorProperty node types

--- packages/ast-spec/src/element/AccessorProperty/spec.ts
+++ packages/ast-spec/src/element/AccessorProperty/spec.ts
@@ -3,15 +3,18 @@ import type {
   PropertyDefinitionComputedNameBase,
   PropertyDefinitionNonComputedNameBase,
 } from '../../base/PropertyDefinitionBase';
+import type { ClassBody } from '../../special/ClassBody/spec';
 
 export interface AccessorPropertyComputedName
   extends PropertyDefinitionComputedNameBase {
   type: AST_NODE_TYPES.AccessorProperty;
+  parent: ClassBody;
 }
 
 export interface AccessorPropertyNonComputedName
   extends PropertyDefinitionNonComputedNameBase {
   type: AST_NODE_TYPES.AccessorProperty;
+  parent: ClassBody;
 }
 
 export type AccessorProperty =
