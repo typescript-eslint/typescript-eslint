4b7743dbce034ba2aaeca7bde53f3ecc984082af
Abraham Guo <aguo@precisionplanting.com>
2024-07-04 23:19:12 -0500

    lint

--- packages/eslint-plugin/src/rules/no-duplicate-type-constituents.ts
+++ packages/eslint-plugin/src/rules/no-duplicate-type-constituents.ts
@@ -1,6 +1,5 @@
 import type { TSESTree } from '@typescript-eslint/utils';
 import { AST_NODE_TYPES } from '@typescript-eslint/utils';
-import console from 'console';
 import type { Type } from 'typescript';
 
 import { createRule, getParserServices } from '../util';
