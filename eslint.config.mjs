2fc209a4cf5327686a40ebcbbcde8874ff4d266f
Abraham Guo <aguo@precisionplanting.com>
2024-07-10 17:57:10 -0500

    operator-assignment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -207,6 +207,7 @@ export default tseslint.config(
         { commentPattern: '.*intentional fallthrough.*' },
       ],
       'one-var': ['error', 'never'],
+      'operator-assignment': 'error',
 
       //
       // eslint-plugin-eslint-comment
