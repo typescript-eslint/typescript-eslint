8258daeee9fb081c6935a187784517e67efe4062
Abraham Guo <aguo@precisionplanting.com>
2024-07-18 11:31:44 -0500

    chore: enable no-useless-call (#9537)
    no-useless-call

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -206,6 +206,7 @@ export default tseslint.config(
         'error',
         { commentPattern: '.*intentional fallthrough.*' },
       ],
+      'no-useless-call': 'error',
       'no-useless-computed-key': 'error',
       'no-void': ['error', { allowAsStatement: true }],
       'one-var': ['error', 'never'],
