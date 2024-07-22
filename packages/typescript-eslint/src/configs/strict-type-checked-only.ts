785ea41e08807dcb531f9ada6e0dab5911e60d3b
Kirk Waiblinger <kirk.waiblinger@gmail.com>
2024-07-21 23:39:14 -0600

    Add return-await to strict preset for v8

--- packages/typescript-eslint/src/configs/strict-type-checked-only.ts
+++ packages/typescript-eslint/src/configs/strict-type-checked-only.ts
@@ -73,6 +73,11 @@ export default (
           allowNever: false,
         },
       ],
+      'no-return-await': 'off',
+      '@typescript-eslint/return-await': [
+        'error',
+        'error-handling-correctness-only',
+      ],
       '@typescript-eslint/unbound-method': 'error',
       '@typescript-eslint/use-unknown-in-catch-callback-variable': 'error',
     },
