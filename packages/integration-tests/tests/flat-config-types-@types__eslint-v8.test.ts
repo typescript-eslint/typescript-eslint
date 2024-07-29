05461938d8e5454d4a4a87de9bfe07bf10acea0d
Brad Zacher <brad.zacher@gmail.com>
2024-07-29 10:20:52 +0930

    fix(utils): correct types to align with @types/eslint v9

--- packages/integration-tests/tests/flat-config-types-@types__eslint-v8.test.ts
+++ packages/integration-tests/tests/flat-config-types-@types__eslint-v8.test.ts
@@ -15,9 +15,10 @@ for (const additionalFlags of [
     out => {
       const lines = out.split('\n').filter(
         line =>
+          line &&
           // error TS18028: Private identifiers are only available when targeting ECMAScript 2015 and higher.
           // this is fine for us to ignore in this context
-          line && !line.includes('error TS18028'),
+          !line.includes('error TS18028'),
       );
 
       // The types should not error (e.g. https://github.com/eslint-stylistic/eslint-stylistic/issues/276)
