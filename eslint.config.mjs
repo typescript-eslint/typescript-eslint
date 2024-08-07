a26e3c7056fd1f64ebce6eb9a9d9006e10e2bc8d
Abraham Guo <aguo@precisionplanting.com>
2024-08-07 17:14:01 -0500

    chore: enable prefer-regexp-test (#9658)
    prefer-regexp-test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -322,6 +322,7 @@ export default tseslint.config(
       'jsdoc/informative-docs': 'error',
       'unicorn/no-typeof-undefined': 'error',
       'unicorn/no-useless-spread': 'error',
+      'unicorn/prefer-regexp-test': 'error',
     },
   },
   {
