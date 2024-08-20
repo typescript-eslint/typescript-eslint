94f7c99550aa06df6d6a039d53494aeaf8356f7c
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2024-08-20 02:21:23 -0400

    fix(eslint-plugin): [no-unnecessary-type-parameters] check mapped alias type arguments (#9741)
    * fix(eslint-plugin): [no-unnecessary-type-parameters] check mapped alias type arguments
    
    * yarn format --write
    
    * Handle type aliases higher up in #9741 (#323)
    
    handle type aliases higher up
    
    ---------
    
    Co-authored-by: Dan Vanderkam <danvdk@gmail.com>

--- packages/eslint-plugin/src/rules/no-unnecessary-type-parameters.ts
+++ packages/eslint-plugin/src/rules/no-unnecessary-type-parameters.ts
@@ -258,6 +258,13 @@ function collectTypeParameterUsageCounts(
       }
     }
 
+    // Catch-all: generic type references like `Exclude<T, null>`
+    else if (type.aliasTypeArguments) {
+      // We don't descend into the definition of the type alias, so we don't
+      // know whether it's used multiple times. It's safest to assume it is.
+      visitTypesList(type.aliasTypeArguments, true);
+    }
+
     // Intersections and unions like `0 | 1`
     else if (tsutils.isUnionOrIntersectionType(type)) {
       visitTypesList(type.types, assumeMultipleUses);
@@ -307,10 +314,6 @@ function collectTypeParameterUsageCounts(
         }
       }
 
-      for (const typeArgument of type.aliasTypeArguments ?? []) {
-        visitType(typeArgument, true);
-      }
-
       visitType(type.getNumberIndexType(), true);
       visitType(type.getStringIndexType(), true);
 
@@ -328,11 +331,6 @@ function collectTypeParameterUsageCounts(
     // Catch-all: operator types like `keyof T`
     else if (isOperatorType(type)) {
       visitType(type.type, assumeMultipleUses);
-    }
-
-    // Catch-all: generic type references like `Exclude<T, null>`
-    else if (type.aliasTypeArguments) {
-      visitTypesList(type.aliasTypeArguments, true);
     }
   }
 
