d4f69435cc6be99f8656e4c340dcfdab1521795a
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2024-08-24 18:20:33 -0400

    chore: enable eslint-plugin-perfectionist on typescript-eslint package (#9851)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -570,11 +570,13 @@ export default tseslint.config(
   },
   {
     extends: [perfectionistPlugin.configs['recommended-alphabetical']],
+    ignores: ['packages/typescript-eslint/src/configs/*'],
     files: [
       'packages/ast-spec/{src,tests,typings}/**/*.ts',
       'packages/integration-tests/{tests,tools,typing}/**/*.ts',
       'packages/parser/{src,tests}/**/*.ts',
       'packages/rule-tester/{src,tests,typings}/**/*.ts',
+      'packages/typescript-eslint/{src,tests}/**/*.ts',
       'packages/utils/src/**/*.ts',
       'packages/visitor-keys/src/**/*.ts',
       'packages/website*/src/**/*.ts',
