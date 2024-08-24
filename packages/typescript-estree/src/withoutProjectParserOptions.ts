ba6ea415f5ddb0e668db49d960292414c92fb709
Josh Goldberg ✨ <me@joshuakgoldberg.com>
2024-08-24 12:56:40 -0400

    Update packages/typescript-estree/src/withoutProjectParserOptions.ts
    Co-authored-by: Brad Zacher <brad.zacher@gmail.com>

--- packages/typescript-estree/src/withoutProjectParserOptions.ts
+++ packages/typescript-estree/src/withoutProjectParserOptions.ts
@@ -8,7 +8,7 @@ import type { TSESTreeOptions } from './parser-options';
  *
  * @see https://github.com/typescript-eslint/typescript-eslint/issues/8428
  */
-export function withoutProjectParserOptions<Options extends TSESTreeOptions>(
+export function withoutProjectParserOptions<Options extends object>(
   opts: Options,
 ): Omit<
   Options,
