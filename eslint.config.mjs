2239927ec9d1c48d854e2423d6f622108a7e4db1
Josh Goldberg <git@joshuakgoldberg.com>
2024-08-25 12:26:37 -0400

    chore: disable eslint-plugin-perfectionist n enums

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -591,14 +591,7 @@ export default tseslint.config(
           type: 'natural',
         },
       ],
-      'perfectionist/sort-enums': [
-        'error',
-        {
-          order: 'asc',
-          partitionByComment: true,
-          type: 'natural',
-        },
-      ],
+      'perfectionist/sort-enums': 'off',
       'perfectionist/sort-objects': [
         'error',
         {
