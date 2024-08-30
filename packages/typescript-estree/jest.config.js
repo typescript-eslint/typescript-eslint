0f404a54c9fb63e3b0531599d37ca634b00a9296
Josh Goldberg <git@joshuakgoldberg.com>
2024-08-30 09:24:03 -0400

    Ignore project-true

--- packages/typescript-estree/jest.config.js
+++ packages/typescript-estree/jest.config.js
@@ -1,8 +1,14 @@
 'use strict';
 // @ts-check
 
+console.log('with', process.env.TYPESCRIPT_ESLINT_PROJECT_SERVICE);
+
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
   ...require('../../jest.config.base.js'),
   testRegex: ['./tests/lib/.*\\.test\\.ts$'],
+  testPathIgnorePatterns: process.env.TYPESCRIPT_ESLINT_PROJECT_SERVICE && [
+    '/node_modules/',
+    'project-true',
+  ],
 };
