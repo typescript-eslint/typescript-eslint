cafed6df463a92ac7bdd94cee9bb4220f79a19f3
Abraham Guo <aguo@precisionplanting.com>
2024-09-04 07:29:44 -0500

    chore: enable `unicorn/prefer-array-some` (#9932)
    prefer-array-some

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -329,6 +329,7 @@ export default tseslint.config(
       'unicorn/no-typeof-undefined': 'error',
       'unicorn/no-single-promise-in-promise-methods': 'error',
       'unicorn/no-useless-spread': 'error',
+      'unicorn/prefer-array-some': 'error',
       'unicorn/prefer-export-from': 'error',
       'unicorn/prefer-node-protocol': 'error',
       'unicorn/prefer-regexp-test': 'error',
