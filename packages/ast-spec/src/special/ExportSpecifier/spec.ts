ced3289ca1c6cfbe046c308f89ddfe3502413a6e
Josh Goldberg <git@joshuakgoldberg.com>
2024-09-12 08:11:55 -0400

    Start on tests

--- packages/ast-spec/src/special/ExportSpecifier/spec.ts
+++ packages/ast-spec/src/special/ExportSpecifier/spec.ts
@@ -4,9 +4,21 @@ import type { ExportKind } from '../../declaration/ExportAndImportKind';
 import type { Identifier } from '../../expression/Identifier/spec';
 import type { StringLiteral } from '../../expression/literal/StringLiteral/spec';
 
-export interface ExportSpecifier extends BaseNode {
-  exported: Identifier | StringLiteral;
+export interface ExportSpecifierBase extends BaseNode {
   exportKind: ExportKind;
-  local: Identifier;
   type: AST_NODE_TYPES.ExportSpecifier;
 }
+
+export interface ExportIdentifierSpecifier extends ExportSpecifierBase {
+  exported: Identifier;
+  local: Identifier;
+}
+
+export interface ExportStringLiteralSpecifier extends ExportSpecifierBase {
+  exported: StringLiteral;
+  local: StringLiteral;
+}
+
+export type ExportSpecifier =
+  | ExportIdentifierSpecifier
+  | ExportStringLiteralSpecifier;
