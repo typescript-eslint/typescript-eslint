5250bd8880fc08d5de3cc3c26cc313b443d5b8e0
auvred <aauvred@gmail.com>
2024-09-17 12:55:22 +0300

    lint --fix

--- packages/utils/src/eslint-utils/index.ts
+++ packages/utils/src/eslint-utils/index.ts
@@ -1,6 +1,6 @@
 export * from './applyDefault';
 export * from './deepMerge';
 export * from './getParserServices';
-export * from './InferTypesFromRule';
+export type * from './InferTypesFromRule';
 export * from './nullThrows';
 export * from './RuleCreator';
