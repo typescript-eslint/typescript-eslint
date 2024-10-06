030dd2f29fa3c7b7771037d55135b33514ce5c82
Anna Bocharova <robin_tail@me.com>
2024-10-06 04:35:29 +0000

    Applying lint fix.

--- packages/rule-tester/src/types/ValidTestCase.ts
+++ packages/rule-tester/src/types/ValidTestCase.ts
@@ -28,6 +28,14 @@ export interface TestLanguageOptions {
 
 export interface ValidTestCase<Options extends readonly unknown[]> {
   /**
+   * Function to execute after testing the case regardless of its result.
+   */
+  readonly after?: () => void;
+  /**
+   * Function to execute before testing the case.
+   */
+  readonly before?: () => void;
+  /**
    * Code for the test case.
    */
   readonly code: string;
@@ -63,12 +71,4 @@ export interface ValidTestCase<Options extends readonly unknown[]> {
    * Skip this case in supported test frameworks.
    */
   readonly skip?: boolean;
-  /**
-   * Function to execute before testing the case.
-   * */
-  readonly before?: () => void;
-  /**
-   * Function to execute after testing the case regardless of its result.
-   * */
-  readonly after?: () => void;
 }
