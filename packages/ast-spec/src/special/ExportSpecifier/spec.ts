5c87171d9c49c36d19bd5a75f0a679e62db389c3
Josh Goldberg <git@joshuakgoldberg.com>
2024-10-14 18:13:59 -0400

    Split ExportSpecifier back out

--- packages/ast-spec/src/special/ExportSpecifier/spec.ts
+++ packages/ast-spec/src/special/ExportSpecifier/spec.ts
@@ -4,9 +4,18 @@ import type { ExportKind } from '../../declaration/ExportAndImportKind';
 import type { Identifier } from '../../expression/Identifier/spec';
 import type { StringLiteral } from '../../expression/literal/StringLiteral/spec';
 
-export interface ExportSpecifier extends BaseNode {
+export interface ExportSpecifierBase extends BaseNode {
   type: AST_NODE_TYPES.ExportSpecifier;
-  exported: StringLiteral | Identifier;
   exportKind: ExportKind;
   local: Identifier;
 }
+
+export interface ExportIdentifierSpecifier extends ExportSpecifierBase {
+  exported: Identifier;
+}
+
+export interface ExportSourceSpecifier extends ExportSpecifierBase {
+  exported: Identifier | StringLiteral;
+}
+
+export type ExportSpecifier = ExportIdentifierSpecifier | ExportSourceSpecifier;
