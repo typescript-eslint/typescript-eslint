1c5b1d6531401728b47ee9516446545307aaf088
yuya.yoshioka <yuya3330930@yahoo.co.jp>
2024-10-16 00:37:24 +0900

    create mock function

--- packages/type-utils/tests/getConstrainedTypeAtLocation.test.ts
+++ packages/type-utils/tests/getConstrainedTypeAtLocation.test.ts
@@ -6,41 +6,50 @@ import { getConstrainedTypeAtLocation } from '../src';
 
 const node = {} as TSESTree.Node;
 
+const mockType = (): ts.Type => {
+  return {} as ts.Type;
+};
+
+const mockServices = ({
+  typeAtLocation,
+  baseConstraintOfType,
+}: {
+  typeAtLocation: ts.Type;
+  baseConstraintOfType?: ts.Type;
+}): ParserServicesWithTypeInformation => {
+  const typeChecker = {
+    getBaseConstraintOfType: (_: ts.Type) => baseConstraintOfType,
+  } as ts.TypeChecker;
+  const program = {
+    getTypeChecker: () => typeChecker,
+  } as ts.Program;
+
+  return {
+    program,
+    getTypeAtLocation: (_: TSESTree.Node) => typeAtLocation,
+  } as ParserServicesWithTypeInformation;
+};
+
 describe('getConstrainedTypeAtLocation', () => {
   describe('when the node has a generic constraint', () => {
     it('returns the generic constraint type', () => {
-      const typeAtLocation = {} as ts.Type;
-      const constraintOfType = {} as ts.Type;
-      const typeChecker = {
-        getBaseConstraintOfType: (_: ts.Type) => constraintOfType,
-      } as ts.TypeChecker;
-      const program = {
-        getTypeChecker: () => typeChecker,
-      } as ts.Program;
-      const services = {
-        program,
-        getTypeAtLocation: (_: TSESTree.Node) => typeAtLocation,
-      } as ParserServicesWithTypeInformation;
+      const typeAtLocation = mockType();
+      const baseConstraintOfType = mockType();
+      const services = mockServices({
+        typeAtLocation,
+        baseConstraintOfType,
+      });
 
       expect(getConstrainedTypeAtLocation(services, node)).toBe(
-        constraintOfType,
+        baseConstraintOfType,
       );
     });
   });
 
   describe('when the node does not have a generic constraint', () => {
     it('returns the node type', () => {
-      const typeAtLocation = {} as ts.Type;
-      const typeChecker = {
-        getBaseConstraintOfType: (_: ts.Type) => undefined,
-      } as ts.TypeChecker;
-      const program = {
-        getTypeChecker: () => typeChecker,
-      } as ts.Program;
-      const services = {
-        program,
-        getTypeAtLocation: (_: TSESTree.Node) => typeAtLocation,
-      } as ParserServicesWithTypeInformation;
+      const typeAtLocation = mockType();
+      const services = mockServices({ typeAtLocation });
 
       expect(getConstrainedTypeAtLocation(services, node)).toBe(typeAtLocation);
     });
