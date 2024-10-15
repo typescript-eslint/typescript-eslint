ff73c4d78f35e87f95c46264f9ea56d18469c6c2
Ronen Amiel <ronen.amiel@gmail.com>
2024-10-15 23:30:16 +0300

    match implementation to be inline with no-unsafe-* rules

--- packages/eslint-plugin/src/rules/no-unsafe-type-assertion.ts
+++ packages/eslint-plugin/src/rules/no-unsafe-type-assertion.ts
@@ -1,9 +1,16 @@
 import type { TSESTree } from '@typescript-eslint/utils';
 
+import * as tsutils from 'ts-api-utils';
+import * as ts from 'typescript';
+
 import {
   createRule,
   getConstrainedTypeAtLocation,
   getParserServices,
+  isTypeAnyType,
+  isTypeFlagSet,
+  isTypeUnknownType,
+  isUnsafeAssignment,
 } from '../util';
 
 export default createRule({
@@ -15,6 +22,10 @@ export default createRule({
       requiresTypeChecking: true,
     },
     messages: {
+      unsafeOfAnyTypeAssertion:
+        'Unsafe cast from {{type}} detected: consider using type guards or a safer cast.',
+      unsafeToAnyTypeAssertion:
+        'Unsafe cast to {{type}} detected: consider using a more specific type to ensure safety.',
       unsafeTypeAssertion:
         "Unsafe type assertion: type '{{type}}' is more narrow than the original type.",
     },
@@ -24,18 +35,125 @@ export default createRule({
   create(context) {
     const services = getParserServices(context);
     const checker = services.program.getTypeChecker();
+    const compilerOptions = services.program.getCompilerOptions();
+
+    function isTypeUnchanged(uncast: ts.Type, cast: ts.Type): boolean {
+      if (uncast === cast) {
+        return true;
+      }
+
+      if (
+        isTypeFlagSet(uncast, ts.TypeFlags.Undefined) &&
+        isTypeFlagSet(cast, ts.TypeFlags.Undefined) &&
+        tsutils.isCompilerOptionEnabled(
+          compilerOptions,
+          'exactOptionalPropertyTypes',
+        )
+      ) {
+        const uncastParts = tsutils
+          .unionTypeParts(uncast)
+          .filter(part => !isTypeFlagSet(part, ts.TypeFlags.Undefined));
+
+        const castParts = tsutils
+          .unionTypeParts(cast)
+          .filter(part => !isTypeFlagSet(part, ts.TypeFlags.Undefined));
+
+        if (uncastParts.length !== castParts.length) {
+          return false;
+        }
+
+        const uncastPartsSet = new Set(uncastParts);
+        return castParts.every(part => uncastPartsSet.has(part));
+      }
+
+      return false;
+    }
 
     function checkExpression(
       node: TSESTree.TSAsExpression | TSESTree.TSTypeAssertion,
     ): void {
-      const nodeType = getConstrainedTypeAtLocation(services, node.expression);
+      const expressionType = getConstrainedTypeAtLocation(
+        services,
+        node.expression,
+      );
       const assertedType = getConstrainedTypeAtLocation(
         services,
         node.typeAnnotation,
       );
 
+      // consider unchanged type as safe
+      const typeIsUnchanged = isTypeUnchanged(expressionType, assertedType);
+
+      if (typeIsUnchanged) {
+        return;
+      }
+
+      // handle cases when casting unknown ==> any.
+      if (isTypeAnyType(assertedType) && isTypeUnknownType(expressionType)) {
+        context.report({
+          node,
+          messageId: 'unsafeToAnyTypeAssertion',
+          data: {
+            type: '`any`',
+          },
+        });
+
+        return;
+      }
+
+      // handle cases when casting of an any expression.
+      const expressionAny = isUnsafeAssignment(
+        expressionType,
+        assertedType,
+        checker,
+        node.expression,
+      );
+
+      if (expressionAny) {
+        context.report({
+          node,
+          messageId: 'unsafeOfAnyTypeAssertion',
+          data: {
+            type: tsutils.isIntrinsicErrorType(expressionAny.sender)
+              ? 'error typed'
+              : '`any`',
+          },
+        });
+
+        return;
+      }
+
+      // handle cases when casting to an any type.
+      const assertedAny = isUnsafeAssignment(
+        assertedType,
+        expressionType,
+        checker,
+        node.typeAnnotation,
+      );
+
+      if (assertedAny) {
+        context.report({
+          node,
+          messageId: 'unsafeToAnyTypeAssertion',
+          data: {
+            type: tsutils.isIntrinsicErrorType(assertedAny.sender)
+              ? 'error typed'
+              : '`any`',
+          },
+        });
+
+        return;
+      }
+
+      // fallback to checking assignability
+      const nodeWidenedType =
+        tsutils.isObjectType(expressionType) &&
+        tsutils.isObjectFlagSet(expressionType, ts.ObjectFlags.ObjectLiteral)
+          ? checker.getWidenedType(expressionType)
+          : expressionType;
+
       const isAssertionSafe = checker.isTypeAssignableTo(
-        nodeType,
+        nodeWidenedType,
         assertedType,
       );
 
@@ -44,7 +162,7 @@ export default createRule({
           node,
           messageId: 'unsafeTypeAssertion',
           data: {
-            type: checker.typeToString(nodeType),
+            type: checker.typeToString(expressionType),
           },
         });
       }
