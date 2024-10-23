ef3176ac3bcb6d900dc5c7f6b85ee7e76c63ea5f
reduckted <reduckted@outlook.com>
2024-10-23 22:01:07 +1000

    Use cwd to set the base path for absolute and relative file names.

--- packages/rule-tester/src/RuleTester.ts
+++ packages/rule-tester/src/RuleTester.ts
@@ -166,7 +166,7 @@ function getUnsubstitutedMessagePlaceholders(
 }
 
 export class RuleTester extends TestFramework {
-  readonly #linter: Linter;
+  readonly #lintersByBasePath: Map<string | undefined, Linter>;
   readonly #rules: Record<string, AnyRuleCreateFunction | AnyRuleModule> = {};
   readonly #testerConfig: TesterConfigWithDefaults;
 
@@ -184,31 +184,7 @@ export class RuleTester extends TestFramework {
       rules: { [`${RULE_TESTER_PLUGIN_PREFIX}validate-ast`]: 'error' },
     });
 
-    this.#linter = (() => {
-      const linter = new Linter({
-        configType: 'flat',
-        cwd: this.#testerConfig.languageOptions.parserOptions?.tsconfigRootDir,
-      });
-
-      // This nonsense is a workaround for https://github.com/jestjs/jest/issues/14840
-      // see also https://github.com/typescript-eslint/typescript-eslint/issues/8942
-      //
-      // For some reason rethrowing exceptions skirts around the circular JSON error.
-      const oldVerify = linter.verify.bind(linter);
-      linter.verify = (
-        ...args: Parameters<Linter['verify']>
-      ): ReturnType<Linter['verify']> => {
-        try {
-          return oldVerify(...args);
-        } catch (error) {
-          throw new Error('Caught an error while linting', {
-            cause: error,
-          });
-        }
-      };
-
-      return linter;
-    })();
+    this.#lintersByBasePath = new Map();
 
     // make sure that the parser doesn't hold onto file handles between tests
     // on linux (i.e. our CI env), there can be very a limited number of watch handles available
@@ -220,6 +196,56 @@ export class RuleTester extends TestFramework {
         // ignored on purpose
       }
     });
+  }
+
+  #getLinterForFilename(filename: string | undefined): Linter {
+    let basePath: string | undefined =
+      this.#testerConfig.languageOptions.parserOptions?.tsconfigRootDir;
+    if (filename !== undefined) {
+      // For an absolute path (`/foo.ts`), or a path that steps
+      // up (`../foo.ts`), resolve the path relative to the base
+      // path (using the current working directory if the parser
+      // options did not specify a base path) and use the file's
+      // root as the base path so that the file is under the base
+      // path. For any other path, which would just be a plain
+      // file name (`foo.ts`), don't change the base path.
+      if (filename.startsWith('/') || filename.startsWith('..')) {
+        basePath = path.parse(
+          path.resolve(basePath ?? process.cwd(), filename),
+        ).root;
+      }
+    }
+
+    let linterForBasePath = this.#lintersByBasePath.get(basePath);
+    if (!linterForBasePath) {
+      linterForBasePath = (() => {
+        const linter = new Linter({
+          configType: 'flat',
+          cwd: basePath,
+        });
+
+        // This nonsense is a workaround for https://github.com/jestjs/jest/issues/14840
+        // see also https://github.com/typescript-eslint/typescript-eslint/issues/8942
+        //
+        // For some reason rethrowing exceptions skirts around the circular JSON error.
+        const oldVerify = linter.verify.bind(linter);
+        linter.verify = (
+          ...args: Parameters<Linter['verify']>
+        ): ReturnType<Linter['verify']> => {
+          try {
+            return oldVerify(...args);
+          } catch (error) {
+            throw new Error('Caught an error while linting', {
+              cause: error,
+            });
+          }
+        };
+
+        return linter;
+      })();
+      this.#lintersByBasePath.set(basePath, linterForBasePath);
+    }
+    return linterForBasePath;
   }
 
   /**
@@ -738,6 +764,7 @@ export class RuleTester extends TestFramework {
     let passNumber = 0;
     const outputs: string[] = [];
     const configWithoutCustomKeys = omitCustomConfigProperties(config);
+    const linter = this.#getLinterForFilename(filename);
 
     do {
       passNumber++;
@@ -767,15 +794,7 @@ export class RuleTester extends TestFramework {
             ...configWithoutCustomKeys.linterOptions,
           },
         });
-        messages = this.#linter.verify(
-          code,
-          // ESLint uses an internal FlatConfigArray that extends @humanwhocodes/config-array.
-          Object.assign([], {
-            basePath: filename ? path.parse(filename).root : '',
-            getConfig: () => actualConfig,
-          }),
-          filename,
-        );
+        messages = linter.verify(code, actualConfig, filename);
       } finally {
         SourceCode.prototype.applyInlineConfig = applyInlineConfig;
         SourceCode.prototype.applyLanguageOptions = applyLanguageOptions;
@@ -802,7 +821,7 @@ export class RuleTester extends TestFramework {
       outputs.push(code);
 
       // Verify if autofix makes a syntax error or not.
-      const errorMessageInFix = this.#linter
+      const errorMessageInFix = linter
         .verify(fixedResult.output, configWithoutCustomKeys, filename)
         .find(m => m.fatal);
 
@@ -1255,7 +1274,9 @@ export class RuleTester extends TestFramework {
                       ]).output;
 
                     // Verify if suggestion fix makes a syntax error or not.
-                    const errorMessageInSuggestion = this.#linter
+                    const errorMessageInSuggestion = this.#getLinterForFilename(
+                      undefined,
+                    )
                       .verify(
                         codeWithAppliedSuggestion,
                         omitCustomConfigProperties(result.config),
