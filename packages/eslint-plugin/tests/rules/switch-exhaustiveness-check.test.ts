47b95f62c45e83dadb012d4f63f71affa323e917
Kirk Waiblinger <kirk.waiblinger@gmail.com>
2024-10-28 18:00:44 -0600

    lint

--- packages/eslint-plugin/tests/rules/switch-exhaustiveness-check.test.ts
+++ packages/eslint-plugin/tests/rules/switch-exhaustiveness-check.test.ts
@@ -146,27 +146,27 @@ function test(value: Union): number {
     // Switch contains default clause.
     {
       code: `
-  type Day =
-    | 'Monday'
-    | 'Tuesday'
-    | 'Wednesday'
-    | 'Thursday'
-    | 'Friday'
-    | 'Saturday'
-    | 'Sunday';
+type Day =
+  | 'Monday'
+  | 'Tuesday'
+  | 'Wednesday'
+  | 'Thursday'
+  | 'Friday'
+  | 'Saturday'
+  | 'Sunday';
 
-  const day = 'Monday' as Day;
-  let result = 0;
+const day = 'Monday' as Day;
+let result = 0;
 
-  switch (day) {
-    case 'Monday': {
-      result = 1;
-      break;
-    }
-    default: {
-      result = 42;
-    }
+switch (day) {
+  case 'Monday': {
+    result = 1;
+    break;
   }
+  default: {
+    result = 42;
+  }
+}
       `,
       options: [
         {
