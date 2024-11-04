c5d5163c86740978f46b4c205f2ed2336126f17f
Kirk Waiblinger <kirk.waiblinger@gmail.com>
2024-11-03 17:25:57 -0800

    unused imports

--- packages/eslint-plugin/src/rules/consistent-type-definitions.ts
+++ packages/eslint-plugin/src/rules/consistent-type-definitions.ts
@@ -1,7 +1,6 @@
 import type { TSESLint, TSESTree } from '@typescript-eslint/utils';
 
-import { AST_NODE_TYPES, AST_TOKEN_TYPES } from '@typescript-eslint/utils';
-import * as ts from 'typescript';
+import { AST_NODE_TYPES } from '@typescript-eslint/utils';
 
 import { createRule, nullThrows, NullThrowsReasons } from '../util';
 
