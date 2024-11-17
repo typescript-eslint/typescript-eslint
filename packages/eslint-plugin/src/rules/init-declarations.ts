3eca8be8ffb0361935bfd09cb4d55b25918a1719
Brad Zacher <brad.zacher@gmail.com>
2024-11-16 19:35:01 -0800

    Update init-declarations.ts

--- packages/eslint-plugin/src/rules/init-declarations.ts
+++ packages/eslint-plugin/src/rules/init-declarations.ts
@@ -15,13 +15,10 @@ const baseRule = getESLintCoreRule('init-declarations');
 export type Options = InferOptionsTypeFromRule<typeof baseRule>;
 export type MessageIds = InferMessageIdsTypeFromRule<typeof baseRule>;
 
-const defaultOptions: Options = ['always'];
-
 export default createRule<Options, MessageIds>({
   name: 'init-declarations',
   meta: {
     type: 'suggestion',
-    defaultOptions,
     docs: {
       description:
         'Require or disallow initialization in variable declarations',
@@ -31,7 +28,7 @@ export default createRule<Options, MessageIds>({
     messages: baseRule.meta.messages,
     schema: baseRule.meta.schema,
   },
-  defaultOptions,
+  defaultOptions: ['always'],
   create(context, [mode]) {
     // Make a custom context to adjust the loc of reports where the base
     // rule's behavior is a bit too aggressive with TS-specific syntax (namely,
