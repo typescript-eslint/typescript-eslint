cac88cafa1639d20ed5cf164c5900f1c4f9f7125
Mark de Dios <99303358+peanutenthusiast@users.noreply.github.com>
2024-11-22 11:28:34 -0800

    chore: set no-else-return allowIfElse to false (#10325)
    * add alowElseIf: false to no return else config
    
    * Add yielded variable to consitent type imports fixToTypeImportDeclaration use for initial control
     flow
    
    * Add output variables for error firing if else statements, refactor no magic numbers if else block to use switch statement
    
    * Add return early boolean to no magic numbers for control flow satement lines 113-136
    
    * Remove output variables from no else return allow else if error firing control statements, change else if statements to singular if statements

--- packages/eslint-plugin/src/rules/no-floating-promises.ts
+++ packages/eslint-plugin/src/rules/no-floating-promises.ts
@@ -335,6 +335,8 @@ export default createRule<Options, MessageId>({
         return { isUnhandled: false };
       }
 
+      let output = { isUnhandled: true };
+
       if (node.type === AST_NODE_TYPES.CallExpression) {
         // If the outer expression is a call, a `.catch()` or `.then()` with
         // rejection handler handles the promise.
@@ -375,7 +377,9 @@ export default createRule<Options, MessageId>({
 
         // All other cases are unhandled.
         return { isUnhandled: true };
-      } else if (node.type === AST_NODE_TYPES.ConditionalExpression) {
+      }
+
+      if (node.type === AST_NODE_TYPES.ConditionalExpression) {
         // We must be getting the promise-like value from one of the branches of the
         // ternary. Check them directly.
         const alternateResult = isUnhandledPromise(checker, node.alternate);
@@ -383,7 +387,9 @@ export default createRule<Options, MessageId>({
           return alternateResult;
         }
         return isUnhandledPromise(checker, node.consequent);
-      } else if (node.type === AST_NODE_TYPES.LogicalExpression) {
+      }
+
+      if (node.type === AST_NODE_TYPES.LogicalExpression) {
         const leftResult = isUnhandledPromise(checker, node.left);
         if (leftResult.isUnhandled) {
           return leftResult;
