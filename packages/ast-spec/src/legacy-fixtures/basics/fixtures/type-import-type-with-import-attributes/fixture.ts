def5643020bca67080749b559ff7165182c73ca5
Ronen Amiel <ronen.amiel@gmail.com>
2025-01-12 19:29:25 +0200

    remove unnecessary typeof

--- packages/ast-spec/src/legacy-fixtures/basics/fixtures/type-import-type-with-import-attributes/fixture.ts
+++ packages/ast-spec/src/legacy-fixtures/basics/fixtures/type-import-type-with-import-attributes/fixture.ts
@@ -1,3 +1,3 @@
 // TODO: This fixture might be too large, and if so should be split up.
 
-type A = typeof import("A", { with: { type: "json" } });
+type A = import("A", { with: { type: "json" } });
