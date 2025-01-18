507c7fd5b98197c2bbb568e5195246759468f2fa
Arya Emami <aryaemami59@yahoo.com>
2025-01-18 02:50:21 -0600

    chore(scope-manager): migrate to `vitest`

--- packages/scope-manager/tests/fixtures.test.ts
+++ packages/scope-manager/tests/fixtures.test.ts
@@ -1,6 +1,5 @@
 import * as glob from 'glob';
-import makeDir from 'make-dir';
-import fs from 'node:fs';
+import * as fs from 'node:fs/promises';
 import path from 'node:path';
 
 import type { AnalyzeOptions } from './test-utils';
@@ -59,137 +58,119 @@ function nestDescribe(
       nestDescribe(fixture, segments.slice(1));
     });
   } else {
-    const test = (): void => {
-      const contents = fs.readFileSync(fixture.absolute, 'utf8');
+    test(
+      fixture.name,
+      { only: [...fixture.segments, fixture.name].join(path.sep) === ONLY },
+      async () => {
+        const contents = await fs.readFile(fixture.absolute, 'utf8');
 
-      const lines = contents.split('\n');
-      const options: Record<string, unknown> = {
-        lib: [],
-      };
+        const lines = contents.split('\n');
+        const options: Record<string, unknown> = {
+          lib: [],
+        };
 
-      /*
-       * What's all this!?
-       *
-       * To help with configuring individual tests, each test may use a four-slash comment to configure the scope manager
-       * This is just a rudimentary "parser" for said comments.
-       */
-      for (const line of lines) {
-        if (!line.startsWith('////')) {
-          continue;
-        }
-
-        const match = FOUR_SLASH.exec(line);
-        if (!match) {
-          throw new Error(`Four-slash did not match expected format: ${line}`);
-        }
-        const [, key, rawValue] = match;
-        const type = ALLOWED_OPTIONS.get(key);
-        if (!type) {
-          throw new Error(`Unknown option ${key}`);
-        }
-
-        let value: unknown = rawValue;
-        switch (type[0]) {
-          case 'string': {
-            const strmatch = QUOTED_STRING.exec(rawValue);
-            if (strmatch) {
-              value = strmatch[1];
-            }
-            break;
+        /*
+         * What's all this!?
+         *
+         * To help with configuring individual tests, each test may use a four-slash comment to configure the scope manager
+         * This is just a rudimentary "parser" for said comments.
+         */
+        for (const line of lines) {
+          if (!line.startsWith('////')) {
+            continue;
           }
 
-          case 'number': {
-            const parsed = parseFloat(rawValue);
-            if (isNaN(parsed)) {
-              throw new Error(
-                `Expected a number for ${key}, but got ${rawValue}`,
-              );
-            }
-            value = parsed;
-            break;
+          const match = FOUR_SLASH.exec(line);
+          if (!match) {
+            throw new Error(
+              `Four-slash did not match expected format: ${line}`,
+            );
+          }
+          const [, key, rawValue] = match;
+          const type = ALLOWED_OPTIONS.get(key);
+          if (!type) {
+            throw new Error(`Unknown option ${key}`);
           }
 
-          case 'boolean': {
-            if (rawValue === 'true') {
-              value = true;
-            } else if (rawValue === 'false') {
-              value = false;
-            } else {
-              throw new Error(
-                `Expected a boolean for ${key}, but got ${rawValue}`,
-              );
+          let value: unknown = rawValue;
+          switch (type[0]) {
+            case 'string': {
+              const strmatch = QUOTED_STRING.exec(rawValue);
+              if (strmatch) {
+                value = strmatch[1];
+              }
+              break;
             }
-            break;
+
+            case 'number': {
+              const parsed = parseFloat(rawValue);
+              if (isNaN(parsed)) {
+                throw new Error(
+                  `Expected a number for ${key}, but got ${rawValue}`,
+                );
+              }
+              value = parsed;
+              break;
+            }
+
+            case 'boolean': {
+              if (rawValue === 'true') {
+                value = true;
+              } else if (rawValue === 'false') {
+                value = false;
+              } else {
+                throw new Error(
+                  `Expected a boolean for ${key}, but got ${rawValue}`,
+                );
+              }
+              break;
+            }
+          }
+
+          if (type[1] && !type[1].has(value)) {
+            throw new Error(
+              `Expected value for ${key} to be one of (${[...type[1]].join(
+                ' | ',
+              )}), but got ${value as string}`,
+            );
+          }
+
+          if (value === 'true') {
+            options[key] = true;
+          } else if (value === 'false') {
+            options[key] = false;
+          } else {
+            options[key] = value;
           }
         }
 
-        if (type[1] && !type[1].has(value)) {
-          throw new Error(
-            `Expected value for ${key} to be one of (${[...type[1]].join(
-              ' | ',
-            )}), but got ${value as string}`,
-          );
+        await fs.mkdir(fixture.snapshotPath, { recursive: true });
+
+        try {
+          const { scopeManager } = parseAndAnalyze(contents, options, {
+            jsx: fixture.ext.endsWith('x'),
+          });
+          await expect(scopeManager).toMatchFileSnapshot(fixture.snapshotFile);
+        } catch (e) {
+          await expect(e).toMatchFileSnapshot(fixture.snapshotFile);
         }
-
-        if (value === 'true') {
-          options[key] = true;
-        } else if (value === 'false') {
-          options[key] = false;
-        } else {
-          options[key] = value;
-        }
-      }
-
-      try {
-        makeDir.sync(fixture.snapshotPath);
-      } catch (
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        e: any
-      ) {
-        if ('code' in e && e.code === 'EEXIST') {
-          // already exists - ignored
-        } else {
-          throw e;
-        }
-      }
-
-      try {
-        const { scopeManager } = parseAndAnalyze(contents, options, {
-          jsx: fixture.ext.endsWith('x'),
-        });
-        expect(scopeManager).toMatchSpecificSnapshot(fixture.snapshotFile);
-      } catch (e) {
-        expect(e).toMatchSpecificSnapshot(fixture.snapshotFile);
-      }
-    };
-
-    if ([...fixture.segments, fixture.name].join(path.sep) === ONLY) {
-      // eslint-disable-next-line jest/no-focused-tests
-      it.only(fixture.name, test);
-    } else {
-      it(fixture.name, test);
-    }
+      },
+    );
   }
 }
 
 fixtures.forEach(f => nestDescribe(f));
 
-if (ONLY === '') {
-  // ensure that the snapshots are cleaned up, because jest-specific-snapshot won't do this check
-  const snapshots = glob.sync(`${FIXTURES_DIR}/**/*.shot`).map(absolute => {
-    const relative = path.relative(FIXTURES_DIR, absolute);
-    const { dir, name } = path.parse(relative);
-    return {
-      fixturePath: path.join(FIXTURES_DIR, dir, name),
-      relative,
-    };
-  });
-
-  describe('ast snapshots should have an associated test', () => {
-    for (const snap of snapshots) {
-      it(snap.relative, () => {
-        expect(fs.existsSync(snap.fixturePath)).toBeTruthy();
-      });
-    }
-  });
-}
+describe.runIf(ONLY === '')(
+  'ast snapshots should have an associated test',
+  () => {
+    const snapshots = glob.sync(`${FIXTURES_DIR}/**/*.shot`).map(absolute => {
+      const relative = path.relative(FIXTURES_DIR, absolute);
+      const { dir, name } = path.parse(relative);
+      return [relative, path.join(FIXTURES_DIR, dir, name)] as const;
+    });
+    it.for(snapshots)('%s', async ([, fixturePath], { expect }) => {
+      expect((await fs.stat(fixturePath)).isFile()).toBe(true);
+    });
+  },
+);
