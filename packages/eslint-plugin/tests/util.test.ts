d69c0e01a67e402903b09973c36245ea4f92d2a0
Arya Emami <aryaemami59@yahoo.com>
2025-01-17 22:02:46 -0600

    chore(eslint-plugin): migrate to `vitest`

--- packages/eslint-plugin/tests/util.test.ts
+++ packages/eslint-plugin/tests/util.test.ts
@@ -1,6 +1,6 @@
 import { isDefinitionFile, upperCaseFirst } from '../src/util';
 
-describe('isDefinitionFile', () => {
+describe(isDefinitionFile, () => {
   describe('returns false for non-definition files', () => {
     const invalid = [
       'test.js',
@@ -18,27 +18,23 @@ describe('isDefinitionFile', () => {
       // yes, it's not a definition file if it's a `.d.tsx`!
       'test.d.tsx',
       'test.D.TSX',
-    ];
+    ] as const;
 
-    invalid.forEach(f => {
-      it(f, () => {
-        expect(isDefinitionFile(f)).toBe(false);
-      });
+    it.for(invalid)('%s', (f, { expect }) => {
+      expect(isDefinitionFile(f)).toBe(false);
     });
   });
 
   describe('returns true for definition files', () => {
-    const valid = ['test.d.ts', 'test.D.TS'];
+    const valid = ['test.d.ts', 'test.D.TS'] as const;
 
-    valid.forEach(f => {
-      it(f, () => {
-        expect(isDefinitionFile(f)).toBe(true);
-      });
+    it.for(valid)('%s', (f, { expect }) => {
+      expect(isDefinitionFile(f)).toBe(true);
     });
   });
 });
 
-describe('upperCaseFirst', () => {
+describe(upperCaseFirst, () => {
   it('upper cases first', () => {
     expect(upperCaseFirst('hello')).toBe('Hello');
   });
