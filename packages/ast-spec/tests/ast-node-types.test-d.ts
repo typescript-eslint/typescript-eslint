6143d29feb4477772dfb7ced353e7a72272385b8
Arya Emami <aryaemami59@yahoo.com>
2025-02-04 00:56:08 -0600

    chore(ast-spec): migrate type tests to `vitest`

--- packages/ast-spec/tests/ast-node-types.test-d.ts
+++ packages/ast-spec/tests/ast-node-types.test-d.ts
@@ -9,8 +9,12 @@ type AllKeys = {
 
 type TakesString<T extends Record<string, string>> = T;
 
-type _Test =
-  // forcing the test onto a new line so it isn't covered by the expect error
-  // If there are any enum members that don't have a corresponding TSESTree.Node, then this line will error with "Type 'string | number | symbol' is not assignable to type 'string'."
-  // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
-  TakesString<AllKeys> | void;
+test('type tests', () => {
+  expectTypeOf<AllKeys>().toMatchTypeOf<Record<string, string>>();
+
+  type _Test =
+    // forcing the test onto a new line so it isn't covered by the expect error
+    // If there are any enum members that don't have a corresponding TSESTree.Node, then this line will error with "Type 'string | number | symbol' is not assignable to type 'string'."
+    // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
+    TakesString<AllKeys> | void;
+});
