136ce55671156e59ae1035bde8c0cb1f386dd1d5
Arya Emami <aryaemami59@yahoo.com>
2025-02-04 00:56:08 -0600

    chore(ast-spec): migrate type tests to `vitest`

--- packages/ast-spec/tests/PunctuatorTokenToText.test-d.ts
+++ packages/ast-spec/tests/PunctuatorTokenToText.test-d.ts
@@ -2,9 +2,15 @@ import type { PunctuationSyntaxKind } from 'typescript';
 
 import type { PunctuatorTokenToText } from '../src';
 
-type _Test = {
-  readonly [T in PunctuationSyntaxKind]: PunctuatorTokenToText[T];
-  // If there are any PunctuationSyntaxKind members that don't have a
-  // corresponding PunctuatorTokenToText, then this line will error with
-  // "Type 'T' cannot be used to index type 'PunctuatorTokenToText'."
-};
+test('type tests', () => {
+  expectTypeOf<PunctuatorTokenToText>()
+    .pick<PunctuationSyntaxKind>()
+    .toEqualTypeOf<PunctuatorTokenToText>();
+
+  type _Test = {
+    readonly [T in PunctuationSyntaxKind]: PunctuatorTokenToText[T];
+    // If there are any PunctuationSyntaxKind members that don't have a
+    // corresponding PunctuatorTokenToText, then this line will error with
+    // "Type 'T' cannot be used to index type 'PunctuatorTokenToText'."
+  };
+});
