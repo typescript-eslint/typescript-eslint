8c3e5447fd1cf410a62b7d16b66d5fc44cf6efa0
Arya Emami <aryaemami59@yahoo.com>
2025-02-08 05:28:31 -0600

    Correctly handle relative imports from the root workspace

--- packages/typescript-estree/tests/lib/tsVersionRangeSync.test.ts
+++ packages/typescript-estree/tests/lib/tsVersionRangeSync.test.ts
@@ -1,4 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/internal/no-relative-paths-to-internal-packages
 import packageJson from '../../../../package.json';
 import { SUPPORTED_TYPESCRIPT_VERSIONS } from '../../src/parseSettings/warnAboutTSVersion';
 
