479af6d401b4a5e780489807907b708a4680e5c6
Kirk Waiblinger <53019676+kirkwaiblinger@users.noreply.github.com>
2025-02-26 23:40:24 -0700

    review changes

--- packages/ast-spec/jest.config.js
+++ packages/ast-spec/jest.config.js
@@ -7,7 +7,6 @@ const baseConfig = require('../../jest.config.base.js');
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
   ...baseConfig,
-  collectCoverage: false,
   setupFilesAfterEnv: [
     ...baseConfig.setupFilesAfterEnv,
     './tests/util/setupJest.ts',
