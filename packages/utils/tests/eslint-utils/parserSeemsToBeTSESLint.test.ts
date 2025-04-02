1a2e35b080866e61451824c25c6fbafb36f43a61
Arya Emami <aryaemami59@yahoo.com>
2025-04-02 14:39:34 -0500

    Switch to `test.for`

--- packages/utils/tests/eslint-utils/parserSeemsToBeTSESLint.test.ts
+++ packages/utils/tests/eslint-utils/parserSeemsToBeTSESLint.test.ts
@@ -1,7 +1,7 @@
 import { parserSeemsToBeTSESLint } from '../../src/eslint-utils/parserSeemsToBeTSESLint';
 
 describe(parserSeemsToBeTSESLint, () => {
-  test.each([
+  test.for([
     [undefined, false],
     ['espree', false],
     ['local.js', false],
@@ -20,7 +20,7 @@ describe(parserSeemsToBeTSESLint, () => {
     ['/path/to/typescript-eslint/packages/parser/index.js', true],
     ['/path/to/@typescript-eslint/packages/parser/dist/index.js', true],
     ['/path/to/@typescript-eslint/packages/parser/index.js', true],
-  ] as const)('%s', (parserPath, expected) => {
+  ] as const)('%s', ([parserPath, expected], { expect }) => {
     expect(parserSeemsToBeTSESLint(parserPath)).toBe(expected);
   });
 });
