6c3cb8734949b0c2bff69a5e476d4cd688f5c5b5
dbarabashh <dbarabashdev@gmail.com>
2025-05-03 16:09:35 +0200

    fix

--- packages/ast-spec/src/type/TSMappedType/fixtures/valid/with-modifiers/fixture.ts
+++ packages/ast-spec/src/type/TSMappedType/fixtures/valid/with-modifiers/fixture.ts
@@ -1,5 +1,11 @@
-type T = {
+type ReadonlyOptional = {
   readonly [P in string]?: number;
+};
+
+type PlusReadonly = {
   +readonly [P in string]: number;
+};
+
+type MinusReadonlyOptional = {
   -readonly [P in string]-?: number;
 };
