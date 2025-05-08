872a9f681fc4d1c78330cfff32dc373f00157ea8
Arya Emami <aryaemami59@yahoo.com>
2025-05-08 14:53:37 -0500

    use the `async` version of `glob`

--- packages/ast-spec/tests/fixtures.test.ts
+++ packages/ast-spec/tests/fixtures.test.ts
@@ -1,4 +1,4 @@
-import * as glob from 'glob';
+import { glob } from 'glob';
 import * as fs from 'node:fs/promises';
 import * as path from 'node:path';
 import { pathToFileURL } from 'node:url';
@@ -21,8 +21,8 @@ const SRC_DIR = path.join(PACKAGE_ROOT, 'src');
 // prettier-ignore
 const ONLY = [].join(path.sep);
 
-const fixturesWithASTDifferences = new Set<string>();
-const fixturesWithTokenDifferences = new Set<string>();
+const fixturesWithASTDifferences = new Map<string, string>();
+const fixturesWithTokenDifferences = new Map<string, string>();
 const fixturesConfiguredToExpectBabelToNotSupport = new Map<string, string>();
 
 const fixturesWithErrorDifferences = {
@@ -30,28 +30,24 @@ const fixturesWithErrorDifferences = {
   [ErrorLabel.TSESTree]: new Set<string>(),
 } as const;
 
-const VALID_FIXTURES: readonly string[] = glob.sync(
-  `**/fixtures/*/fixture.{ts,tsx}`,
-  {
-    absolute: true,
-    cwd: SRC_DIR,
-  },
-);
-const ERROR_FIXTURES: readonly string[] = glob.sync(
-  `**/fixtures/_error_/*/fixture.{ts,tsx}`,
-  {
-    absolute: true,
-    cwd: SRC_DIR,
-  },
-);
-
 describe('AST Fixtures', async () => {
+  const VALID_FIXTURES = await glob(
+    ['**/fixtures/*/fixture.ts?(x)', '**/fixtures/_error_/*/fixture.ts?(x)'],
+    {
+      absolute: true,
+      cwd: SRC_DIR,
+    },
+  );
+
   const FIXTURES: readonly Fixture[] = await Promise.all(
-    [...VALID_FIXTURES, ...ERROR_FIXTURES].map(async absolute => {
+    VALID_FIXTURES.map(async absolute => {
       const relativeToSrc = path.relative(SRC_DIR, absolute);
       const { base, dir, ext } = path.parse(relativeToSrc);
       const directorySegments = dir.split(path.sep);
-      const segments = directorySegments.filter(s => s !== 'fixtures');
+      const segments = directorySegments.filter(
+        segment => segment !== 'fixtures',
+      );
+
       const name = segments.pop();
 
       assert.isDefined(name);
@@ -138,11 +134,11 @@ describe('AST Fixtures', async () => {
         );
 
         if (diffHasChanges(diffAstResult)) {
-          fixturesWithASTDifferences.add(relative);
+          fixturesWithASTDifferences.set(relative, diffAstResult);
         }
 
         if (diffHasChanges(diffTokensResult)) {
-          fixturesWithTokenDifferences.add(relative);
+          fixturesWithTokenDifferences.set(relative, diffTokensResult);
         }
       }
 
@@ -212,6 +208,7 @@ describe('AST Fixtures', async () => {
           contents,
           errorLabel,
           isError,
+          relative,
           snapshotFiles,
           TSESTreeParsed,
           vitestSnapshotHeader,
@@ -255,6 +252,7 @@ describe('AST Fixtures', async () => {
 
         it.skipIf(isError)('TSESTree - AST', async () => {
           assert.isSuccessResponse(TSESTreeParsed);
+
           await expect(TSESTreeParsed.ast).toMatchFileSnapshot(
             snapshotFiles.success.tsestree.ast(1),
           );
@@ -262,6 +260,7 @@ describe('AST Fixtures', async () => {
 
         it.skipIf(isError)('TSESTree - Tokens', async () => {
           assert.isSuccessResponse(TSESTreeParsed);
+
           await expect(TSESTreeParsed.tokens).toMatchFileSnapshot(
             snapshotFiles.success.tsestree.tokens(2),
           );
@@ -292,6 +291,7 @@ describe('AST Fixtures', async () => {
           'Babel - AST',
           async () => {
             assert.isSuccessResponse(babelParsed);
+
             await expect(babelParsed.ast).toMatchFileSnapshot(
               snapshotFiles.success.babel.ast(3),
             );
@@ -302,53 +302,48 @@ describe('AST Fixtures', async () => {
           'Babel - Tokens',
           async () => {
             assert.isSuccessResponse(babelParsed);
+
             await expect(babelParsed.tokens).toMatchFileSnapshot(
               snapshotFiles.success.babel.tokens(4),
             );
           },
         );
 
-        it.skipIf(isError || hasExpectBabelToNotSupport)(
-          'AST Alignment - AST',
-          async () => {
-            assert.isSuccessResponse(TSESTreeParsed);
-            assert.isSuccessResponse(babelParsed);
-            const diffResult = snapshotDiff(
-              'TSESTree',
-              TSESTreeParsed.ast,
-              'Babel',
-              babelParsed.ast,
-            );
-            await expect(
-              [
-                `${vitestSnapshotHeader}\n\nexports[\`${expect.getState().currentTestName}\`]`,
-                diffResult,
-                '',
-              ].join('\n'),
-            ).toMatchFileSnapshot(snapshotFiles.success.alignment.ast(5));
-          },
-        );
+        it.skipIf(
+          isError ||
+            hasExpectBabelToNotSupport ||
+            !fixturesWithASTDifferences.has(relative),
+        )('AST Alignment - AST', async () => {
+          const diffResult = fixturesWithASTDifferences.get(relative);
 
-        it.skipIf(isError || hasExpectBabelToNotSupport)(
-          'AST Alignment - Token',
-          async () => {
-            assert.isSuccessResponse(TSESTreeParsed);
-            assert.isSuccessResponse(babelParsed);
-            const diffResult = snapshotDiff(
-              'TSESTree',
-              TSESTreeParsed.tokens,
-              'Babel',
-              babelParsed.tokens,
-            );
-            await expect(
-              [
-                `${vitestSnapshotHeader}\n\nexports[\`${expect.getState().currentTestName}\`]`,
-                diffResult,
-                '',
-              ].join('\n'),
-            ).toMatchFileSnapshot(snapshotFiles.success.alignment.tokens(6));
-          },
-        );
+          assert.isDefined(diffResult);
+
+          await expect(
+            [
+              `${vitestSnapshotHeader}\n\nexports[\`${expect.getState().currentTestName}\`]`,
+              diffResult,
+              '',
+            ].join('\n'),
+          ).toMatchFileSnapshot(snapshotFiles.success.alignment.ast(5));
+        });
+
+        it.skipIf(
+          isError ||
+            hasExpectBabelToNotSupport ||
+            !fixturesWithTokenDifferences.has(relative),
+        )('AST Alignment - Token', async () => {
+          const diffResult = fixturesWithTokenDifferences.get(relative);
+
+          assert.isDefined(diffResult);
+
+          await expect(
+            [
+              `${vitestSnapshotHeader}\n\nexports[\`${expect.getState().currentTestName}\`]`,
+              diffResult,
+              '',
+            ].join('\n'),
+          ).toMatchFileSnapshot(snapshotFiles.success.alignment.tokens(6));
+        });
 
         it.skipIf(isError)('Should parse with no errors', () => {
           // log the error for debug purposes in case there wasn't supposed to be an error
@@ -376,7 +371,7 @@ describe('AST Fixtures', async () => {
               break;
           }
 
-          // NOTE - the comments below exist so that they show up in the stack trace jest shows
+          // NOTE - the comments below exist so that they show up in the stack trace vitest shows
           //        when the test fails. Yes, sadly, they're duplicated, but it's necessary to
           //        provide the best and most understandable DevX that we can here.
           //        Vitest will print a code frame with the fail line as well as 2 lines before and after
@@ -405,7 +400,7 @@ describe('AST Fixtures', async () => {
     await expect(
       [
         `${vitestSnapshotHeader}\n\nexports[\`${expect.getState().currentTestName}\`]`,
-        JSON.stringify([...fixturesWithASTDifferences].sort(), null, 2),
+        JSON.stringify([...fixturesWithASTDifferences.keys()].sort(), null, 2),
         '',
       ].join('\n'),
     ).toMatchFileSnapshot(
@@ -417,7 +412,11 @@ describe('AST Fixtures', async () => {
     await expect(
       [
         `${vitestSnapshotHeader}\n\nexports[\`${expect.getState().currentTestName}\`]`,
-        JSON.stringify([...fixturesWithTokenDifferences].sort(), null, 2),
+        JSON.stringify(
+          [...fixturesWithTokenDifferences.keys()].sort(),
+          null,
+          2,
+        ),
         '',
       ].join('\n'),
     ).toMatchFileSnapshot(
