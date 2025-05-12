46e2e7e81b00ecab352f21ad7b54ef7214ced773
Kirk Waiblinger <53019676+kirkwaiblinger@users.noreply.github.com>
2025-05-12 15:19:44 -0700

    tweaks

--- packages/typescript-eslint/tests/defineConfig-interop.test-d.ts
+++ packages/typescript-eslint/tests/defineConfig-interop.test-d.ts
@@ -1,43 +1,28 @@
 import { defineConfig } from 'eslint/config';
 
-import type { Linter } from 'eslint';
-
 import * as tseslint from '../src/index.js';
 
+// Type tests to ensure that migration from `tseslint.config()` -> `defineConfig()`
+// doesn't introduce type errors for the user. We don't care about the
+// `defineConfig()` -> `tseslint.config()` direction.
 describe('tseslint.config() should be replaceable with defineConfig()', () => {
   it('should work with recommended setup', () => {
-    const eslintConfig = defineConfig(tseslint.configs.recommended);
-
-    const tseslintConfig = tseslint.config(tseslint.configs.recommended);
-
-    expectTypeOf(defineConfig)
-      .toBeCallableWith(tseslint.configs.recommended)
-      .returns.items.toExtend<(typeof tseslintConfig)[number]>();
-
-    expectTypeOf(eslintConfig).toExtend<typeof tseslintConfig>();
-
-    expectTypeOf(eslintConfig).items.toExtend<
-      (typeof tseslintConfig)[number]
-    >();
+    expectTypeOf(defineConfig).toBeCallableWith(tseslint.configs.recommended);
   });
 
   it('should allow manual assignment of the plugin', () => {
-    expectTypeOf(defineConfig)
-      .toBeCallableWith({
-        plugins: {
-          ts: tseslint.plugin,
-        },
-      })
-      .returns.items.toEqualTypeOf<Linter.Config>();
+    expectTypeOf(defineConfig).toBeCallableWith({
+      plugins: {
+        ts: tseslint.plugin,
+      },
+    });
   });
 
   it('should allow manual assignment of the parser', () => {
-    expectTypeOf(defineConfig)
-      .toBeCallableWith({
-        languageOptions: {
-          parser: tseslint.parser,
-        },
-      })
-      .returns.items.toEqualTypeOf<Linter.Config>();
+    expectTypeOf(defineConfig).toBeCallableWith({
+      languageOptions: {
+        parser: tseslint.parser,
+      },
+    });
   });
 });
