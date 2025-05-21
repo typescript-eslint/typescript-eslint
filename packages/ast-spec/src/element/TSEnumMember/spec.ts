f8d5ff0a1869bd213031fb5992c3ef5cdcf69281
fisker <lionkay@gmail.com>
2025-05-22 02:23:24 +0800

    Narrow types

--- packages/ast-spec/src/element/TSEnumMember/spec.ts
+++ packages/ast-spec/src/element/TSEnumMember/spec.ts
@@ -1,11 +1,12 @@
 import type { AST_NODE_TYPES } from '../../ast-node-types';
 import type { BaseNode } from '../../base/BaseNode';
+import type { Identifier } from '../../expression/Identifier/spec';
+import type { StringLiteral } from '../../expression/literal/StringLiteral/spec';
 import type { Expression } from '../../unions/Expression';
-import type { PropertyNameNonComputed } from '../../unions/PropertyName';
 
 export interface TSEnumMember extends BaseNode {
   type: AST_NODE_TYPES.TSEnumMember;
-  id: PropertyNameNonComputed;
+  id: Identifier | StringLiteral;
   initializer: Expression | undefined;
   /**
    * @deprecated the enum member is always non-computed.
