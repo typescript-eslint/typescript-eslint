303c3ceb035c367553344aaaf734f448fb10e0ac
nayounsang <nayounsang722@gmail.com>
2025-06-21 20:04:17 +0900

    test: add test case

--- packages/typescript-estree/tests/lib/inferSingleRun.test.ts
+++ packages/typescript-estree/tests/lib/inferSingleRun.test.ts
@@ -157,4 +157,12 @@ describe(inferSingleRun, () => {
 
     expect(actual).toBe(false);
   });
+
+  it('returns true for project service in single run', () => {
+    vi.stubEnv('CI', 'true');
+    // Do not set `project: true` if `projectService: true`
+    const actual = inferSingleRun({ projectService: true });
+
+    expect(actual).toBe(true);
+  });
 });
