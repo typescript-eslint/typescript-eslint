e7c3b860ba486e186b8cf23d4a3de8ede7bb865e
Josh Goldberg <git@joshuakgoldberg.com>
2025-06-25 13:47:14 -0400

    docs: overhaul homepage

---
+++ packages/website/src/components/home/ExplainerSpotlight/index.tsx
@@ -0,0 +1,26 @@
+import Heading from '@theme/Heading';
+import React from 'react';
+
+import styles from './styles.module.css';
+
+export interface ExplainerSpotlightProps extends React.PropsWithChildren {
+  header: string;
+  emoji: string;
+  href: string;
+}
+
+export function ExplainerSpotlight({
+  children,
+  emoji,
+  header,
+  href,
+}: ExplainerSpotlightProps): React.JSX.Element {
+  return (
+    <a className={styles.explainerSpotlight} href={href}>
+      <Heading as="h3" className={styles.heading}>
+        {header} <span>{emoji}</span>
+      </Heading>
+      <div>{children}</div>
+    </a>
+  );
+}
