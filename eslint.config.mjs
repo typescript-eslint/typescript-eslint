4b9d47a371e41fd900b37b082987c4d5815356ac
nayounsang <nayounsang722@gmail.com>
2025-07-10 22:12:38 +0900

    chore: test commit to enable new rule in CI

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -417,6 +417,7 @@ export default tseslint.config(
     name: 'eslint-plugin-and-eslint-plugin-internal/test-files/rules',
     rules: {
       '@typescript-eslint/internal/plugin-test-formatting': 'error',
+      '@typescript-eslint/internal/no-dynamic-tests': 'error',
     },
   },
 
