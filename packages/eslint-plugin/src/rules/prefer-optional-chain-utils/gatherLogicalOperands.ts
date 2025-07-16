87107cdac33353b3656a3b0c5f0b7a9ffd267e83
nayounsang <nayounsang722@gmail.com>
2025-07-16 17:31:16 +0900

    chore: revert gatherLogicalOperands.ts

--- packages/eslint-plugin/src/rules/prefer-optional-chain-utils/gatherLogicalOperands.ts
+++ packages/eslint-plugin/src/rules/prefer-optional-chain-utils/gatherLogicalOperands.ts
@@ -59,6 +59,8 @@ export interface InvalidOperand {
   type: OperandValidity.Invalid;
 }
 type Operand = InvalidOperand | ValidOperand;
+
+const NULLISH_FLAGS = ts.TypeFlags.Null | ts.TypeFlags.Undefined;
 function isValidFalseBooleanCheckType(
   node: TSESTree.Node,
   disallowFalseyLiteral: boolean,
@@ -90,27 +92,26 @@ function isValidFalseBooleanCheckType(
     return false;
   }
 
-  let flagsToExcludeFromCheck = 0;
-  if (options.checkAny !== true) {
-    flagsToExcludeFromCheck |= ts.TypeFlags.Any;
+  let allowedFlags = NULLISH_FLAGS | ts.TypeFlags.Object;
+  if (options.checkAny === true) {
+    allowedFlags |= ts.TypeFlags.Any;
   }
-  if (options.checkUnknown !== true) {
-    flagsToExcludeFromCheck |= ts.TypeFlags.Unknown;
+  if (options.checkUnknown === true) {
+    allowedFlags |= ts.TypeFlags.Unknown;
   }
-  if (options.checkString !== true) {
-    flagsToExcludeFromCheck |= ts.TypeFlags.StringLike;
+  if (options.checkString === true) {
+    allowedFlags |= ts.TypeFlags.StringLike;
   }
-  if (options.checkNumber !== true) {
-    flagsToExcludeFromCheck |= ts.TypeFlags.NumberLike;
+  if (options.checkNumber === true) {
+    allowedFlags |= ts.TypeFlags.NumberLike;
   }
-  if (options.checkBoolean !== true) {
-    flagsToExcludeFromCheck |= ts.TypeFlags.BooleanLike;
+  if (options.checkBoolean === true) {
+    allowedFlags |= ts.TypeFlags.BooleanLike;
   }
-  if (options.checkBigInt !== true) {
-    flagsToExcludeFromCheck |= ts.TypeFlags.BigIntLike;
+  if (options.checkBigInt === true) {
+    allowedFlags |= ts.TypeFlags.BigIntLike;
   }
-
-  return types.every(t => !isTypeFlagSet(t, flagsToExcludeFromCheck));
+  return types.every(t => isTypeFlagSet(t, allowedFlags));
 }
 
 export function gatherLogicalOperands(
@@ -247,13 +248,12 @@ export function gatherLogicalOperands(
       case AST_NODE_TYPES.UnaryExpression:
         if (
           operand.operator === '!' &&
-          (!areMoreOperands ||
-            isValidFalseBooleanCheckType(
-              operand.argument,
-              node.operator === '||',
-              parserServices,
-              options,
-            ))
+          isValidFalseBooleanCheckType(
+            operand.argument,
+            areMoreOperands && node.operator === '||',
+            parserServices,
+            options,
+          )
         ) {
           result.push({
             comparedName: operand.argument,
@@ -274,10 +274,9 @@ export function gatherLogicalOperands(
 
       default:
         if (
-          !areMoreOperands ||
           isValidFalseBooleanCheckType(
             operand,
-            node.operator === '&&',
+            areMoreOperands && node.operator === '&&',
             parserServices,
             options,
           )
