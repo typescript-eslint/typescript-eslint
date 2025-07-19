23915a6d7cf930ad8091e4ab3f1d3b3e8cdabc98
Kirk Waiblinger <53019676+kirkwaiblinger@users.noreply.github.com>
2025-07-18 23:06:35 -0600

    remove unnecessary defensive coding

--- packages/typescript-eslint/src/getTSConfigRootDirFromStack.ts
+++ packages/typescript-eslint/src/getTSConfigRootDirFromStack.ts
@@ -2,27 +2,27 @@ import path from 'node:path';
 
 /**
  * Infers the `tsconfigRootDir` from the current call stack, using the V8 API.
- * This appears to be available in current Deno and Bun as well.
+ *
+ * See https://v8.dev/docs/stack-trace-api
+ *
+ * This API is implemented in Deno and Bun as well.
  */
 export function getTSConfigRootDirFromStack(): string | undefined {
   function getStack(): NodeJS.CallSite[] {
     const stackTraceLimit = Error.stackTraceLimit;
     Error.stackTraceLimit = Infinity;
-    try {
-      const prepareStackTrace = Error.prepareStackTrace;
-      Error.prepareStackTrace = (_, structuredStackTrace) =>
-        structuredStackTrace;
-      try {
-        const dummyObject: { stack?: NodeJS.CallSite[] } = {};
-        Error.captureStackTrace(dummyObject, getTSConfigRootDirFromStack);
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- stack is set by captureStackTrace
-        return dummyObject.stack!;
-      } finally {
-        Error.prepareStackTrace = prepareStackTrace;
-      }
-    } finally {
-      Error.stackTraceLimit = stackTraceLimit;
-    }
+    const prepareStackTrace = Error.prepareStackTrace;
+    Error.prepareStackTrace = (_, structuredStackTrace) => structuredStackTrace;
+
+    const dummyObject: { stack?: NodeJS.CallSite[] } = {};
+    Error.captureStackTrace(dummyObject, getTSConfigRootDirFromStack);
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- stack is set by captureStackTrace
+    const rv = dummyObject.stack!;
+
+    Error.prepareStackTrace = prepareStackTrace;
+    Error.stackTraceLimit = stackTraceLimit;
+
+    return rv;
   }
 
   for (const callSite of getStack()) {
