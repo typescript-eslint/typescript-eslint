3f280f74e0b36294c8607fc3b69ab07e24151bdb
Zach Siegel <zsiegel@walleyecapital.com>
2025-08-05 09:36:16 -0400

    remove newline in lint unboundWithoutThisAnnotation message

--- packages/eslint-plugin/src/rules/unbound-method.ts
+++ packages/eslint-plugin/src/rules/unbound-method.ts
@@ -115,7 +115,7 @@ export default createRule<Options, MessageIds>({
     },
     messages: {
       unbound: BASE_MESSAGE,
-      unboundWithoutThisAnnotation: `${BASE_MESSAGE}\nIf your function does not access \`this\`, you can annotate it with \`this: void\`, or consider using an arrow function instead.`,
+      unboundWithoutThisAnnotation: `${BASE_MESSAGE} If your function does not access \`this\`, you can annotate it with \`this: void\`, or consider using an arrow function instead.`,
     },
     schema: [
       {
