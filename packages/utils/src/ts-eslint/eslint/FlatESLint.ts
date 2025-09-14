d4443b37df011401cf09cb96f0a587062af6cd00
Joshua Chen <sidachen2003@gmail.com>
2025-09-14 19:52:08 -0400

    Update packages/utils/src/ts-eslint/eslint/FlatESLint.ts

--- packages/utils/src/ts-eslint/eslint/FlatESLint.ts
+++ packages/utils/src/ts-eslint/eslint/FlatESLint.ts
@@ -49,13 +49,12 @@ export namespace FlatESLint {
      */
     ignorePatterns?: string[] | null;
     /**
-     * The path to a configuration file.
+     * The path to a configuration file. Overrides all configurations used with this instance.
+     * The options.overrideConfig option is applied after this option is applied.
      *
-     * - Overrides all configurations used with this instance.
-     * - The options.overrideConfig option is applied after this option is applied.
-     * - Searches for default config file when falsy.
-     * - Does not do any config file lookup when `true`.
-     * - Considered to be a config filename when a string.
+     * - When falsy, searches for default config file.
+     * - When `true`, does not do any config file lookup.
+     * - When a string, considered to be a config file name.
      *
      * @default false
      */
