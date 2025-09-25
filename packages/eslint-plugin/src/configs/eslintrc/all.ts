a17acf15c9c16fe11072140bb2f942d13d9ce455
Josh Goldberg <git@joshuakgoldberg.com>
2025-09-25 10:15:49 -0400

    Revert "Also: no-redeclare"
    This reverts commit 08535a59040058fba776999e3d87ba2e8ad4a47b.

--- packages/eslint-plugin/src/configs/eslintrc/all.ts
+++ packages/eslint-plugin/src/configs/eslintrc/all.ts
@@ -60,6 +60,8 @@ export = {
     '@typescript-eslint/no-non-null-asserted-nullish-coalescing': 'error',
     '@typescript-eslint/no-non-null-asserted-optional-chain': 'error',
     '@typescript-eslint/no-non-null-assertion': 'error',
+    'no-redeclare': 'off',
+    '@typescript-eslint/no-redeclare': 'error',
     '@typescript-eslint/no-redundant-type-constituents': 'error',
     '@typescript-eslint/no-require-imports': 'error',
     '@typescript-eslint/no-restricted-types': 'error',
