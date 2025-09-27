4dd2118f945f5c448c925ac570e67feaa5e6c014
Abraham Guo <aguo@precisionplanting.com>
2025-09-27 15:18:28 -0500

    enumerate unary expressions

--- packages/ast-spec/src/type/TSLiteralType/spec.ts
+++ packages/ast-spec/src/type/TSLiteralType/spec.ts
@@ -2,12 +2,16 @@ import type { AST_NODE_TYPES } from '../../ast-node-types';
 import type { BaseNode } from '../../base/BaseNode';
 import type { NullLiteral } from '../../expression/literal/NullLiteral/spec';
 import type { RegExpLiteral } from '../../expression/literal/RegExpLiteral/spec';
-import type { UnaryExpression } from '../../expression/UnaryExpression/spec';
+import type {
+  UnaryExpressionMinus,
+  UnaryExpressionPlus,
+} from '../../expression/UnaryExpression/spec';
 import type { LiteralExpression } from '../../unions/LiteralExpression';
 
 export interface TSLiteralType extends BaseNode {
   type: AST_NODE_TYPES.TSLiteralType;
   literal:
     | Exclude<LiteralExpression, NullLiteral | RegExpLiteral>
-    | (UnaryExpression & { operator: '+' | '-' });
+    | UnaryExpressionMinus
+    | UnaryExpressionPlus;
 }
