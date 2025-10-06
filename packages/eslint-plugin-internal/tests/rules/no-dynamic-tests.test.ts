74dd89afb8c0f47130c10c4e8c6d97d4a983f9f5
Josh Goldberg <git@joshuakgoldberg.com>
2025-10-06 09:02:09 -0400

    fix typecheck

--- packages/eslint-plugin-internal/tests/rules/no-dynamic-tests.test.ts
+++ packages/eslint-plugin-internal/tests/rules/no-dynamic-tests.test.ts
@@ -1,6 +1,6 @@
 import { RuleTester } from '@typescript-eslint/rule-tester';
 
-import rule from '../../src/rules/no-dynamic-tests';
+import rule from '../../src/rules/no-dynamic-tests.js';
 
 const ruleTester = new RuleTester({
   languageOptions: {
