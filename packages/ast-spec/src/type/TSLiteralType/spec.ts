8b0520832d32a9f80911007693657760a5ff98cc
Abraham Guo <aguo@precisionplanting.com>
2025-10-06 20:54:58 -0500

    fix(ast-spec): cleanup `TSLiteralType` (#11624)

--- packages/ast-spec/src/type/TSLiteralType/spec.ts
+++ packages/ast-spec/src/type/TSLiteralType/spec.ts
@@ -1,10 +1,17 @@
 import type { AST_NODE_TYPES } from '../../ast-node-types';
 import type { BaseNode } from '../../base/BaseNode';
-import type { UnaryExpression } from '../../expression/UnaryExpression/spec';
-import type { UpdateExpression } from '../../expression/UpdateExpression/spec';
+import type { NullLiteral } from '../../expression/literal/NullLiteral/spec';
+import type { RegExpLiteral } from '../../expression/literal/RegExpLiteral/spec';
+import type {
+  UnaryExpressionMinus,
+  UnaryExpressionPlus,
+} from '../../expression/UnaryExpression/spec';
 import type { LiteralExpression } from '../../unions/LiteralExpression';
 
 export interface TSLiteralType extends BaseNode {
   type: AST_NODE_TYPES.TSLiteralType;
-  literal: LiteralExpression | UnaryExpression | UpdateExpression;
+  literal:
+    | Exclude<LiteralExpression, NullLiteral | RegExpLiteral>
+    | UnaryExpressionMinus
+    | UnaryExpressionPlus;
 }
