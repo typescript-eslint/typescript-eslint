6ced6b355ce01e748b1ef259f6ee9ba5e978f870
Lonercode <penpalclassmate@gmail.com>
2025-11-09 17:09:47 +0100

    chore: add test for ast-checks.ts

--- packages/typescript-estree/tests/lib/ast-checks.test.ts
+++ packages/typescript-estree/tests/lib/ast-checks.test.ts
@@ -1,7 +1,5 @@
 import * as ts from 'typescript';
-
 import type { TSESTree } from '../../src/ts-estree';
-
 import { checkForStatementDeclaration } from '../../src/ast-checks';
 
 describe(checkForStatementDeclaration, () => {
@@ -45,5 +43,102 @@ describe(checkForStatementDeclaration, () => {
     ).toThrow(
       "Only a single variable declaration is allowed in a 'for...of' statement.",
     );
+  });
+
+  it('throws when declaration has an initializer', () => {
+    const node = ts.factory.createVariableDeclarationList([
+      ts.factory.createVariableDeclaration(
+        'x',
+        undefined,
+        undefined,
+        ts.factory.createNumericLiteral(5),
+      ),
+    ]);
+
+    expect(() =>
+      checkForStatementDeclaration(
+        node,
+        ts.SyntaxKind.ForOfStatement,
+        throwError,
+      ),
+    ).toThrow(
+      "The variable declaration of a 'for...of' statement cannot have an initializer.",
+    );
+  });
+
+  it('throws when declaration has a type annotation', () => {
+    const node = ts.factory.createVariableDeclarationList([
+      ts.factory.createVariableDeclaration(
+        'x',
+        undefined,
+        ts.factory.createKeywordTypeNode(ts.SyntaxKind.NumberKeyword),
+      ),
+    ]);
+
+    expect(() =>
+      checkForStatementDeclaration(
+        node,
+        ts.SyntaxKind.ForOfStatement,
+        throwError,
+      ),
+    ).toThrow(
+      "The variable declaration of a 'for...of' statement cannot have a type annotation.",
+    );
+  });
+
+  it('throws when "using" declaration in for...in', () => {
+    const node = ts.factory.createVariableDeclarationList(
+      [ts.factory.createVariableDeclaration('x')],
+      ts.NodeFlags.Using,
+    );
+
+    expect(() =>
+      checkForStatementDeclaration(
+        node,
+        ts.SyntaxKind.ForInStatement,
+        throwError,
+      ),
+    ).toThrow(
+      "The left-hand side of a 'for...in' statement cannot be a 'using' declaration.",
+    );
+  });
+
+  it('throws when initializer is not a valid assignment target', () => {
+    const node = ts.factory.createNumericLiteral(42);
+
+    expect(() =>
+      checkForStatementDeclaration(
+        node,
+        ts.SyntaxKind.ForOfStatement,
+        throwError,
+      ),
+    ).toThrow(
+      "The left-hand side of a 'for...of' statement must be a variable or a property access.",
+    );
+  });
+
+  it('handles both for...in and for...of paths', () => {
+    const forInNode = ts.factory.createVariableDeclarationList([
+      ts.factory.createVariableDeclaration('x'),
+    ]);
+    const forOfNode = ts.factory.createVariableDeclarationList([
+      ts.factory.createVariableDeclaration('x'),
+    ]);
+
+    expect(() =>
+      checkForStatementDeclaration(
+        forInNode,
+        ts.SyntaxKind.ForInStatement,
+        throwError,
+      ),
+    ).not.toThrow();
+
+    expect(() =>
+      checkForStatementDeclaration(
+        forOfNode,
+        ts.SyntaxKind.ForOfStatement,
+        throwError,
+      ),
+    ).not.toThrow();
   });
 });
