f7beddc4ddfa7ce64b970189addc550c1a100556
Brad Zacher <brad.zacher@gmail.com>
2025-11-11 11:21:50 -0500

    review

--- packages/eslint-plugin/src/util/class-scope-analyzer/types.ts
+++ packages/eslint-plugin/src/util/class-scope-analyzer/types.ts
@@ -13,8 +13,8 @@ export type PrivateKey = string & { __brand: 'private-key' };
 export type PublicKey = string & { __brand: 'public-key' };
 export type Key = PrivateKey | PublicKey;
 
-export function publicKey(node: string): PublicKey {
-  return node as PublicKey;
+export function publicKey(name: string): PublicKey {
+  return name as PublicKey;
 }
 export function privateKey(node: TSESTree.PrivateIdentifier): PrivateKey {
   return `#private@@${node.name}` as PrivateKey;
