a6aebe223c9d8345cba8d1736da51e88226a641d
Dima <dbarabashdev@gmail.com>
2025-11-14 17:53:36 +0000

    disallow binding patterns in parameter properties

--- packages/ast-spec/src/parameter/TSParameterProperty/spec.ts
+++ packages/ast-spec/src/parameter/TSParameterProperty/spec.ts
@@ -2,16 +2,15 @@ import type { AST_NODE_TYPES } from '../../ast-node-types';
 import type { Accessibility } from '../../base/Accessibility';
 import type { BaseNode } from '../../base/BaseNode';
 import type { Decorator } from '../../special/Decorator/spec';
-import type { BindingName } from '../../unions/BindingName';
+import type { Identifier } from '../../expression/Identifier/spec';
 import type { AssignmentPattern } from '../AssignmentPattern/spec';
-import type { RestElement } from '../RestElement/spec';
 
 export interface TSParameterProperty extends BaseNode {
   type: AST_NODE_TYPES.TSParameterProperty;
   accessibility: Accessibility | undefined;
   decorators: Decorator[];
   override: boolean;
-  parameter: AssignmentPattern | BindingName | RestElement;
+  parameter: AssignmentPattern | Identifier;
   readonly: boolean;
   static: boolean;
 }
