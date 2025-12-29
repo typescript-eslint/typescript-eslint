6a05a1ab06c2ce91965949c8706c618e69494b44
Lonercode <91500485+Lonercode@users.noreply.github.com>
2025-12-29 20:31:53 +0100

    chore: extract AST check from convert.ts to ast-checks.ts (#11748)
    * Chore: Extract AST check from convert.ts to ast-checks.ts
    
    * chore: extract AST check from convert.ts to ast-checks.ts
    
    * chore: add test for ast-checks.ts
    
    * chore: add test for ast-checks.ts
    
    * chore: add test for ast-checks.ts
    
    * chore: add function in ast-checks to run ts node checks
    
    * chore: fix lint errors
    
    * chore: update test
    
    * chore: address review
    
    * chore: review update
    
    * chore: update based on reviews
    
    * chore: minor fix
    
    * chore: merge main and update code
    
    * chore: minor update
    
    * chore: update based on reviews

---
+++ packages/typescript-estree/src/check-syntax-errors.ts
@@ -0,0 +1,70 @@
+import * as ts from 'typescript';
+
+import type { TSNode } from './ts-estree';
+
+import { checkModifiers } from './check-modifiers';
+import { isValidAssignmentTarget, createError } from './node-utils';
+
+const SyntaxKind = ts.SyntaxKind;
+
+export function checkSyntaxError(tsNode: ts.Node): void {
+  checkModifiers(tsNode);
+
+  const node = tsNode as TSNode;
+
+  switch (node.kind) {
+    case SyntaxKind.ForInStatement:
+    case SyntaxKind.ForOfStatement: {
+      checkForStatementDeclaration(node);
+      break;
+    }
+    default: {
+      break;
+    }
+  }
+}
+
+function checkForStatementDeclaration(
+  node: ts.ForInStatement | ts.ForOfStatement,
+): void {
+  const { initializer, kind } = node;
+  const loop = kind === SyntaxKind.ForInStatement ? 'for...in' : 'for...of';
+  if (ts.isVariableDeclarationList(initializer)) {
+    if (initializer.declarations.length !== 1) {
+      throw createError(
+        initializer,
+        `Only a single variable declaration is allowed in a '${loop}' statement.`,
+      );
+    }
+    const declaration = initializer.declarations[0];
+    if (declaration.initializer) {
+      throw createError(
+        declaration,
+        `The variable declaration of a '${loop}' statement cannot have an initializer.`,
+      );
+    } else if (declaration.type) {
+      throw createError(
+        declaration,
+        `The variable declaration of a '${loop}' statement cannot have a type annotation.`,
+      );
+    }
+    if (
+      kind === SyntaxKind.ForInStatement &&
+      initializer.flags & ts.NodeFlags.Using
+    ) {
+      throw createError(
+        initializer,
+        "The left-hand side of a 'for...in' statement cannot be a 'using' declaration.",
+      );
+    }
+  } else if (
+    !isValidAssignmentTarget(initializer) &&
+    initializer.kind !== SyntaxKind.ObjectLiteralExpression &&
+    initializer.kind !== SyntaxKind.ArrayLiteralExpression
+  ) {
+    throw createError(
+      initializer,
+      `The left-hand side of a '${loop}' statement must be a variable or a property access.`,
+    );
+  }
+}
