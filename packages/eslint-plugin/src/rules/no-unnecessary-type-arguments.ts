e4f178d1a9526404d9ea08c811de265792f5d7df
ejshafran <ejshafran@hilma.tech>
2026-01-03 19:08:20 +0200

    refactor(eslint-plugin): rename parameters to be a bit clearer

--- packages/eslint-plugin/src/rules/no-unnecessary-type-arguments.ts
+++ packages/eslint-plugin/src/rules/no-unnecessary-type-arguments.ts
@@ -62,28 +62,29 @@ export default createRule<[], MessageIds>({
     }
 
     function checkTSArgsAndParameters(
-      esParameters: TSESTree.TSTypeParameterInstantiation,
+      typeArguments: TSESTree.TSTypeParameterInstantiation,
       typeParameters: readonly ts.TypeParameterDeclaration[],
     ): void {
       // Just check the last one. Must specify previous type parameters if the last one is specified.
-      const i = esParameters.params.length - 1;
-      const arg = esParameters.params[i];
-      const param = typeParameters.at(i);
-      if (!param?.default) {
+      const i = typeArguments.params.length - 1;
+      const typeArgument = typeArguments.params[i];
+      const typeParameter = typeParameters.at(i);
+
+      if (!typeParameter?.default) {
         return;
       }
 
       // TODO: would like checker.areTypesEquivalent. https://github.com/Microsoft/TypeScript/issues/13502
-      const defaultType = checker.getTypeAtLocation(param.default);
-      const argType = services.getTypeAtLocation(arg);
+      const defaultType = checker.getTypeAtLocation(typeParameter.default);
+      const typeArgumentType = services.getTypeAtLocation(typeArgument);
       // this check should handle some of the most simple cases of like strings, numbers, etc
-      if (defaultType !== argType) {
+      if (defaultType !== typeArgumentType) {
         // For more complex types (like aliases to generic object types) - TS won't always create a
         // global shared type object for the type - so we need to resort to manually comparing the
         // reference type and the passed type arguments.
         // Also - in case there are aliases - we need to resolve them before we do checks
         const defaultTypeResolved = getTypeForComparison(defaultType);
-        const argTypeResolved = getTypeForComparison(argType);
+        const argTypeResolved = getTypeForComparison(typeArgumentType);
         if (
           // ensure the resolved type AND all the parameters are the same
           defaultTypeResolved.type !== argTypeResolved.type ||
@@ -98,13 +99,13 @@ export default createRule<[], MessageIds>({
       }
 
       context.report({
-        node: arg,
+        node: typeArgument,
         messageId: 'unnecessaryTypeParameter',
         fix: fixer =>
           fixer.removeRange(
             i === 0
-              ? esParameters.range
-              : [esParameters.params[i - 1].range[1], arg.range[1]],
+              ? typeArguments.range
+              : [typeArguments.params[i - 1].range[1], typeArgument.range[1]],
           ),
       });
     }
