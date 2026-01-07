fa5d94242bc44816562908222fff51ed32eb4eb8
Francesco Trotta <github@fasttime.org>
2026-01-07 01:04:51 +0100

    fix: handle missing `FlatESLint` and `LegacyESLint`

--- packages/utils/tests/ts-eslint/ESLint.test.ts
+++ packages/utils/tests/ts-eslint/ESLint.test.ts
@@ -1,35 +1,51 @@
-import { FlatESLint, LegacyESLint } from 'eslint/use-at-your-own-risk';
+import { ESLint } from 'eslint';
+import useAtYourOwnRisk from 'eslint/use-at-your-own-risk';
 
 import type { FlatConfig } from '../../src/ts-eslint';
 
-import * as ESLint from '../../src/ts-eslint/ESLint';
+import { FlatESLint, LegacyESLint } from '../../src/ts-eslint/ESLint';
 
 describe('ESLint', () => {
-  describe('Constructs successfully and has the correct base type', () => {
-    it('flat', () => {
-      const eslint = new ESLint.FlatESLint({
+  describe('Constructors', () => {
+    it('flat constructs successfully and has the correct base type', () => {
+      const eslint = new FlatESLint({
         // accepts flat configs
         baseConfig: [{ ignores: [] }, { languageOptions: {} }],
         overrideConfig: [{ ignores: [] }, { languageOptions: {} }],
       });
-      expect(eslint).toBeInstanceOf(FlatESLint);
+      if (useAtYourOwnRisk.FlatESLint) {
+        expect(eslint).toBeInstanceOf(useAtYourOwnRisk.FlatESLint);
+      } else {
+        expect(eslint).toBeInstanceOf(ESLint);
+      }
     });
 
-    it('legacy', () => {
-      // eslint-disable-next-line @typescript-eslint/no-deprecated
-      const eslint = new ESLint.LegacyESLint({
-        // accepts legacy configs
-        baseConfig: {
-          overrides: [],
-          parserOptions: {},
-        },
-        overrideConfig: {
-          overrides: [],
-          parserOptions: {},
-        },
+    if (useAtYourOwnRisk.LegacyESLint) {
+      it('legacy constructs successfully and has the correct base type', () => {
+        // eslint-disable-next-line @typescript-eslint/no-deprecated
+        const eslint = new LegacyESLint({
+          // accepts legacy configs
+          baseConfig: {
+            overrides: [],
+            parserOptions: {},
+          },
+          overrideConfig: {
+            overrides: [],
+            parserOptions: {},
+          },
+        });
+        expect(eslint).toBeInstanceOf(useAtYourOwnRisk.LegacyESLint);
       });
-      expect(eslint).toBeInstanceOf(LegacyESLint);
-    });
+    } else {
+      it('legacy throws an error', () => {
+        expect(() => {
+          // eslint-disable-next-line @typescript-eslint/no-deprecated
+          new LegacyESLint();
+        }).toThrowError(
+          'LegacyESLint is not available with the current version of ESLint.',
+        );
+      });
+    }
 
     it('config type permits basePath (type test)', () => {
       const __config: FlatConfig.Config = {
