2e07f00dcdd85788975cb9d3ecc2bbc92dee74c1
Josh Goldberg <git@joshuakgoldberg.com>
2026-01-12 09:37:28 -0500

    Update .d.ts customization to new top level language namespaces

--- packages/website/typings/monaco-editor.d.ts
+++ packages/website/typings/monaco-editor.d.ts
@@ -1,7 +1,7 @@
 import 'monaco-editor/esm/vs/editor/editor.api';
 
 declare module 'monaco-editor/esm/vs/editor/editor.api' {
-  namespace languages.typescript {
+  namespace typescript {
     interface TypeScriptWorker {
       /**
        * https://github.com/microsoft/TypeScript-Website/blob/246798df5013036bd9b4389932b642c20ab35deb/packages/playground-worker/types.d.ts#L48
