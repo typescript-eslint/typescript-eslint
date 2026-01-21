ec1fac63cd93fbc6f68dd290f0b245761c0a8140
Francesco Trotta <github@fasttime.org>
2026-01-21 18:50:59 +0100

    add back updated TODO comment

--- packages/utils/src/ts-eslint/ESLint.ts
+++ packages/utils/src/ts-eslint/ESLint.ts
@@ -1,6 +1,7 @@
 export { FlatESLint } from './eslint/FlatESLint';
 export { FlatESLint as ESLint } from './eslint/FlatESLint';
 export {
+  // TODO(typescript-eslint@v9) - remove this in the next major release
   /**
    * @deprecated - use ESLint instead
    */
