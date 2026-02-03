93a1b50470fa9f254c98e20c531f71c77ab37811
Lonercode <penpalclassmate@gmail.com>
2026-02-03 20:03:57 +0100

    docs: update based on review

--- tools/release/changelog-renderer.js
+++ tools/release/changelog-renderer.js
@@ -8,11 +8,9 @@ const {
 module.exports = class CustomChangelogRenderer extends (
   DefaultChangelogRenderer
 ) {
-  /** @type {any} */
-  config;
   async render() {
     const defaultChangelog = await super.render();
-    const version = this.config?.changelogEntryVersion;
+    const version = this.changelogEntryVersion;
     const githubLink = version
       ? `See [GitHub Releases](https://github.com/typescript-eslint/typescript-eslint/releases/tag/v${version}) for more information.`
       : `See [GitHub Releases](https://github.com/typescript-eslint/typescript-eslint/releases) for more information.`;
