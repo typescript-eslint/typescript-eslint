d6d07455cacaa138e967c03c0e83736f61aa2e5b
bradzacher <brad.zacher@gmail.com>
2026-02-13 16:59:18 +1030

    fix test

--- packages/utils/tests/ts-eslint/ESLint.test.ts
+++ packages/utils/tests/ts-eslint/ESLint.test.ts
@@ -1,40 +1,64 @@
-import { FlatESLint, LegacyESLint } from 'eslint/use-at-your-own-risk';
-
-import type { FlatConfig } from '../../src/ts-eslint';
+import { ESLint as BaseESLint } from 'eslint';
 
 import * as ESLint from '../../src/ts-eslint/ESLint';
 
-describe('ESLint', () => {
-  describe('Constructs successfully and has the correct base type', () => {
-    it('flat', () => {
+describe('ESLint v10+', () => {
+  describe('FlatESLint', () => {
+    it('constructs successfully and has the correct base type', () => {
       const eslint = new ESLint.FlatESLint({
         // accepts flat configs
         baseConfig: [{ ignores: [] }, { languageOptions: {} }],
         overrideConfig: [{ ignores: [] }, { languageOptions: {} }],
       });
-      expect(eslint).toBeInstanceOf(FlatESLint);
+      expect(eslint).toBeInstanceOf(BaseESLint);
     });
-
-    it('legacy', () => {
-      // eslint-disable-next-line @typescript-eslint/no-deprecated
-      const eslint = new ESLint.LegacyESLint({
-        // accepts legacy configs
-        baseConfig: {
-          overrides: [],
-          parserOptions: {},
-        },
-        overrideConfig: {
-          overrides: [],
-          parserOptions: {},
-        },
-      });
-      expect(eslint).toBeInstanceOf(LegacyESLint);
+  });
+  describe('LegacyESLint', () => {
+    it('constructor fails with an error', () => {
+      expect(() => {
+        // eslint-disable-next-line @typescript-eslint/no-deprecated
+        new ESLint.LegacyESLint();
+      }).toThrowError(
+        'LegacyESLint is not available with the current version of ESLint.',
+      );
     });
-
-    it('config type permits basePath (type test)', () => {
-      const __config: FlatConfig.Config = {
-        basePath: 'some/path',
-      };
+    it('configType', () => {
+      expect(
+        // eslint-disable-next-line @typescript-eslint/no-deprecated
+        ESLint.LegacyESLint.configType,
+      ).toBe('eslintrc');
+    });
+    it('version', () => {
+      expect(
+        // eslint-disable-next-line @typescript-eslint/no-deprecated
+        ESLint.LegacyESLint.version,
+      ).toBe((BaseESLint as typeof ESLint.ESLint).version);
+    });
+    it('outputFixes fails with an error', async () => {
+      await expect(async () =>
+        // eslint-disable-next-line @typescript-eslint/no-deprecated
+        ESLint.LegacyESLint.outputFixes([]),
+      ).rejects.toThrowError(
+        'LegacyESLint is not available with the current version of ESLint.',
+      );
+    });
+    it('getErrorResults fails with an error', () => {
+      expect(() =>
+        // eslint-disable-next-line @typescript-eslint/no-deprecated
+        ESLint.LegacyESLint.getErrorResults({
+          errorCount: 0,
+          fatalErrorCount: 0,
+          filePath: '',
+          fixableErrorCount: 0,
+          fixableWarningCount: 0,
+          messages: [],
+          suppressedMessages: [],
+          usedDeprecatedRules: [],
+          warningCount: 0,
+        }),
+      ).toThrowError(
+        'LegacyESLint is not available with the current version of ESLint.',
+      );
     });
   });
 });
