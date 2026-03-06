686e2f786c401f4772d0ccb542468ef054181a2c
Kirk Waiblinger <53019676+kirkwaiblinger@users.noreply.github.com>
2026-03-05 18:05:17 -0700

    export basic globs for using tseslint

---
+++ packages/typescript-eslint/tests/globs.test.ts
@@ -0,0 +1,30 @@
+import { minimatch } from 'minimatch';
+
+import tseslint from '../src';
+
+describe('tsDeclaration', () => {
+  it.for(['file.d.ts', 'path/to/file.d.ts', 'deeply/nested/path/to/file.d.ts'])(
+    'matches standard TypeScript declaration file: "%s"',
+    testCase => {
+      expect(minimatch(testCase, tseslint.globs.tsDeclaration)).toBe(true);
+    },
+  );
+
+  it.for(['file.d.css.ts', 'file.d.some-other-extension.ts'])(
+    'matches TypeScript declaration file with additional extension: "%s"',
+    testCase => {
+      expect(minimatch(testCase, tseslint.globs.tsDeclaration)).toBe(true);
+    },
+  );
+
+  it.for([
+    'file.ts',
+    'file.mts',
+    'file.cts',
+    'file.dts',
+    'file.tsx',
+    'path/to/file.ts',
+  ])('does not match non-declaration file: "%s"', testCase => {
+    expect(minimatch(testCase, tseslint.globs.tsDeclaration)).toBe(false);
+  });
+});
