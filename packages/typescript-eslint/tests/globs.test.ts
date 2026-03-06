1ab069d92e632dac342e009e17d43b4d8d23edb3
Kirk Waiblinger <53019676+kirkwaiblinger@users.noreply.github.com>
2026-03-05 18:50:09 -0700

    tweak again

--- packages/typescript-eslint/tests/globs.test.ts
+++ packages/typescript-eslint/tests/globs.test.ts
@@ -3,25 +3,27 @@ import { minimatch } from 'minimatch';
 import tseslint from '../src';
 
 const MINIMATCH_OPTIONS = { dot: true };
-function createMatcherLikeEslint(pattern: string) {
-  // https://github.com/eslint/eslint/blob/main/lib/eslint/eslint-helpers.js
-  return new minimatch.Minimatch(pattern, MINIMATCH_OPTIONS);
+/**
+ * Helper function to call minimatch in roughly the same way that eslint uses it.
+ * https://github.com/eslint/eslint/blob/main/lib/eslint/eslint-helpers.js
+ */
+function callMinimatch(testString: string, pattern: string): boolean {
+  const matcher = new minimatch.Minimatch(pattern, MINIMATCH_OPTIONS);
+  return matcher.match(testString);
 }
 
 describe('tsDeclaration', () => {
   it.for(['file.d.ts', 'path/to/file.d.ts', 'deeply/nested/path/to/file.d.ts'])(
     'matches standard TypeScript declaration file: "%s"',
     testCase => {
-      const matcher = createMatcherLikeEslint(tseslint.globs.tsDeclaration);
-      expect(matcher.match(testCase)).toBe(true);
+      expect(callMinimatch(testCase, tseslint.globs.tsDeclaration)).toBe(true);
     },
   );
 
   it.for(['file.d.css.ts', 'file.d.some-other-extension.ts'])(
     'matches TypeScript declaration file with additional extension: "%s"',
     testCase => {
-      const matcher = createMatcherLikeEslint(tseslint.globs.tsDeclaration);
-      expect(matcher.match(testCase)).toBe(true);
+      expect(callMinimatch(testCase, tseslint.globs.tsDeclaration)).toBe(true);
     },
   );
 
@@ -33,7 +35,6 @@ describe('tsDeclaration', () => {
     'file.tsx',
     'path/to/file.ts',
   ])('does not match non-declaration file: "%s"', testCase => {
-    const matcher = createMatcherLikeEslint(tseslint.globs.tsDeclaration);
-    expect(matcher.match(testCase)).toBe(false);
+    expect(callMinimatch(testCase, tseslint.globs.tsDeclaration)).toBe(false);
   });
 });
