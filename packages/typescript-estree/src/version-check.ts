8cde2d06fb9bc591a4c93452509ddbd600c76a35
Evyatar Daud <32631382+StyleShit@users.noreply.github.com>
2026-03-29 13:16:11 +0300

    feat: support TypeScript 6 (#12124)

--- packages/typescript-estree/src/version-check.ts
+++ packages/typescript-estree/src/version-check.ts
@@ -20,6 +20,12 @@ const versions = [
   '5.2',
   '5.3',
   '5.4',
+  '5.5',
+  '5.6',
+  '5.7',
+  '5.8',
+  '5.9',
+  '6.0',
 ] as const;
 type Versions = typeof versions extends ArrayLike<infer U> ? U : never;
 
