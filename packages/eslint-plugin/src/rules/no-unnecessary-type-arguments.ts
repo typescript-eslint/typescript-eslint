76502016c25d5e8201298af4b8393614fd664cc9
ejshafran <ejshafran@hilma.tech>
2026-04-08 22:03:23 +0300

    refactor(eslint-plugin): extract `areTypesEquivalent`

--- packages/eslint-plugin/src/rules/no-unnecessary-type-arguments.ts
+++ packages/eslint-plugin/src/rules/no-unnecessary-type-arguments.ts
@@ -61,50 +61,57 @@ export default createRule<[], MessageIds>({
       };
     }
 
+    function areTypesEquivalent(a: ts.Type, b: ts.Type) {
+      if (a === b) return true;
+
+      const aResolved = getTypeForComparison(a);
+      const bResolved = getTypeForComparison(b);
+
+      if (aResolved.type !== bResolved.type) return false;
+      if (aResolved.typeArguments.length != bResolved.typeArguments.length)
+        return false;
+      if (
+        aResolved.typeArguments.some((t, i) => t !== bResolved.typeArguments[i])
+      )
+        return false;
+      return true;
+    }
+
     function checkTSArgsAndParameters(
-      esParameters: TSESTree.TSTypeParameterInstantiation,
+      typeArguments: TSESTree.TSTypeParameterInstantiation,
       typeParameters: readonly ts.TypeParameterDeclaration[],
     ): void {
       // Just check the last one. Must specify previous type parameters if the last one is specified.
-      const i = esParameters.params.length - 1;
-      const arg = esParameters.params[i];
-      const param = typeParameters.at(i);
-      if (!param?.default) {
+      const typeArgumentIndex = typeArguments.params.length - 1;
+      const typeArgument = typeArguments.params[typeArgumentIndex];
+      const typeParameter = typeParameters.at(typeArgumentIndex);
+
+      if (!typeParameter) {
         return;
       }
 
-      // TODO: would like checker.areTypesEquivalent. https://github.com/Microsoft/TypeScript/issues/13502
-      const defaultType = checker.getTypeAtLocation(param.default);
-      const argType = services.getTypeAtLocation(arg);
-      // this check should handle some of the most simple cases of like strings, numbers, etc
-      if (defaultType !== argType) {
-        // For more complex types (like aliases to generic object types) - TS won't always create a
-        // global shared type object for the type - so we need to resort to manually comparing the
-        // reference type and the passed type arguments.
-        // Also - in case there are aliases - we need to resolve them before we do checks
-        const defaultTypeResolved = getTypeForComparison(defaultType);
-        const argTypeResolved = getTypeForComparison(argType);
-        if (
-          // ensure the resolved type AND all the parameters are the same
-          defaultTypeResolved.type !== argTypeResolved.type ||
-          defaultTypeResolved.typeArguments.length !==
-            argTypeResolved.typeArguments.length ||
-          defaultTypeResolved.typeArguments.some(
-            (t, i) => t !== argTypeResolved.typeArguments[i],
-          )
-        ) {
-          return;
-        }
+      const typeArgumentType = services.getTypeAtLocation(typeArgument);
+
+      if (!typeParameter.default) {
+        return;
+      }
+
+      const defaultType = checker.getTypeAtLocation(typeParameter.default);
+      if (!areTypesEquivalent(defaultType, typeArgumentType)) {
+        return;
       }
 
       context.report({
-        node: arg,
+        node: typeArgument,
         messageId: 'isDefaultTypeArgument',
         fix: fixer =>
           fixer.removeRange(
-            i === 0
-              ? esParameters.range
-              : [esParameters.params[i - 1].range[1], arg.range[1]],
+            typeArgumentIndex === 0
+              ? typeArguments.range
+              : [
+                  typeArguments.params[typeArgumentIndex - 1].range[1],
+                  typeArgument.range[1],
+                ],
           ),
       });
     }
