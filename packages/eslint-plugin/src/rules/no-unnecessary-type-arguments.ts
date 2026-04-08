530ac264fb035db48ab6de21b7f2749791786d3c
ejshafran <ejshafran@hilma.tech>
2026-04-08 22:19:03 +0300

    feat(eslint-plugin): add `checkForInferrableTypes` option
    When the option is passed, use the code that was reverted.
    
    TODO: add `invalid` test cases.
    TODO: modify regression tests to pass this option, and fix them so they
    work.

--- packages/eslint-plugin/src/rules/no-unnecessary-type-arguments.ts
+++ packages/eslint-plugin/src/rules/no-unnecessary-type-arguments.ts
@@ -22,9 +22,15 @@ type ParameterCapableTSNode =
   | ts.TypeQueryNode
   | ts.TypeReferenceNode;
 
-export type MessageIds = 'isDefaultTypeArgument';
+export type MessageIds = 'isDefaultTypeArgument' | 'canBeInferred';
 
-export default createRule<[], MessageIds>({
+export type Options = [
+  {
+    checkForInferrableTypes?: boolean;
+  },
+];
+
+export default createRule<Options, MessageIds>({
   name: 'no-unnecessary-type-arguments',
   meta: {
     type: 'suggestion',
@@ -37,10 +43,24 @@ export default createRule<[], MessageIds>({
     messages: {
       isDefaultTypeArgument:
         'This is the default value for this type parameter, so it can be omitted.',
+      canBeInferred:
+        'This value can be trivially inferred for this type parameter, so it can be omitted.',
     },
-    schema: [],
+    defaultOptions: [{}],
+    schema: [
+      {
+        type: 'object',
+        additionalProperties: false,
+        properties: {
+          checkForInferrableTypes: {
+            type: 'boolean',
+            description:
+              'Whether to check for type arguments that could be inferred.',
+          },
+        },
+      },
+    ],
   },
-  defaultOptions: [],
   create(context) {
     const services = getParserServices(context);
     const checker = services.program.getTypeChecker();
@@ -80,6 +100,7 @@ export default createRule<[], MessageIds>({
     function checkTSArgsAndParameters(
       typeArguments: TSESTree.TSTypeParameterInstantiation,
       typeParameters: readonly ts.TypeParameterDeclaration[],
+      tsNode: ParameterCapableTSNode,
     ): void {
       // Just check the last one. Must specify previous type parameters if the last one is specified.
       const typeArgumentIndex = typeArguments.params.length - 1;
@@ -91,6 +112,69 @@ export default createRule<[], MessageIds>({
       }
 
       const typeArgumentType = services.getTypeAtLocation(typeArgument);
+
+      if (context.options[0].checkForInferrableTypes) {
+        const parent = typeArguments.parent;
+
+        if (
+          parent.type === AST_NODE_TYPES.CallExpression ||
+          parent.type === AST_NODE_TYPES.NewExpression
+        ) {
+          const sig = checker.getResolvedSignature(
+            tsNode as ts.CallExpression | ts.NewExpression,
+          );
+
+          parent.arguments.forEach((argument, i) => {
+            const parameter = sig?.parameters.at(i);
+
+            if (
+              !parameter?.valueDeclaration ||
+              !ts.isParameter(parameter.valueDeclaration) ||
+              !parameter.valueDeclaration.type
+            ) {
+              return;
+            }
+
+            const typeParameterType = checker.getTypeAtLocation(typeParameter);
+
+            const parameterTypeFromDeclaration = checker.getTypeFromTypeNode(
+              parameter.valueDeclaration.type,
+            );
+
+            // TODO: right now we check if the type is declared as `T` or `T | something`;
+            // we should really be checking if `parameterTypeFromDeclaration` depends on `typeParameterType` somehow
+            // (e.g. `NonNullable<T>`, `(arg: T) => void`, etc.)
+            if (
+              !checker.isTypeAssignableTo(
+                typeParameterType,
+                parameterTypeFromDeclaration,
+              )
+            ) {
+              return;
+            }
+
+            const argumentType = checker.getBaseTypeOfLiteralType(
+              services.getTypeAtLocation(argument),
+            );
+
+            if (areTypesEquivalent(typeArgumentType, argumentType)) {
+              context.report({
+                node: typeArgument,
+                messageId: 'canBeInferred',
+                fix: fixer =>
+                  fixer.removeRange(
+                    typeArgumentIndex === 0
+                      ? typeArguments.range
+                      : [
+                          typeArguments.params[typeArgumentIndex - 1].range[1],
+                          typeArgument.range[1],
+                        ],
+                  ),
+              });
+            }
+          });
+        }
+      }
 
       if (!typeParameter.default) {
         return;
@@ -126,7 +210,7 @@ export default createRule<[], MessageIds>({
         );
 
         if (typeParameters) {
-          checkTSArgsAndParameters(node, typeParameters);
+          checkTSArgsAndParameters(node, typeParameters, expression);
         }
       },
     };
