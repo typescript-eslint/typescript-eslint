77080a256ab56cb81288dc8df0b6ce2232dbb815
Kirk Waiblinger <53019676+kirkwaiblinger@users.noreply.github.com>
2026-05-27 23:24:00 -0600

    feat(ast-spec): `prefix` is always `true` for UnaryExpression

--- packages/ast-spec/src/base/UnaryExpressionBase.ts
+++
@@ -1,8 +0,0 @@
-import type { Expression } from '../unions/Expression';
-import type { BaseNode } from './BaseNode';
-
-export interface UnaryExpressionBase extends BaseNode {
-  argument: Expression;
-  operator: string;
-  prefix: boolean;
-}
