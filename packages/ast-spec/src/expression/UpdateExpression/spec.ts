3be642de8fd231ec2f717686cc0a3518549785b6
Kirk Waiblinger <53019676+kirkwaiblinger@users.noreply.github.com>
2026-05-27 23:52:03 -0600

    derp - extend BaseNode

--- packages/ast-spec/src/expression/UpdateExpression/spec.ts
+++ packages/ast-spec/src/expression/UpdateExpression/spec.ts
@@ -1,7 +1,8 @@
 import type { AST_NODE_TYPES } from '../../ast-node-types';
+import type { BaseNode } from '../../base/BaseNode';
 import type { Expression } from '../../unions/Expression';
 
-export interface UpdateExpression {
+export interface UpdateExpression extends BaseNode {
   type: AST_NODE_TYPES.UpdateExpression;
   argument: Expression;
   operator: '++' | '--';
