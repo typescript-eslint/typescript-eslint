c889b427097f2b3e30a583508dea0f1aa3ccf579
Kirk Waiblinger <53019676+kirkwaiblinger@users.noreply.github.com>
2026-07-09 14:08:25 -0600

    write directly to stderr

--- packages/eslint-plugin/src/index.ts
+++ packages/eslint-plugin/src/index.ts
@@ -7,13 +7,15 @@ const [versionMajor, _versionMinor] = ts.versionMajorMinor
   .map(Number);
 
 if (versionMajor >= 7) {
-  throw new Error(
+  // eslint-disable-next-line no-console
+  console.error(
     [
       'typescript-eslint does not support TS 7.',
       'Please see https://devblogs.microsoft.com/typescript/announcing-typescript-7-0/#running-side-by-side-with-typescript-6.0 to run typescript-eslint using the TS 6 API.',
       "See also https://github.com/typescript-eslint/typescript-eslint/issues/10940 for tracking typescript-eslint's support for TS 7",
     ].join('\n'),
   );
+  throw new Error('typescript-eslint does not support TS 7.');
 }
 
 import rawPlugin from './raw-plugin';
