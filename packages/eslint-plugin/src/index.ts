f7970c828a47657a1afeb570209d8dfa662fa377
Kirk Waiblinger <53019676+kirkwaiblinger@users.noreply.github.com>
2026-07-09 13:36:58 -0600

    fix

--- packages/eslint-plugin/src/index.ts
+++ packages/eslint-plugin/src/index.ts
@@ -1,3 +1,21 @@
+/* eslint-disable import/first -- intentionally executing code before rest of the require()s. This will not work with ESM. */
+
+import * as ts from 'typescript';
+
+const [versionMajor, _versionMinor] = ts.versionMajorMinor
+  .split('.')
+  .map(Number);
+
+if (versionMajor >= 7) {
+  throw new Error(
+    [
+      'typescript-eslint does not support TS 7.',
+      'Please see https://devblogs.microsoft.com/typescript/announcing-typescript-7-0/#running-side-by-side-with-typescript-6.0 to run typescript-eslint using the TS 6 API.',
+      "See also https://github.com/typescript-eslint/typescript-eslint/issues/10940 for tracking typescript-eslint's support for TS 7",
+    ].join('\n'),
+  );
+}
+
 import rawPlugin from './raw-plugin';
 
 export = rawPlugin.plugin;
--- packages/parser/src/index.ts
+++ packages/parser/src/index.ts
@@ -1,3 +1,21 @@
+import * as ts from 'typescript';
+
+// intentionally executing code before rest of the require()s. This will not work with ESM.
+
+const [versionMajor, _versionMinor] = ts.versionMajorMinor
+  .split('.')
+  .map(Number);
+
+if (versionMajor >= 7) {
+  throw new Error(
+    [
+      'typescript-eslint does not support TS 7.',
+      'Please see https://devblogs.microsoft.com/typescript/announcing-typescript-7-0/#running-side-by-side-with-typescript-6.0 to run typescript-eslint using the TS 6 API.',
+      "See also https://github.com/typescript-eslint/typescript-eslint/issues/10940 for tracking typescript-eslint's support for TS 7",
+    ].join('\n'),
+  );
+}
+
 export { parse, parseForESLint, type ParserOptions } from './parser';
 export {
   clearCaches,
