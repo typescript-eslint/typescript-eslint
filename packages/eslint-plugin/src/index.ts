00cafe46d40916124bbf6e17fff6b255d947f7ad
Kirk Waiblinger <53019676+kirkwaiblinger@users.noreply.github.com>
2026-07-12 14:43:54 -0600

    Update packages/eslint-plugin/src/index.ts
    Co-authored-by: Josh Goldberg ✨ <git@joshuakgoldberg.com>

--- packages/eslint-plugin/src/index.ts
+++ packages/eslint-plugin/src/index.ts
@@ -10,7 +10,7 @@ if (versionMajor >= 7) {
   // eslint-disable-next-line no-console
   console.error(
     [
-      'typescript-eslint does not support TS 7.',
+      'typescript-eslint does not support TS 7.0.',
       'Please see https://devblogs.microsoft.com/typescript/announcing-typescript-7-0/#running-side-by-side-with-typescript-6.0 to run typescript-eslint using the TS 6 API.',
       "See also https://github.com/typescript-eslint/typescript-eslint/issues/10940 for tracking typescript-eslint's support for TS 7",
     ].join('\n'),
