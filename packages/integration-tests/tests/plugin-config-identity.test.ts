ec4c1e8a6f591799d92bf87862eec120a1e99720
awnoa <zerouali.bardai.omar@gmail.com>
2026-07-18 21:07:28 +0200

    test(integration-tests): adapt plugin identity runner

--- packages/integration-tests/tests/plugin-config-identity.test.ts
+++ packages/integration-tests/tests/plugin-config-identity.test.ts
@@ -1,3 +1,8 @@
 import { nodeIntegrationTest } from '../tools/integration-test-base';
 
+nodeIntegrationTest(__filename, 'index.mjs', stderr => {
+  expect(stderr).toBe('');
+});
+import { nodeIntegrationTest } from '../tools/integration-test-base';
+
 nodeIntegrationTest(__filename, 'index.mjs');
