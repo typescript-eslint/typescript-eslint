0083d51ade022043b7f959ccf66166869f80e7ad
awnoa <zerouali.bardai.omar@gmail.com>
2026-07-18 21:09:18 +0200

    test(integration-tests): fix plugin identity test

--- packages/integration-tests/tests/plugin-config-identity.test.ts
+++ packages/integration-tests/tests/plugin-config-identity.test.ts
@@ -3,6 +3,3 @@ import { nodeIntegrationTest } from '../tools/integration-test-base';
 nodeIntegrationTest(__filename, 'index.mjs', stderr => {
   expect(stderr).toBe('');
 });
-import { nodeIntegrationTest } from '../tools/integration-test-base';
-
-nodeIntegrationTest(__filename, 'index.mjs');
