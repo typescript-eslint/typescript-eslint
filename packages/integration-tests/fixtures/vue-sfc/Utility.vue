1ad0a2d6b14aede2e061e3461e9dfd302d0c8b13
StyleShit <32631382+StyleShit@users.noreply.github.com>
2026-08-10 09:18:11 +0300

    test(integration-tests): modernize vue tests

--- packages/integration-tests/fixtures/vue-sfc/Utility.vue
+++ packages/integration-tests/fixtures/vue-sfc/Utility.vue
@@ -1,6 +1,5 @@
 <script lang="ts">
 /* https://github.com/typescript-eslint/typescript-eslint/issues/2591 */
-/* eslint-disable class-methods-use-this */
 export default class Utility {
   get a() {
     const list: Array<string> = [];
--- packages/integration-tests/tools/integration-test-base.ts
+++ packages/integration-tests/tools/integration-test-base.ts
@@ -76,8 +76,7 @@ export function eslintIntegrationTest(
         /"filePath":"([^"]*)"/g,
         (_, testFile: string) =>
           `"filePath": "<root>/${path.relative(testFolder, testFile)}"`,
-      )
-      .replaceAll(/C:\\\\(usr)\\\\(linked)\\\\(tsconfig.json)/g, '/$1/$2/$3');
+      );
     let lintOutput: unknown;
     try {
       lintOutput = JSON.parse(lintOutputRAW);
