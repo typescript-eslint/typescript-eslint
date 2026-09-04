9b2e775de929865bedb393addc97bf76ff1125fb
Vincent Girard <vinccool96@gmail.com>
2026-09-04 11:07:24 -0400

    fix(eslint-plugin-internal): suppress no-multiple-lines-of-errors

--- packages/eslint-plugin/tests/rules/member-ordering/member-ordering-natural-case-insensitive-order.test.ts
+++ packages/eslint-plugin/tests/rules/member-ordering/member-ordering-natural-case-insensitive-order.test.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/internal/no-multiple-lines-of-errors */
 import { RuleTester } from '@typescript-eslint/rule-tester';
 
 import rule from '../../../src/rules/member-ordering';
