f3be6db871e8fbb86431d87ae792bb194e8ac455
Cameron Clark <cameron.clark@hey.com>
2026-09-15 11:17:55 +0800

    fix(typescript-estree): require string literal import attribute values

--- packages/ast-spec/src/special/ImportAttribute/spec.ts
+++ packages/ast-spec/src/special/ImportAttribute/spec.ts
@@ -2,10 +2,9 @@ import type { AST_NODE_TYPES } from '../../ast-node-types';
 import type { BaseNode } from '../../base/BaseNode';
 import type { Identifier } from '../../expression/Identifier/spec';
 import type { StringLiteral } from '../../expression/literal/StringLiteral/spec';
-import type { Literal } from '../../unions/Literal';
 
 export interface ImportAttribute extends BaseNode {
   type: AST_NODE_TYPES.ImportAttribute;
   key: Identifier | StringLiteral;
-  value: Literal;
+  value: StringLiteral;
 }
