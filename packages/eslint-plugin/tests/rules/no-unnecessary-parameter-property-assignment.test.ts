5bfb82049d0e89173466cabe3742f2dc6ee82b7d
diptomistry <diptomistrybd@gmail.com>
2026-09-15 10:28:21 +0600

    fix(eslint-plugin): [no-unnecessary-parameter-property-assignment] allow after param reassignment

--- packages/eslint-plugin/tests/rules/no-unnecessary-parameter-property-assignment.test.ts
+++ packages/eslint-plugin/tests/rules/no-unnecessary-parameter-property-assignment.test.ts
@@ -173,6 +173,46 @@ class Foo {
   }
 }
     `,
+    `
+class User {
+  constructor(public name: string) {
+    name = name.trim();
+    this.name = name;
+  }
+}
+    `,
+    `
+class User {
+  constructor(public name: string) {
+    name += '!';
+    this.name = name;
+  }
+}
+    `,
+    `
+class User {
+  constructor(
+    public name: string,
+    flag: boolean,
+  ) {
+    if (flag) {
+      name = name.trim();
+    }
+    this.name = name;
+  }
+}
+    `,
+    `
+class User {
+  constructor(
+    public name: string,
+    public age: number,
+  ) {
+    name = name.trim();
+    this.name = name;
+  }
+}
+    `,
   ],
   invalid: [
     {
@@ -518,6 +558,48 @@ class Foo {
           endColumn: 21,
           endLine: 5,
           line: 5,
+          messageId: 'unnecessaryAssign',
+        },
+      ],
+    },
+    {
+      code: `
+class User {
+  constructor(public name: string) {
+    this.name = name;
+    name = name.trim();
+  }
+}
+      `,
+      errors: [
+        {
+          column: 5,
+          endColumn: 21,
+          endLine: 4,
+          line: 4,
+          messageId: 'unnecessaryAssign',
+        },
+      ],
+    },
+    {
+      code: `
+class User {
+  constructor(
+    public name: string,
+    public age: number,
+  ) {
+    name = name.trim();
+    this.name = name;
+    this.age = age;
+  }
+}
+      `,
+      errors: [
+        {
+          column: 5,
+          endColumn: 19,
+          endLine: 9,
+          line: 9,
           messageId: 'unnecessaryAssign',
         },
       ],
