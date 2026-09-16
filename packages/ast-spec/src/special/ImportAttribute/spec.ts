c34e49315e011c8292c9907e895bc98c5e63d6c9
Cameron <cameron.clark@hey.com>
2026-09-17 06:01:11 +0800

    fix(ast-spec): narrow import attribute keys to identifiers and strings (#12879)

--- packages/ast-spec/src/special/ImportAttribute/spec.ts
+++ packages/ast-spec/src/special/ImportAttribute/spec.ts
@@ -1,10 +1,11 @@
 import type { AST_NODE_TYPES } from '../../ast-node-types';
 import type { BaseNode } from '../../base/BaseNode';
 import type { Identifier } from '../../expression/Identifier/spec';
+import type { StringLiteral } from '../../expression/literal/StringLiteral/spec';
 import type { Literal } from '../../unions/Literal';
 
 export interface ImportAttribute extends BaseNode {
   type: AST_NODE_TYPES.ImportAttribute;
-  key: Identifier | Literal;
+  key: Identifier | StringLiteral;
   value: Literal;
 }
